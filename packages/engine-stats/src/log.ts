07c90f0eb81e1a998f2a931415797e12e12d53fe
qianwei.xqw <qianwei.xqw@alibaba-inc.com>
2019-09-11 13:48:08 +0800

    引擎状态监控面版

---
+++ packages/engine-stats/src/log.ts
@@ -0,0 +1,7 @@
+export function log (...args: any) {
+  console.log('🚀 [o3-engine-stats]', ...args)
+}
+
+export function errorLog (...args: any) {
+  console.error('🚀 [o3-engine-stats]', ...args)
+}
