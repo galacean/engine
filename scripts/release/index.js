5a065768752cde03c2dad6b4608f130baf6c1cb6
胡松 <kevin.hs@antfin.com>
2019-09-23 10:54:01 +0800

    fix: lerna release

--- scripts/release/index.js
+++ scripts/release/index.js
@@ -1,11 +1,22 @@
-const getOtp = require("./get-otp-token");
 const { exec } = require("child_process");
+const { get } = require("/usr/local/lib/node_modules/tnpm/lib/get_otp");
+const {
+  alilangOTPEndpoint
+} = require("/usr/local/lib/node_modules/tnpm/lib/config");
 
-getOtp().then(res => {
-  exec(`tnpx lerna publish prerelease --yes --otp ${res}`, (err, stdout) => {
+get(alilangOTPEndpoint, (err, token) => {
+  if (err) {
+    return console.error(err);
+  }
+  exec(`tnpm run release:canary --otp ${token}`, (err, stdout, stderr) => {
     if (err) {
       return console.error(err);
     }
     console.log(stdout);
+    console.log(stderr);
   });
 });
+
+// getOtp().then(res => {
+//
+// });
