d89418f67c53f19a5bc175dbe62312bd6a253ca6
shensi <shensi.zxd@alibaba-inc.com>
2019-12-11 22:23:21 +0800

    refractor: 更新 Utils 方法

--- packages/base/src/Util.ts
+++ packages/base/src/Util.ts
@@ -5,14 +5,21 @@ export const Util = {
       : (value: any): boolean => {
           return toString.call(value) === "[object Array]";
         },
-
+  isArrayLike(val) {
+    try {
+      // @ts-ignore
+      return [...val], val.slice, true;
+    } catch (e) {
+      return false;
+    }
+  },
   clone<T>(obj: T): T {
     if (typeof obj !== "object" || obj === null) {
       return obj;
     }
     let rst;
-    if (Util.isArray(obj)) {
-      rst = [];
+    if (Util.isArrayLike(obj)) {
+      rst = (obj as any).slice();
       for (let i = 0, l = (obj as any).length; i < l; i++) {
         rst[i] = Util.clone(obj[i]);
       }
