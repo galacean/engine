7941f5a7230263fc3862a17cee04de35cce083d9
shensi <shensi.zxd@alibaba-inc.com>
2019-12-11 16:50:16 +0800

    refractor: 重构成纯数学库，删除 primitive 等相关方法

--- packages/bounding-info/src/AABB.ts
+++ packages/bounding-info/src/AABB.ts
@@ -1,18 +1,33 @@
 import { vec3 } from "@alipay/o3-math";
-import { Primitive } from "@alipay/o3-primitive";
-import { AMeshRenderer } from "@alipay/o3-mesh";
-import { AGeometryRenderer } from "@alipay/o3-geometry";
-import { Frustum } from "@alipay/o3-renderer-cull";
 import { IntersectInfo } from "@alipay/o3-base";
-import { pointDistanceToPlane, getMinMaxFromPrimitive } from "./util";
-import { Vec3, Mat4 } from "./type";
+import { Vec3, Vec4 } from "@alipay/o3-math/types/type";
+import { pointDistanceToPlane } from "./util";
 
 /**
  * 轴对齐的包围盒(Axis Aligned Bound Box)
  * */
 export class AABB {
-  public min: Vec3 = [0, 0, 0];
-  public max: Vec3 = [0, 0, 0];
+  public min: Vec3 = vec3.create();
+  public max: Vec3 = vec3.create();
+
+  /**
+   * AABB 的 min/max 基于世界坐标系，且不能通过 modelMatrix 缓存计算
+   * @param {Vec3} minWorld - 世界坐标系的最小坐标
+   * @param {Vec3} maxWorld - 世界坐标系的最大坐标
+   * */
+  constructor(minWorld: Vec3, maxWorld: Vec3) {
+    this.update(minWorld, maxWorld);
+  }
+
+  /**
+   * 更新 AABB
+   * @param {Vec3} minWorld - 世界坐标系的最小坐标
+   * @param {Vec3} maxWorld - 世界坐标系的最大坐标
+   * */
+  update(minWorld: Vec3, maxWorld: Vec3) {
+    vec3.copy(this.min, minWorld);
+    vec3.copy(this.max, maxWorld);
+  }
 
   /**
    * 使用中心点和 Size 的方式来计算 AABB 包围盒
@@ -28,63 +43,18 @@ export class AABB {
   }
 
   /**
-   * 通过 primitive 和模型矩阵来计算 AABB 包围盒
-   * @param {Primitive}  primitive - Oasis primitive
-   * @param {Mat4} modelMatrix - Local to World矩阵
-   * @param {boolean} littleEndian - 是否以小端字节序读取，默认true
-   * */
-  setFromPrimitive(primitive: Primitive, modelMatrix: Mat4, littleEndian = true) {
-    let { min, max } = getMinMaxFromPrimitive(primitive, modelMatrix, littleEndian);
-
-    this.min = min;
-    this.max = max;
-  }
-
-  /**
-   * 通过 AMeshRenderer 来计算 AABB 包围盒
-   * @param {AMeshRenderer} meshRenderer - Oasis AMeshRenderer
-   * @param {boolean} littleEndian - 是否以小端字节序读取，默认true
-   * */
-  setFromMeshRenderer(meshRenderer: AMeshRenderer, littleEndian = true) {
-    let {
-      mesh: { primitives },
-      node
-    } = meshRenderer;
-    let modelMatrix = node.getModelMatrix();
-
-    primitives.forEach(p => {
-      this.setFromPrimitive(p, modelMatrix, littleEndian);
-    });
-  }
-
-  /**
-   * 通过 AGeometryRenderer 来计算 AABB 包围盒
-   * @param {AGeometryRenderer} geometryRenderer - Oasis AGeometryRenderer
-   * @param {boolean} littleEndian - 是否以小端字节序读取，默认true
-   * */
-  setFromGeometryRenderer(geometryRenderer: AGeometryRenderer, littleEndian = true) {
-    let {
-      geometry: { primitive },
-      node
-    } = geometryRenderer;
-    let modelMatrix = node.getModelMatrix();
-    this.setFromPrimitive(primitive, modelMatrix, littleEndian);
-  }
-
-  /**
    * 获取与视锥体的 具体相交状态
-   * @param {Frustum} frustum - Oasis 视锥体
+   * @param { Vec4[] } frustumPlanes - Oasis 视锥体的6个平面方程
    * @return {IntersectInfo} 返回相交状态
    * */
-  intersectsFrustum(frustum: Frustum): IntersectInfo {
-    const planes = frustum.planes;
+  intersectsFrustum(frustumPlanes: Vec4[]): IntersectInfo {
     const min = this.min;
     const max = this.max;
     const p1 = [],
       p2 = [];
 
     for (let i = 0; i < 6; i++) {
-      const plane = planes[i];
+      const plane = frustumPlanes[i];
       p1[0] = plane[0] > 0 ? min[0] : max[0];
       p2[0] = plane[0] > 0 ? max[0] : min[0];
       p1[1] = plane[1] > 0 ? min[1] : max[1];
@@ -109,18 +79,17 @@ export class AABB {
   }
 
   /**
-   * 是否在视锥体内部
-   * @param {Frustum} frustum - Oasis 视锥体
+   * 是否在视锥体内部（包含或者交叉）
+   * @param { Vec4[] } frustumPlanes -  Oasis 视锥体的6个平面方程
    * @return {boolean}
    * */
-  isInFrustum(frustum: Frustum): boolean {
-    const planes = frustum.planes;
+  isInFrustum(frustumPlanes: Vec4[]): boolean {
     const min = this.min;
     const max = this.max;
     const p = [];
 
     for (let i = 0; i < 6; i++) {
-      const plane = planes[i];
+      const plane = frustumPlanes[i];
       p[0] = plane[0] > 0 ? max[0] : min[0];
       p[1] = plane[1] > 0 ? max[1] : min[1];
       p[2] = plane[2] > 0 ? max[2] : min[2];
--- packages/bounding-info/src/OBB.ts
+++ packages/bounding-info/src/OBB.ts
@@ -1,27 +1,38 @@
 import { vec3 } from "@alipay/o3-math";
-import { Primitive } from "@alipay/o3-primitive";
-import { AMeshRenderer } from "@alipay/o3-mesh";
-import { AGeometryRenderer } from "@alipay/o3-geometry";
-import { Frustum } from "@alipay/o3-renderer-cull";
 import { IntersectInfo } from "@alipay/o3-base";
-import { getMinMaxFromPrimitive, pointDistanceToPlane } from "./util";
-import { Mat4, Vec3 } from "./type";
+import { Mat4, Vec3, Vec4 } from "@alipay/o3-math/types/type";
+import { pointDistanceToPlane } from "./util";
 
 /**
  * 方向包围盒(Oriented Bounding Box)
  * */
 export class OBB {
   /** 本地坐标系 */
-  public min: Vec3 = [0, 0, 0];
-  public max: Vec3 = [0, 0, 0];
+  public min: Vec3 = vec3.create();
+  public max: Vec3 = vec3.create();
   public corners: Vec3[] = [];
   /** 世界坐标系 */
-  public minWorld: Vec3 = [0, 0, 0];
-  public maxWorld: Vec3 = [0, 0, 0];
+  public minWorld: Vec3 = vec3.create();
+  public maxWorld: Vec3 = vec3.create();
   public cornersWorld: Vec3[] = [];
 
   /**
-   * 根据 min/max 取得八个顶点的位置
+   * 初始化 OBB, 之后可以通过 modelMatrix 缓存计算
+   * @param {Vec3} minLocal - 本地坐标系的最小坐标
+   * @param {Vec3} maxLocal - 本地坐标系的最大坐标
+   * @param {Mat4} modelMatrix - Local to World矩阵
+   * */
+  constructor(minLocal: Vec3, maxLocal: Vec3, modelMatrix: Mat4) {
+    vec3.copy(this.min, minLocal);
+    vec3.copy(this.max, maxLocal);
+    this.corners = this.getCornersFromMinMax(minLocal, maxLocal);
+
+    // world
+    this.updateByModelMatrix(modelMatrix);
+  }
+
+  /**
+   * 根据 min/max ,取得八个顶点的位置
    * @param {Vec3} min - 最小坐标
    * @param {Vec3} max - 最大坐标
    */
@@ -46,55 +57,6 @@ export class OBB {
   }
 
   /**
-   * 通过 primitive 和模型矩阵来计算 OBB 包围盒
-   * @param {Primitive}  primitive - Oasis primitive
-   * @param {Mat4} modelMatrix - Local to World矩阵
-   * @param {boolean} littleEndian - 是否以小端字节序读取，默认true
-   * */
-  setFromPrimitive(primitive: Primitive, modelMatrix: Mat4, littleEndian = true) {
-    let { min, max } = getMinMaxFromPrimitive(primitive, null, littleEndian);
-
-    // local
-    this.min = min;
-    this.max = max;
-    this.corners = this.getCornersFromMinMax(min, max);
-
-    // world
-    this.updateByModelMatrix(modelMatrix);
-  }
-
-  /**
-   * 通过 AMeshRenderer 来计算 OBB 包围盒
-   * @param {AMeshRenderer} meshRenderer - Oasis AMeshRenderer
-   * @param {boolean} littleEndian - 是否以小端字节序读取，默认true
-   * */
-  setFromMeshRenderer(meshRenderer: AMeshRenderer, littleEndian = true) {
-    let {
-      mesh: { primitives },
-      node
-    } = meshRenderer;
-    let modelMatrix = node.getModelMatrix();
-
-    primitives.forEach(p => {
-      this.setFromPrimitive(p, modelMatrix, littleEndian);
-    });
-  }
-
-  /**
-   * 通过 AGeometryRenderer 来计算 OBB 包围盒
-   * @param {AGeometryRenderer} geometryRenderer - Oasis AGeometryRenderer
-   * @param {boolean} littleEndian - 是否以小端字节序读取，默认true
-   * */
-  setFromGeometryRenderer(geometryRenderer: AGeometryRenderer, littleEndian = true) {
-    let {
-      geometry: { primitive },
-      node
-    } = geometryRenderer;
-    let modelMatrix = node.getModelMatrix();
-    this.setFromPrimitive(primitive, modelMatrix, littleEndian);
-  }
-
-  /**
    * 通过模型矩阵，和缓存的本地坐标系 OBB，获取新的世界坐标系 OBB
    * @param {Mat4} modelMatrix - Local to World矩阵
    * */
@@ -115,15 +77,14 @@ export class OBB {
 
   /**
    * 获取与视锥体的 具体相交状态
-   * @param {Frustum} frustum - Oasis 视锥体
+   * @param { Vec4[] } frustumPlanes - Oasis 视锥体的6个平面方程
    * @return {IntersectInfo} 返回相交状态
    * */
-  intersectsFrustum(frustum: Frustum): IntersectInfo {
-    const planes = frustum.planes;
+  intersectsFrustum(frustumPlanes: Vec4[]): IntersectInfo {
     const cornersWorld = this.cornersWorld;
 
     for (let i = 0; i < 6; i++) {
-      const plane = planes[i];
+      const plane = frustumPlanes[i];
       let isInPlane = false;
       for (let j = 0; j < 8; j++) {
         if (pointDistanceToPlane(plane, cornersWorld[j]) > 0) {
@@ -141,16 +102,15 @@ export class OBB {
   }
 
   /**
-   * 是否在视锥体内部
-   * @param {Frustum} frustum - Oasis 视锥体
+   * 是否在视锥体内部（包含或者交叉）
+   * @param { Vec4[] } frustumPlanes - Oasis 视锥体的6个平面方程
    * @return {boolean}
    * */
-  isInFrustum(frustum: Frustum): boolean {
-    const planes = frustum.planes;
+  isInFrustum(frustumPlanes: Vec4[]): boolean {
     const cornersWorld = this.cornersWorld;
 
     for (let i = 0; i < 6; i++) {
-      const plane = planes[i];
+      const plane = frustumPlanes[i];
       let isInPlane = false;
       for (let j = 0; j < 8; j++) {
         if (pointDistanceToPlane(plane, cornersWorld[j]) > 0) {
