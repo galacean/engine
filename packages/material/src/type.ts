a306314968a78c1bf9247b572c6902c288f4f0f2
shensi <shensi.zxd@alibaba-inc.com>
2019-12-30 17:08:58 +0800

    refactor: 删除冗余 API

--- packages/material/src/type.ts
+++ packages/material/src/type.ts
@@ -7,14 +7,29 @@ export type TextureConfig = {
   wrapT?: TextureWrapMode;
   flipY?: boolean;
   premultiplyAlpha?: boolean;
+  /** 纹理 U 方向的缩放 */
   uScale?: number;
+  /** 纹理 V 方向的缩放 */
   vScale?: number;
+  /** 纹理 U 方向的偏移 */
   uOffset?: number;
+  /** 纹理 V 方向的偏移 */
   vOffset?: number;
+  /** 纹理旋转弧度，0～2PI */
   uvRotation?: number;
+  /** 纹理中心点 */
   uvCenter?: number[];
   [key: string]: any;
 };
+
+export type RenderTargetConfig = {
+  width?: number;
+  height?: number;
+  clearColor?;
+  enableDepthTexture?: boolean;
+  isCube?: boolean;
+};
+
 export type React = { x: number; y: number; width: number; height: number };
 
 export type TechniqueStates = {
