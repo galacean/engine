8606d341a033643f2ee95a3f250baec4efd1b8cd
shensi <shensi.zxd@alibaba-inc.com>
2020-01-07 14:44:48 +0800

    refactor: use mix

--- packages/shaderlib/src/shaderlib/refract_frag.glsl
+++ packages/shaderlib/src/shaderlib/refract_frag.glsl
@@ -1,10 +1,6 @@
 #ifdef HAS_PERTURBATIONMAP
 
   vec4 screenColor = texture2D(u_perturbationSampler, getScreenUv() + normalize(u_viewMat * vec4(normal, 1.)).xy * vec2(u_perturbationUOffset, u_perturbationVOffset));
-  if (gl_FragColor.a < 1.) {
-    // mock alpha blend
-    gl_FragColor *=  gl_FragColor.a;
-    gl_FragColor += (1.0 - gl_FragColor.a)  * screenColor;
-  }
+  gl_FragColor = mix(screenColor, gl_FragColor, gl_FragColor.a);
 
 #endif
