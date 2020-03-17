9a646c20fdffd5770cff63367d957d2c2a07c0d7
胡松 <kevin.hs@antfin.com>
2020-03-17 10:09:04 +0800

    chore: change SceneVisitior to abstract

--- packages/core/src/SceneVisitor.ts
+++ packages/core/src/SceneVisitor.ts
@@ -4,18 +4,16 @@ import { Node } from "./Node";
 /**
  * Scene 访问者接口
  */
-export class SceneVisitor {
+export abstract class SceneVisitor {
   /**
    * 接受一个 Node
    * @return {boolean} 返回 false，则忽略本节点及其所有子节点
    */
-  public acceptNode(node: Node): boolean {
-    return true;
-  }
+  public abstract acceptNode(node: Node): boolean;
 
   /**
    * 接受一个节点的组件
    * @param {NodeAbility} nodeAbility
    */
-  public acceptAbility(nodeAbility: NodeAbility) {}
+  public abstract acceptAbility(nodeAbility: NodeAbility);
 }
