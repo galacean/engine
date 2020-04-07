e76217a2b59ec6565da4f55e6ddd155f536a69bb
izzy.cz <izzy.cz@antfin.com>
2020-04-07 10:03:41 +0800

    fix: ci ignore path

--- jest.config.js
+++ jest.config.js
@@ -30,7 +30,7 @@ module.exports = {
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
     "/node_modules/",
-    "/playground/"
+    "playground/"
   ],
 
   // A list of reporter names that Jest uses when writing coverage reports
