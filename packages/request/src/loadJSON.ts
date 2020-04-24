aeb03d88c07f0112ae96ea95fd5524ab88287ac4
qianwei.xqw <qianwei.xqw@alibaba-inc.com>
2020-04-24 17:11:12 +0800

    增加图片和JSON的超时设置

--- packages/request/src/loadJSON.ts
+++ packages/request/src/loadJSON.ts
@@ -1,7 +1,19 @@
 import { loadText } from "./loadText";
 
-export function loadJSON(file, callback) {
+export function loadJSON(file, callback, timeout?: number) {
+  let timer;
+
+  if (timeout) {
+    timer = setTimeout(() => {
+      callback(new Error("JSON load timeout " + file), null);
+    }, timeout);
+  }
+
   loadText(file, function(err, data) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
     if (err) {
       callback(err, null);
     } else {
--- packages/schema-parser/src/resources/TextureResource.ts
+++ packages/schema-parser/src/resources/TextureResource.ts
@@ -1,20 +1,25 @@
 import { SchemaResource } from "./SchemaResource";
 import * as o3 from "@alipay/o3";
 import { AssetConfig } from "../types";
+import { Oasis } from "../Oasis";
 
 export class TextureResource extends SchemaResource {
-  load(resourceLoader: o3.ResourceLoader, assetConfig: AssetConfig): Promise<TextureResource> {
+  load(resourceLoader: o3.ResourceLoader, assetConfig: AssetConfig, oasis: Oasis): Promise<TextureResource> {
     return new Promise((resolve, reject) => {
       const resource = new o3.Resource(assetConfig.name, { type: assetConfig.type as any, url: assetConfig.url });
-      resourceLoader.load(resource, (err, res) => {
-        if (err) {
-          reject(err);
-        } else {
-          this._resource = res.asset;
-          this.setMeta();
-          resolve(this);
-        }
-      });
+      resourceLoader.load(
+        resource,
+        (err, res) => {
+          if (err) {
+            reject(err);
+          } else {
+            this._resource = res.asset;
+            this.setMeta();
+            resolve(this);
+          }
+        },
+        oasis.options.timeout
+      );
     });
   }
 
