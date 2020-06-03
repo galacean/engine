962b020b38df35eac311a41de0cb782e26ac9692
chenmo.gl <chenmo.gl@alibaba-inc.com>
2020-06-03 17:51:09 +0800

    change RenderTarget relative format.

--- packages/core/src/graphics/RenderTextureDepthFormat.ts
+++ packages/core/src/graphics/RenderTextureDepthFormat.ts
@@ -1,11 +1,22 @@
 /**
- * 渲染纹理深度格式枚举。
+ * 渲染缓冲深度格式枚举。
  */
-export enum RenderTextureDepthFormat {
-  /** 至少16位深度缓冲，无模版缓冲。*/
-  Depth16 = 0,
-  /** 8位模板缓冲，无深度缓冲。*/
-  Stencil8 = 1,
-  /** 深度缓冲和模板缓冲。*/
-  DepthStencil = 2
+export enum RenderBufferDepthFormat {
+  /** 深度缓冲，自动选择最佳精度。 */
+  Depth = 0,
+  /** 深度模版缓冲，自动选择最佳精度。 */
+  DepthStencil = 1,
+  /** 模板缓冲。 */
+  Stencil = 2,
+
+  /** 16位深度缓冲。 */
+  Depth16 = 3,
+  /** 24位深度缓冲。 */
+  Depth24 = 4,
+  /** 32位深度缓冲。 */
+  Depth32 = 5,
+  /** 24位深度缓冲+8位模版缓冲。 */
+  Depth24Stencil8 = 6,
+  /** 32位深度缓冲+8位模版缓冲。 */
+  Depth32Stencil8 = 7
 }
