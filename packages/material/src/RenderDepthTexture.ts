cb029faf70d74ec78aec7e4457225c5883c471f2
shensi <shensi.zxd@alibaba-inc.com>
2020-06-08 11:39:47 +0800

    fix: disable fetch pixel from depth render target

--- packages/material/src/RenderDepthTexture.ts
+++ packages/material/src/RenderDepthTexture.ts
@@ -94,26 +94,6 @@ export class RenderDepthTexture extends Texture {
     this._height = height;
     this._format = format;
 
-  }
-
-  /**
-   * 根据指定区域获得像素颜色缓冲
-   * @param x - 区域起始X坐标
-   * @param y - 区域起始Y坐标
-   * @param width - 区域宽
-   * @param height - 区域高
-   * @param out - 颜色数据缓冲
-   * @param face - 如果是立方体纹理，可以选择读取第几个面
-   */
-  public getPixelsBuffer(
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-    out: ArrayBufferView,
-    face?: TextureCubeFace
-  ): void {
-    super._getPixelsBuffer(x, y, width, height, out, face);
     this._initMipmap();
     //todo: delete
     this.type = AssetType.Scene;
