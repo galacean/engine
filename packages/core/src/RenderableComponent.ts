2bbc97d5b354ee3f37e12037846e2914febc523a
wz84888 <wz84888@alibaba-inc.com>
2020-06-10 22:09:23 +0800

    optimiz code

---
+++ packages/core/src/RenderableComponent.ts
@@ -0,0 +1,37 @@
+import { ACamera } from "./ACamera";
+import { NodeAbility } from "./NodeAbility";
+import { vec3 } from "@alipay/o3-math";
+/**
+ * 渲染组件
+ */
+export class RenderableComponent extends NodeAbility {
+  _renderable: boolean = true;
+  /**
+   * @override
+   */
+  render(camera: ACamera): void {}
+
+  _onActive() {
+    super._onActive();
+    this.scene._componentsManager.addRenderer(this);
+  }
+
+  _onInActive() {
+    super._onInActive();
+    this.scene._componentsManager.removeRenderer(this);
+  }
+
+  _render(camera: ACamera) {
+    let culled = false;
+
+    // distance cull
+    if (this.cullDistanceSq > 0) {
+      const distanceSq = vec3.squaredDistance(camera.eyePos, this.node.worldPosition);
+      culled = this.cullDistanceSq < distanceSq;
+    }
+
+    if (!culled) {
+      this.render(camera);
+    }
+  }
+}
