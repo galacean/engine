f918f26bcc02cef40775265d4297005250946765
izzy.cz <izzy.cz@antfin.com>
2020-06-11 21:11:06 +0800

    feat: schema parser load compressed cubemap

--- packages/schema-parser/src/resources/TextureCubeMapResource.ts
+++ packages/schema-parser/src/resources/TextureCubeMapResource.ts
@@ -1,7 +1,16 @@
 import { SchemaResource } from "./SchemaResource";
 import * as o3 from "@alipay/o3";
-import { TextureResource } from "./TextureResource";
 import { AssetConfig } from "../types";
+import { Oasis } from "../Oasis";
+import _ from "lodash";
+
+const loadOrder = {
+  astc: 1,
+  s3tc: 2,
+  pvrtc: 3,
+  etc: 4,
+  etc1: 5
+};
 
 const imageOrderMap = {
   px: 0,
@@ -13,30 +22,49 @@ const imageOrderMap = {
 };
 
 export class TextureCubeMapResource extends SchemaResource {
-  load(resourceLoader: o3.ResourceLoader, assetConfig: AssetConfig): Promise<TextureCubeMapResource> {
+  load(resourceLoader: o3.ResourceLoader, assetConfig: AssetConfig, oasis: Oasis): Promise<TextureCubeMapResource> {
     return new Promise((resolve, reject) => {
       const imageUrls = [];
-      for (const key in assetConfig.props) {
-        if (assetConfig.props.hasOwnProperty(key)) {
-          const element = assetConfig.props[key];
-          imageUrls[imageOrderMap[key]] = element.url;
-        }
-      }
-      resourceLoader.load(
-        new o3.Resource(assetConfig.name, {
-          type: "cubemap",
-          urls: imageUrls
-        }),
-        (err, res) => {
-          if (err) {
-            reject(err);
-          } else {
-            this._resource = res.asset;
-            this.setMeta();
-            resolve(this);
+      let resource;
+      if (this.resourceManager.useCompressedTexture && assetConfig?.props?.compression?.compressions.length) {
+        const rhi = oasis.engine.getRHI(oasis.canvas);
+        const compressions = assetConfig.props.compression.compressions;
+        compressions.sort((a, b) => {
+          return loadOrder[a.type] - loadOrder[b.type];
+        });
+        for (let i = 0; i < compressions.length; i++) {
+          const compression = compressions[i];
+          if (compression.container === "ktx" && rhi.canIUse(o3.GLCapabilityType[compression.type])) {
+            _.forEach(compression.files, (image, key) => {
+              imageUrls[imageOrderMap[key]] = image.url;
+            });
+            resource = new o3.Resource(assetConfig.name, {
+              type: compression.container,
+              urls: imageUrls
+            });
+            break;
           }
         }
-      );
+      }
+      if (!resource) {
+        _.forEach(assetConfig.props.images, (image, key) => {
+          imageUrls[imageOrderMap[key]] = image.url;
+        });
+        resource = new o3.Resource(assetConfig.name, {
+          type: "cubemap",
+          urls: imageUrls
+        });
+      }
+
+      resourceLoader.load(resource, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          this._resource = res.asset;
+          this.setMeta();
+          resolve(this);
+        }
+      });
     });
   }
 
