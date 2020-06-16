f4c1ac5401bfdfa61d982c561380f2afc03d892b
shensi <shensi.zxd@alibaba-inc.com>
2020-06-16 20:55:22 +0800

    feat: Texture2D

--- packages/material/tests/Material-test.js
+++
@@ -1,8 +0,0 @@
-import { Material } from '../src/Material';
-
-describe("Material", () => {
-  it("It worked!", () => {
-    let material = new Material();
-    expect(material).to.be.an("object");
-  });
-});
