995ef1cfa372a98c5cf839f3d98287e1b9b3c780
chenmo.gl <chenmo.gl@alibaba-inc.com>
2020-06-18 13:43:38 +0800

    add. CM and. optimization code

--- packages/core/src/Transform.ts
+++ packages/core/src/Transform.ts
@@ -377,17 +377,18 @@ export class Transform extends NodeAbility {
   }
 
   /**
-   * 在指定的方向和距离上位移
+   * 在指定的方向和距离上位移。
    * @param translation - 位移的方向和距离
    * @param relativeToLocal - 是否相对局部空间
    */
   translate(translation: vec3Type, relativeToLocal: boolean = true): void {
     if (relativeToLocal) {
-      mat4.fromQuat(Transform._tempMat4, this.rotationQuaternion);
-      translation = vec3.transformMat4(Transform._tempVec3, translation, Transform._tempMat4);
+      const rotationMat = Transform._tempMat4;
+      mat4.fromQuat(rotationMat, this.rotationQuaternion);
+      translation = vec3.transformMat4(Transform._tempVec3, translation, rotationMat);
       this.position = vec3.add(this._position, this._position, translation);
     } else {
-      vec3.add(this.worldPosition, translation, this._worldPosition);
+      vec3.add(this.worldPosition, translation, this._worldPosition); //CM:这样触发不了worldPosition的set属性，有BUG
     }
   }
 
