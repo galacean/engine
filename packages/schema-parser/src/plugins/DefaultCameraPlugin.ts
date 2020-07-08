f95b71197e0acc82a5a6d765bd97c806243140b9
胡松 <kevin.hs@antfin.com>
2020-07-08 18:35:02 +0800

    chore: schema parser compatible

--- packages/schema-parser/src/plugins/DefaultCameraPlugin.ts
+++ packages/schema-parser/src/plugins/DefaultCameraPlugin.ts
@@ -2,10 +2,12 @@ import { Camera, OldCamera, vec3 } from "@alipay/o3";
 import { Plugin } from "./Plugin";
 export const defaultCameraPlugin: Plugin = (oasis) => {
   const position = [];
+  const rotation = [];
   return {
     beforeAbilityAdded: (conf) => {
       if (conf.type === "Camera") {
         vec3.copy(position, oasis.options.config.nodes[conf.node].position);
+        vec3.copy(rotation, oasis.options.config.nodes[conf.node].rotation);
       }
     },
     abilityAdded: (ability) => {
@@ -13,6 +15,7 @@ export const defaultCameraPlugin: Plugin = (oasis) => {
         ability.attachToScene(oasis.canvas, oasis.options.rhiAttr);
         if (ability instanceof Camera) {
           ability.node.transform.position = position;
+          ability.node.transform.rotation = rotation;
         }
       }
     }
