c9a2d053344eb1f74fcf5d4b42fa7d88549ad5b7
胡松 <kevin.hs@antfin.com>
2020-07-14 20:33:23 +0800

    chore: loader implemention

--- packages/core/src/AssetDesign/Loader.ts
+++ packages/core/src/AssetDesign/Loader.ts
@@ -1,6 +1,9 @@
 import { LoadItem } from "./LoadItem";
 import { AssetPromise } from "./AssetPromise";
+import { RequestConfig, request } from "./request";
+import { ResourceManager } from "./ResourceManager";
 
-export interface Loader<T> {
-  load(item: LoadItem): AssetPromise<T>;
+export abstract class Loader<T> {
+  request: <U>(url: string, config: RequestConfig) => AssetPromise<U> = request;
+  abstract load(item: LoadItem, resouceManager: ResourceManager): AssetPromise<T>;
 }
