27ef7fbd94da66e58f79898443b61025e13f9ea5
chenmo.gl <chenmo.gl@alibaba-inc.com>
2020-07-16 15:49:08 +0800

    fix  comment

--- packages/rhi-webgl/src/WebCanvas.ts
+++ packages/rhi-webgl/src/WebCanvas.ts
@@ -1,6 +1,6 @@
 import { Canvas } from "@alipay/o3-core";
 export class WebCanvas implements Canvas {
-  _htmlCanvas: HTMLCanvasElement | OffscreenCanvas;
+  _webCanvas: HTMLCanvasElement | OffscreenCanvas;
 
   private _width: number;
   private _height: number;
@@ -12,6 +12,13 @@ export class WebCanvas implements Canvas {
     return this._width;
   }
 
+  set width(value: number) {
+    if (this._width !== value) {
+      this._webCanvas.width = value;
+      this._width = value;
+    }
+  }
+
   /**
    * @inheritdoc
    */
@@ -19,27 +26,36 @@ export class WebCanvas implements Canvas {
     return this._height;
   }
 
+  set height(value: number) {
+    if (this._height !== value) {
+      this._webCanvas.height = value;
+      this._height = value;
+    }
+  }
+
   /**
    * 创建Web画布。
-   * @param htmlCanvas 画布。
+   * @param webCanvas 画布。
    */
-  constructor(htmlCanvas: HTMLCanvasElement | OffscreenCanvas) {
-    if (htmlCanvas instanceof HTMLCanvasElement) {
-      htmlCanvas.width = htmlCanvas.clientWidth;
-      htmlCanvas.height = htmlCanvas.clientHeight;
+  constructor(webCanvas: HTMLCanvasElement | OffscreenCanvas) {
+    if (webCanvas instanceof HTMLCanvasElement) {
+      webCanvas.width = webCanvas.clientWidth;
+      webCanvas.height = webCanvas.clientHeight;
     }
-    this._width = htmlCanvas.width;
-    this._height = htmlCanvas.height;
-    this._htmlCanvas = htmlCanvas;
+    this._width = webCanvas.width;
+    this._height = webCanvas.height;
+    this._webCanvas = webCanvas;
   }
 
   /**
    * @inheritdoc
    */
   setResolution(width: number, height: number): void {
-    this._width = width;
-    this._height = height;
-    this._htmlCanvas.width = width;
-    this._htmlCanvas.height = height;
+    if (this._width !== width || this._height !== height) {
+      this._webCanvas.width = width;
+      this._webCanvas.height = height;
+      this._width = width;
+      this._height = height;
+    }
   }
 }
