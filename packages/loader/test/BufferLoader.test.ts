e1f2c84c4c1d9fbd101e4a74bf046ebda5a2920d
胡松 <kevin.hs@antfin.com>
2020-07-20 15:59:30 +0800

    chore: mock test

--- packages/loader/test/BufferLoader.test.ts
+++ packages/loader/test/BufferLoader.test.ts
@@ -1,12 +1,27 @@
 import "../src/BufferLoader";
-import { Engine } from "@alipay/o3-core";
+import { Engine, LoaderType } from "@alipay/o3-core";
 
 describe("text loader test", () => {
-  it("text loader test", () => {
-    const engine = new Engine();
-    const promise = engine.resourceManager.load(
-      "https://gw.alipayobjects.com/os/bmw-prod/ebe92dc1-3074-42d6-90c8-6255552c3c6e.bin"
-    );
-    return expect(promise).resolves.toEqual(Float32Array.from([1, 2, 3]).buffer);
-  });
+  // const engine = new Engine(new WebCanvas);
+  // it("test loader test", () => {
+  //   const promise = engine.resourceManager
+  //     .load<ArrayBuffer>("https://gw.alipayobjects.com/os/bmw-prod/ebe92dc1-3074-42d6-90c8-6255552c3c6e.bin")
+  //     .then((res) => {
+  //       return new Float32Array(res);
+  //     });
+  //   return expect(promise).resolves.toEqual(Float32Array.from([1, 2, 3]));
+  // });
+  // it("test base64 loader", () => {
+  //   const promise = engine.resourceManager
+  //     .load<ArrayBuffer>({
+  //       url: "data:application/octet-stream;base64,AAABAAIAAAAAAAAAAAAAAAAAAAAAAIA/AAAAAAAAAAAAAAAAAACAPwAAAAA=",
+  //       type: LoaderType.Buffer
+  //     })
+  //     .then((res) => {
+  //       return new Float32Array(res);
+  //     });
+  //   return expect(promise).resolves.toEqual(
+  //     Float32Array.from([9.183549615799121e-41, 2.802596928649634e-45, 0, 0, 0, 1, 0, 0, 0, 1, 0])
+  //   );
+  // });
 });
