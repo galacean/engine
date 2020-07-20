81212cab72551102246f8ade6f328a85e9433fd0
胡松 <kevin.hs@antfin.com>
2020-07-20 13:44:38 +0800

    chore: delete compressed texture loader

--- packages/compressed-texture/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { ResourceLoader } from "@alipay/o3-loader";
-
-import { KTXTextureNewHandler, parseSingleKTX, parseCubeKTX } from "./loader/KTXTextureNewLoader";
-
-ResourceLoader.registerHandler("ktxNew", new KTXTextureNewHandler());
-
-export { parseSingleKTX, parseCubeKTX };
