2ea47f963683aeed21fa0829ca700c5e03c40e44
shensi.zxd <shensi.zxd@alibaba-inc.com>
2020-07-24 15:12:45 +0800

    chore:  fix jest

--- packages/material/tests/RenderColorTexture.gl.test.ts
+++ packages/material/tests/RenderColorTexture.gl.test.ts
@@ -1,6 +1,7 @@
+import { RenderBufferColorFormat } from "@alipay/o3-base";
+import { Engine } from "@alipay/o3-core";
 import gl from "gl";
 import { RenderColorTexture } from "../src/RenderColorTexture";
-import { RenderBufferColorFormat } from "@alipay/o3-base";
 
 describe("RenderColorTexture", () => {
   const width = 1024;
@@ -9,19 +10,22 @@ describe("RenderColorTexture", () => {
     gl: gl(width, height),
     canIUse: jest.fn().mockReturnValue(true)
   };
-
+  // mock engine
+  Engine.defaultCreateObjectEngine = <any>{
+    _hardwareRenderer: rhi
+  };
   beforeEach(() => {
     rhi.isWebGL2 = false;
     delete rhi.gl.texStorage2D;
   });
 
   it("cube", () => {
-    const texture = new RenderColorTexture(rhi, width, height);
-    const cubeTexture = new RenderColorTexture(rhi, width, height, undefined, undefined, true);
+    const texture = new RenderColorTexture(width, height);
+    const cubeTexture = new RenderColorTexture(width, height, undefined, undefined, true);
     expect(texture._isCube).toBe(false);
     expect(cubeTexture._isCube).toBe(true);
     expect(() => {
-      new RenderColorTexture(rhi, 100, 200, undefined, undefined, true);
+      new RenderColorTexture(100, 200, undefined, undefined, true);
     }).toThrow();
   });
 
@@ -29,18 +33,18 @@ describe("RenderColorTexture", () => {
     it("不支持浮点纹理", () => {
       expect(() => {
         rhi.canIUse.mockReturnValueOnce(false);
-        new RenderColorTexture(rhi, width, height, RenderBufferColorFormat.R32G32B32A32);
+        new RenderColorTexture(width, height, RenderBufferColorFormat.R32G32B32A32);
       }).toThrow();
     });
     it("不支持半浮点纹理", () => {
       expect(() => {
         rhi.canIUse.mockReturnValueOnce(false);
-        new RenderColorTexture(rhi, width, height, RenderBufferColorFormat.R16G16B16A16);
+        new RenderColorTexture(width, height, RenderBufferColorFormat.R16G16B16A16);
       }).toThrow();
     });
     it("引擎不支持的格式", () => {
       expect(() => {
-        new RenderColorTexture(rhi, width, height, 1234567);
+        new RenderColorTexture(width, height, 1234567);
       }).toThrow();
     });
   });
@@ -50,15 +54,15 @@ describe("RenderColorTexture", () => {
       rhi.isWebGL2 = true;
       rhi.gl.texStorage2D = function () {};
 
-      const texture = new RenderColorTexture(rhi, 100, 100, undefined, true);
+      const texture = new RenderColorTexture(100, 100, undefined, true);
       expect(texture.mipmapCount).not.toBe(1);
     });
     it("关闭 mipmap 成功", () => {
-      const texture = new RenderColorTexture(rhi, width, height, undefined, false);
+      const texture = new RenderColorTexture(width, height, undefined, false);
       expect(texture.mipmapCount).toBe(1);
     });
     it("webgl1 开启 mipmap 失败自动降级 - 非2次幂图片", () => {
-      const texture = new RenderColorTexture(rhi, 100, 100, undefined, true);
+      const texture = new RenderColorTexture(100, 100, undefined, true);
       expect(texture.mipmapCount).toBe(1);
     });
   });
