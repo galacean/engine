28bd1e786a4efbc96136ec83a4911755d148c726
曾欣欣 <chengkong.zxx@alibaba-inc.com>
2020-07-30 20:33:13 +0800

    modify: 数学库build错误修复

--- packages/math/src/Spherical.ts
+++ packages/math/src/Spherical.ts
@@ -1,7 +1,7 @@
-import { vec3, MathUtil } from "./index";
+import { Vector3, MathUtil } from "./index";
 
 // 防止万向锁
-const ESP = MathUtil.EPSILON;
+const ESP = MathUtil.ZeroTolerance;
 
 // 球面坐标
 export class Spherical {
@@ -28,25 +28,25 @@ export class Spherical {
     return this;
   }
 
-  setFromVec3(v3) {
-    this.radius = vec3.len(v3);
+  setFromVec3(v3: Vector3) {
+    this.radius = v3.length();
     if (this.radius === 0) {
       this.theta = 0;
       this.phi = 0;
     } else {
-      this.theta = Math.atan2(v3[0], v3[2]);
-      this.phi = Math.acos(MathUtil.clamp(v3[1] / this.radius, -1, 1));
+      this.theta = Math.atan2(v3.x, v3.z);
+      this.phi = Math.acos(MathUtil.clamp(v3.y / this.radius, -1, 1));
     }
 
     return this;
   }
 
-  setToVec3(v3) {
+  setToVec3(v3: Vector3) {
     const sinPhiRadius = Math.sin(this.phi) * this.radius;
 
-    v3[0] = sinPhiRadius * Math.sin(this.theta);
-    v3[1] = Math.cos(this.phi) * this.radius;
-    v3[2] = sinPhiRadius * Math.cos(this.theta);
+    v3.x = sinPhiRadius * Math.sin(this.theta);
+    v3.y = Math.cos(this.phi) * this.radius;
+    v3.z = sinPhiRadius * Math.cos(this.theta);
 
     return this;
   }
