0260d870924fee2952973a95be0c75ffcaea154a
chenmo.gl <chenmo.gl@alibaba-inc.com>
2020-08-04 15:12:55 +0800

    fix: fix property sort

--- packages/core/src/asset/ResourceManager.ts
+++ packages/core/src/asset/ResourceManager.ts
@@ -27,6 +27,13 @@ export class ResourceManager {
     return this._extTypeMapping[url.substring(url.lastIndexOf(".") + 1)];
   }
 
+  /** 加载资产失败后的重试次数。*/
+  retryCount: number = 1;
+  /** 加载资产失败后的重试延迟时间，单位是毫秒(ms)。*/
+  retryInterval: number = 0;
+  /** 加载资产默认的超时时间，单位是毫秒(ms)。*/
+  timeout: number = 10000;
+
   /** 资产路径池,key为资产ID，值为资产路径，通过路径加载的资源均放入该池中，用于资源文件管理。*/
   private _assetPool: { [key: number]: string } = Object.create(null);
   /** 资产池,key为资产路径，值为资产，通过路径加载的资源均放入该池中，用于资产文件管理。*/
@@ -35,13 +42,6 @@ export class ResourceManager {
   private _referenceObjectPool: { [key: number]: ReferenceObject } = Object.create(null);
   /** 加载中的资源。*/
   private _loadingPromises: { [url: string]: AssetPromise<any> } = {};
-
-  /** 加载资产失败后的重试次数。*/
-  retryCount: number = 1;
-  /** 加载资产失败后的重试延迟时间，单位是毫秒(ms)。*/
-  retryInterval: number = 0;
-  /** 加载资产默认的超时时间，单位是毫秒(ms)。*/
-  timeout: number = 10000;
 
   /**
    * 创建资源管理员。
