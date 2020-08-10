417e032f846eeff7ec1c8e41111b080aadcfbf3e
胡松 <kevin.hs@antfin.com>
2020-08-10 14:19:50 +0800

    chore:  fix gltf model error

--- packages/schema-parser/src/plugins/DefaultCameraPlugin.ts
+++ packages/schema-parser/src/plugins/DefaultCameraPlugin.ts
@@ -5,19 +5,19 @@ export const defaultCameraPlugin: Plugin = (oasis) => {
   const rotation = [];
   return {
     beforeAbilityAdded: (conf) => {
-      if (conf.type === "Camera") {
-        vec3.copy(position, oasis.options.config.nodes[conf.node].position);
-        vec3.copy(rotation, oasis.options.config.nodes[conf.node].rotation);
-      }
+      // if (conf.type === "Camera") {
+      //   vec3.copy(position, oasis.options.config.nodes[conf.node].position);
+      //   vec3.copy(rotation, oasis.options.config.nodes[conf.node].rotation);
+      // }
     },
     abilityAdded: (ability) => {
-      if (ability instanceof Camera) {
-        // ability.attachToScene(oasis.canvas, oasis.options.rhiAttr);
-        if (ability instanceof Camera) {
-          ability.entity.transform.position = position;
-          ability.entity.transform.rotation = rotation;
-        }
-      }
+      // if (ability instanceof Camera) {
+      //   // ability.attachToScene(oasis.canvas, oasis.options.rhiAttr);
+      //   if (ability instanceof Camera) {
+      //     ability.entity.transform.position = position;
+      //     ability.entity.transform.rotation = rotation;
+      //   }
+      // }
     }
   };
 };
