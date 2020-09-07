106bb9fa2e255e45b97a9b4f794861a354e831dd
胡松 <kevin.hs@antfin.com>
2020-09-07 15:23:28 +0800

    chore: modify canvas width height setting

--- packages/adapter-miniprogram/src/register.ts
+++ packages/adapter-miniprogram/src/register.ts
@@ -2,25 +2,13 @@ import devicePixelRatio from "./devicePixelRatio";
 import * as Mixin from "./util/mixin";
 import document from "./document";
 
-declare let my: any;
-
 /**同步和异步都需要的数据*/
 let canvas: any = {};
 let canvas2D: any = {};
 
 /**异步注册3Dcanvas*/
-function registerCanvas(c, id: string) {
-  canvas = c;
+function registerCanvas(canvas, id: string) {
   canvas.id = id;
-  const { platform } = my.getSystemInfoSync();
-  // todo bug : 小程序 Android width/height 和 iOS 不一致
-  if (platform === "iOS") {
-    canvas.clientWidth = c.width / devicePixelRatio;
-    canvas.clientHeight = c.height / devicePixelRatio;
-  } else {
-    canvas.clientWidth = c.width;
-    canvas.clientHeight = c.height;
-  }
 
   if (!("tagName" in canvas)) {
     canvas.tagName = "CANVAS";
@@ -34,18 +22,18 @@ function registerCanvas(c, id: string) {
   Mixin.clientRegion(canvas);
   Mixin.offsetRegion(canvas);
 
-  canvas.focus = function() {};
-  canvas.blur = function() {};
+  canvas.focus = function () {};
+  canvas.blur = function () {};
 
-  canvas.addEventListener = function(type, listener, options = {}) {
+  canvas.addEventListener = function (type, listener, options = {}) {
     document.addEventListener(type, listener, options);
   };
 
-  canvas.removeEventListener = function(type, listener) {
+  canvas.removeEventListener = function (type, listener) {
     document.removeEventListener(type, listener);
   };
 
-  canvas.dispatchEvent = function(event: Event) {
+  canvas.dispatchEvent = function (event: Event) {
     document.dispatchEvent(event);
   };
 }
@@ -73,23 +61,23 @@ function registerCanvas2D(ctx, id: string) {
   Mixin.clientRegion(canvas2D);
   Mixin.offsetRegion(canvas2D);
 
-  canvas2D.getContext = function(type) {
+  canvas2D.getContext = function (type) {
     if (type === "2d") {
       return ctx;
     }
   };
-  canvas2D.focus = function() {};
-  canvas2D.blur = function() {};
+  canvas2D.focus = function () {};
+  canvas2D.blur = function () {};
 
-  canvas2D.addEventListener = function(type, listener, options = {}) {
+  canvas2D.addEventListener = function (type, listener, options = {}) {
     document.addEventListener(type, listener, options);
   };
 
-  canvas2D.removeEventListener = function(type, listener) {
+  canvas2D.removeEventListener = function (type, listener) {
     document.removeEventListener(type, listener);
   };
 
-  canvas2D.dispatchEvent = function(event: Event) {
+  canvas2D.dispatchEvent = function (event: Event) {
     document.dispatchEvent(event);
   };
 }
