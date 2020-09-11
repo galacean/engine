a062d1eb73cf4bbe40433dd9f03208e12b51db8b
chenmo.gl <chenmo.gl@alibaba-inc.com>
2020-09-11 21:59:55 +0800

    clear code

--- packages/core/src/geometry-shape/GeometryShape.ts
+++ packages/core/src/geometry-shape/GeometryShape.ts
@@ -1,14 +1,14 @@
-import { BufferGeometry } from "../geometry/BufferGeometry";
-
+import { Vector3 } from "@alipay/o3-math";
 import { Engine } from "../Engine";
+import { BufferGeometry } from "../geometry/BufferGeometry";
 import { BufferUsage } from "../geometry/graphic/enums/BufferUsage";
 import { IndexFormat } from "../geometry/graphic/enums/IndexFormat";
 import { VertexElementFormat } from "../geometry/graphic/enums/VertexElementFormat";
 import { IndexBuffer } from "../geometry/graphic/IndexBuffer";
 import { VertexBuffer } from "../geometry/graphic/VertexBuffer";
+import { VertexBufferBinding } from "../geometry/graphic/VertexBufferBinding";
 import { VertexDeclaration } from "../geometry/graphic/VertexDeclaration";
 import { VertexElement } from "../geometry/graphic/VertexElement";
-import { VertexBufferBinding } from "../geometry/graphic/VertexBufferBinding";
 
 /**
  * 基本形状几何体。
@@ -27,11 +27,10 @@ export class GeometryShape extends BufferGeometry {
       new VertexElement("TEXCOORD_0", 24, VertexElementFormat.Vector2, 0)
     ]);
 
-    this._init(engine, vertices, indices, vertexStride, declaration);
+    this._initBuffer(engine, vertices, indices, vertexStride, declaration);
   }
 
-  // TODO api命名待定
-  _init(
+  private _initBuffer(
     engine: Engine,
     vertices: Float32Array,
     indices: Uint16Array,
@@ -44,13 +43,49 @@ export class GeometryShape extends BufferGeometry {
 
     vertexBuffer.setData(vertices);
     this.primitive.setVertexDeclaration(declaration);
-    this.addVertexBuffer(new VertexBufferBinding(vertexBuffer, vertexStride), null);
+    this.addVertexBuffer(new VertexBufferBinding(vertexBuffer, vertexStride));
     this.vertexCount = vertexBufferlength / vertexStride;
 
     indexBuffer.setData(indices);
-    this.setIndexBuffer(indexBuffer, null);
+    this.setIndexBuffer(indexBuffer);
     this.indexCount = indices.length;
 
-    this._getMinMax(vertices);
+    this._computeBounds(vertices);
+  }
+
+  private _computeBounds(vertices: ArrayBuffer | Float32Array): void {
+    debugger;
+    const vertexElement = this.primitive.vertexAttributes["POSITION"];
+    const bufferIndex = vertexElement.vertexBufferSlot;
+    const stride = this.primitive.vertexBufferBindings[bufferIndex].stride;
+    const offset = vertexElement.offset;
+    const vertexCount = this.vertexCount;
+    let arrayBuffer: ArrayBuffer = vertices;
+    if (!(arrayBuffer instanceof ArrayBuffer)) {
+      arrayBuffer = (<Float32Array>arrayBuffer).buffer;
+    }
+    const dataView = new DataView(arrayBuffer, offset);
+
+    let min = new Vector3(Infinity, Infinity, Infinity);
+    let max = new Vector3(-Infinity, -Infinity, -Infinity);
+    for (let i = 0; i < vertexCount; i++) {
+      const base = offset + stride * i;
+      const position = new Vector3(
+        dataView.getFloat32(base, true),
+        dataView.getFloat32(base + 4, true),
+        dataView.getFloat32(base + 8, true)
+      );
+      Vector3.min(min, position, min);
+      Vector3.max(max, position, max);
+    }
+
+    let bounds = this.bounds;
+    if (bounds) {
+      min.cloneTo(bounds.min);
+      max.cloneTo(bounds.max);
+    } else {
+      bounds = { min: min, max: max };
+      this.bounds = bounds;
+    }
   }
 }
--- packages/core/src/geometry/BufferGeometry.ts
+++ packages/core/src/geometry/BufferGeometry.ts
@@ -1,9 +1,5 @@
-import { Matrix, Vector3 } from "@alipay/o3-math";
 import { AssetObject } from "../asset/AssetObject";
-import { TypedArray, UpdateType } from "../base/Constant";
 import { Primitive } from "../primitive/Primitive";
-import { DataMap, UpdateRangeMap, UpdateTypeMap } from "../primitive/type";
-import { BufferUtil } from "./graphic/BufferUtil";
 import { VertexBufferBinding } from "./graphic/VertexBufferBinding";
 import { VertexElements } from "./graphic/VertexElement";
 import { IndexBuffer } from "./index";
@@ -11,18 +7,29 @@ import { IndexBuffer } from "./index";
 let geometryCount = 0;
 
 /**
- * BufferGeometry 几何体创建类
- * @extends AssetObject
+ * 缓冲几何体。
  */
 export class BufferGeometry extends AssetObject {
   primitive: Primitive;
-  bounds: any = {};
-  private _bufferCount: number;
-  // private dataCache: DataMap = {};
-  // private updateTypeCache: UpdateTypeMap = {};
-  // private updateRangeCache: UpdateRangeMap = {};
 
-  get vertexCount() {
+  private _bufferCount: number;
+  private _bounds: any;
+
+  /**
+   * 包围体。
+   */
+  get bounds(): any {
+    return this._bounds;
+  }
+
+  set bounds(value: any) {
+    this._bounds = value;
+  }
+
+  /**
+   * 顶点数量。
+   */
+  get vertexCount(): number {
     return this.primitive.vertexCount;
   }
 
@@ -30,7 +37,10 @@ export class BufferGeometry extends AssetObject {
     this.primitive.vertexCount = value;
   }
 
-  get instancedCount() {
+  /**
+   * 实例数量。
+   */
+  get instancedCount(): number {
     return this.primitive.instancedCount;
   }
 
@@ -38,6 +48,9 @@ export class BufferGeometry extends AssetObject {
     this.primitive.instancedCount = count;
   }
 
+  /**
+   * 绘制模式。
+   */
   set mode(mode) {
     this.primitive.mode = mode;
   }
@@ -50,10 +63,16 @@ export class BufferGeometry extends AssetObject {
     return this.primitive.attributes;
   }
 
+  /**
+   * 索引缓冲。
+   */
   get indexBuffer(): IndexBuffer {
     return this.primitive.indexBuffer;
   }
 
+  /**
+   * 索引数量。
+   */
   set indexCount(v: number) {
     this.primitive.indexCount = v;
   }
@@ -63,8 +82,7 @@ export class BufferGeometry extends AssetObject {
   }
 
   /**
-   * @constructor
-   * @param {string} name 名称
+   * @param name - 名称
    */
   constructor(name?: string) {
     super();
@@ -74,274 +92,36 @@ export class BufferGeometry extends AssetObject {
   }
 
   /**
-   * 添加一个VB数据
+   * 添加一个顶点缓冲。
+   * @param vertexBuffer - 顶点缓冲
    */
-  addVertexBuffer(vertexBuffer: VertexBufferBinding) {
+  addVertexBuffer(vertexBuffer: VertexBufferBinding): void {
     this.primitive.addVertexBuffer(vertexBuffer);
-    // this.dataCache[this._bufferCount] = data;
-    // this.updateTypeCache[this._bufferCount] = UpdateType.INIT;
-    // this.updateRangeCache[this._bufferCount] = { offset: -1, end: -1, bufferOffset: -1 };
     this._bufferCount++;
   }
 
-  // /**
-  //  * 设置顶点数据
-  //  */
-  // setVertexData(semantic: string, data: TypedArray, offset: number = 0, dataIndex: number = 0, dataCount?: number) {
-  //   debugger;
-  //   const declaration = this.primitive.vertexDeclaration;
-  //   const element = this.primitive.vertexAttributes[semantic];
-  //   const bufferIndex = element.vertexBufferSlot;
-  //   const byteSize = BufferUtil._getVertexDataTypeSize(element._glElementInfo.type);
-  //   const dataOffset = element.offset / byteSize;
-  //   const totalSize = declaration.elements.map((item) => item._glElementInfo.size).reduce((a, b) => a + b);
-  //   const bufferOffset = offset * byteSize + element.offset;
-
-  //   if (dataCount !== undefined) {
-  //     data = data.slice(dataIndex, dataCount);
-  //   }
-  //   // if (declaration.elements.length > 1) {
-  //   //   const _offset = offset * totalSize + dataOffset;
-  //   //   this.dataCache[bufferIndex].set(data, _offset);
-  //   //   this._udpateInterleavedFlag(semantic, offset, byteSize, data.length);
-  //   // } else {
-  //   this.dataCache[bufferIndex].set(data, offset);
-  //   this._updateFlag(bufferIndex, offset, bufferOffset, data.length);
-  //   // }
-  // }
-
-  // resizeVertexBuffer(semantic: string, data: TypedArray) {
-  //   debugger;
-  //   const element = this.primitive.vertexAttributes[semantic];
-  //   const bufferIndex = element.vertexBufferSlot;
-  //   this.dataCache[bufferIndex] = data;
-  //   const vertexBufferBinding = this.primitive.vertexBufferBindings[bufferIndex];
-  //   const { buffer: vertexBuffer, stride } = vertexBufferBinding;
-
-  //   const byteSize = BufferUtil._getVertexDataTypeSize(element._glElementInfo.type);
-  //   vertexBuffer.resize(data.length * byteSize);
-  //   this.updateTypeCache[bufferIndex] = UpdateType.NO_UPDATE;
-  //   this.updateRangeCache[bufferIndex] = { offset: -1, end: -1, bufferOffset: -1 };
-  // }
-
-  setIndexBuffer(indexBuffer: IndexBuffer) {
+  /**
+   * 添加一个索引缓冲。
+   * @param indexBuffer - 索引缓冲
+   */
+  setIndexBuffer(indexBuffer: IndexBuffer): void {
     this.primitive.indexBuffer = indexBuffer;
-    // this.dataCache.index = data;
-    // this.updateTypeCache.index = UpdateType.INIT;
-    // this.updateRangeCache.index = { offset: -1, end: -1, bufferOffset: -1 };
   }
 
-  // setIndexData(data: TypedArray, offset: number = 0, dataIndex: number = 0, dataCount?: number) {
-  //   debugger;
-  //   if (dataCount !== undefined) {
-  //     data = data.slice(dataIndex, dataCount);
-  //   }
-  //   this.dataCache.index.set(data, offset);
-  //   this._updateFlag("index", offset, offset, data.length);
-  // }
-
-  // resizeIndexBuffer(data: Uint8Array | Uint16Array | Uint32Array) {
-  //   debugger;
-  //   this.dataCache.index = data;
-  //   const { indexBuffer } = this.primitive;
-  //   indexBuffer.resize(data.length);
-  //   this.updateTypeCache.index = UpdateType.NO_UPDATE;
-  //   this.updateRangeCache.index = { offset: -1, end: -1, bufferOffset: -1 };
-  // }
-
-  // getIndexData() {
-  //   debugger;
-  //   return this.dataCache.index;
-  // }
-
-  // getVertexData(semantic) {
-  //   debugger;
-  //   const bufferIndex = this.primitive.vertexAttributes[semantic].vertexBufferSlot;
-  //   return this.dataCache[bufferIndex];
-  // }
-
-  // /**
-  //  * 通过 primitive 计算本地/世界坐标系的 min/max
-  //  * @param - modelMatrix - Local to World矩阵,如果传此值，则计算min/max时将考虑RTS变换，如果不传，则计算local min/max
-  //  * @param - littleEndian - 是否以小端字节序读取，默认true
-  //  * */
-  // _getMinMax(data: ArrayBuffer | Float32Array, littleEndian = true): any {
-  //   const vertexElement = this.primitive.vertexAttributes["POSITION"];
-  //   const bufferIndex = vertexElement.vertexBufferSlot;
-  //   const stride = this.primitive.vertexBufferBindings[bufferIndex].stride;
-  //   const offset = vertexElement.offset;
-  //   const vertexCount = this.vertexCount;
-  //   let arrayBuffer: TypedArray | ArrayBuffer = this.dataCache[bufferIndex] || data;
-  //   if (!(arrayBuffer instanceof ArrayBuffer)) {
-  //     arrayBuffer = arrayBuffer.buffer;
-  //   }
-  //   const dataView = new DataView(arrayBuffer, offset);
-
-  //   let min = new Vector3(Infinity, Infinity, Infinity);
-  //   let max = new Vector3(-Infinity, -Infinity, -Infinity);
-  //   for (let i = 0; i < vertexCount; i++) {
-  //     const base = offset + stride * i;
-  //     const position = new Vector3(
-  //       dataView.getFloat32(base, littleEndian),
-  //       dataView.getFloat32(base + 4, littleEndian),
-  //       dataView.getFloat32(base + 8, littleEndian)
-  //     );
-  //     Vector3.min(min, position, min);
-  //     Vector3.max(max, position, max);
-  //   }
-
-  //   this.bounds.min = min;
-  //   this.bounds.max = max;
-  // }
-
-  // getMinMax(modelMatrix?: Matrix): any {
-  //   const min = new Vector3();
-  //   const max = new Vector3();
-  //   if (modelMatrix) {
-  //     Vector3.transformCoordinate(this.bounds.min, modelMatrix, min);
-  //     Vector3.transformCoordinate(this.bounds.max, modelMatrix, max);
-  //   } else {
-  //     this.bounds.min.cloneTo(min);
-  //     this.bounds.max.cloneTo(max);
-  //   }
-  //   return {
-  //     min: min,
-  //     max: max
-  //   };
-  // }
-
   /**
-   * 释放内部资源对象
+   * 释放内部资源对象。
    */
-  destroy() {
+  destroy(): void {
     if (this.primitive) {
       this.primitive.destroy();
       this.primitive = null;
     }
   }
 
-  reset() {
+  reset(): void {
     this.primitive.reset();
     this._bufferCount = 0;
   }
 
-  _render(): void {
-    // this.prepareBuffers();
-  }
-
-  // /**
-  //  * 更新 VBO
-  //  */
-  // protected updateVertexBuffer(index: number, updateRange: any) {
-  //   const primitive = this.primitive;
-  //   const { vertexBufferBindings: vertexBuffers } = primitive;
-  //   const { bufferOffset, offset, end } = updateRange;
-  //   const data = this.dataCache[index];
-  //   const vertexBuffer = vertexBuffers[index].buffer;
-  //   if (offset === -1) {
-  //     vertexBuffer.setData(data);
-  //   } else {
-  //     vertexBuffer.setData(data, bufferOffset, offset, end - offset);
-  //   }
-  // }
-
-  // /**
-  //  * 更新 IBO
-  //  */
-  // protected updateIndexBuffer(updateRange: any) {
-  //   const { indexBuffer } = this.primitive;
-  //   const data = <Uint8Array | Uint16Array | Uint32Array>this.dataCache.index;
-  //   const { bufferOffset, offset, end } = updateRange;
-  //   if (offset === -1) {
-  //     indexBuffer.setData(data);
-  //   } else {
-  //     indexBuffer.setData(data, bufferOffset, offset, end - offset);
-  //   }
-  // }
-
-  // /**
-  //  * 初始化或更新 BufferObject
-  //  * */
-  // protected prepareBuffers() {
-  //   const vertexBuffer = this.primitive.vertexBufferBindings;
-  //   for (let i: number = 0, n: number = vertexBuffer.length; i < n; i++) {
-  //     this._handleUpdateVertex(i);
-  //   }
-  //   this._handleIndexUpdate();
-  // }
-
-  // private _handleUpdateVertex(bufferIndex: number) {
-  //   const updateType = this.updateTypeCache[bufferIndex];
-  //   switch (updateType) {
-  //     case UpdateType.NO_UPDATE:
-  //       break;
-  //     case UpdateType.UPDATE_RANGE:
-  //       const updateRange = this.updateRangeCache[bufferIndex];
-  //       this.updateVertexBuffer(bufferIndex, updateRange);
-  //       this.updateTypeCache[bufferIndex] = UpdateType.NO_UPDATE;
-  //       updateRange.bufferOffset = -1;
-  //       updateRange.offset = -1;
-  //       updateRange.end = -1;
-  //       break;
-  //   }
-  // }
-
-  // private _handleIndexUpdate() {
-  //   const { indexBuffer } = this.primitive;
-  //   const updateType = this.updateTypeCache.index;
-  //   const updateRange = this.updateRangeCache.index;
-  //   if (indexBuffer) {
-  //     switch (updateType) {
-  //       case UpdateType.NO_UPDATE:
-  //         break;
-  //       case UpdateType.UPDATE_RANGE:
-  //         this.updateIndexBuffer(updateRange);
-  //         this.updateTypeCache.index = UpdateType.NO_UPDATE;
-  //         this.updateRangeCache.index.bufferOffset = -1;
-  //         this.updateRangeCache.index.offset = -1;
-  //         this.updateRangeCache.index.end = -1;
-  //         break;
-  //     }
-  //   }
-  // }
-
-  // private _updateFlag(bufferIndex: number | string, offset: number, bufferOffset: number, dataLength: number) {
-  //   const updateRange = this.updateRangeCache[bufferIndex];
-  //   const updateTypeCache = this.updateTypeCache;
-  //   if (updateTypeCache[bufferIndex] === UpdateType.NO_UPDATE) {
-  //     updateRange.bufferOffset = bufferOffset;
-  //     updateRange.offset = offset;
-  //     updateRange.end = offset + dataLength;
-  //     updateTypeCache[bufferIndex] = UpdateType.UPDATE_RANGE;
-  //   } else if (updateTypeCache[bufferIndex] === UpdateType.UPDATE_RANGE) {
-  //     updateRange.bufferOffset = Math.min(updateRange.bufferOffset, bufferOffset);
-  //     updateRange.offset = Math.min(updateRange.offset, offset);
-  //     updateRange.end = Math.max(updateRange.end, offset + dataLength);
-  //   }
-  // }
-
-  // private _udpateInterleavedFlag(semantic: string, vertexIndex: number, byteSize: number, dataLength: number) {
-  //   const vertexElement = this.primitive.vertexAttributes[semantic];
-  //   const bufferIndex = vertexElement.vertexBufferSlot;
-  //   const vertexStride = this.primitive.vertexBufferBindings[bufferIndex].stride;
-  //   const offset = vertexElement.offset;
-  //   if (this.updateTypeCache[bufferIndex] === UpdateType.NO_UPDATE) {
-  //     this.updateRangeCache[bufferIndex].offset = (vertexIndex * vertexStride + offset) / byteSize;
-  //     this.updateRangeCache[bufferIndex].end = (vertexIndex * vertexStride + offset + byteSize * dataLength) / byteSize;
-  //     this.updateTypeCache[bufferIndex] = UpdateType.UPDATE_RANGE;
-  //   } else if (this.updateTypeCache[bufferIndex] === UpdateType.UPDATE_RANGE) {
-  //     const newRange = this._getUpdateRange(bufferIndex, (vertexIndex * vertexStride + offset) / byteSize, dataLength);
-  //     this.updateRangeCache[bufferIndex].offset = newRange.offset;
-  //     this.updateRangeCache[bufferIndex].end = newRange.end;
-  //   }
-  // }
-
-  // private _getUpdateRange(bufferIndex: number, offset: number, length: number) {
-  //   const updateRange = this.updateRangeCache[bufferIndex];
-  //   const rangeEnd1 = this.updateRangeCache[bufferIndex].end;
-  //   const _offset = Math.min(offset, updateRange.offset);
-  //   const rangeEnd2 = offset + length;
-  //   const _end = rangeEnd1 <= rangeEnd2 ? rangeEnd2 : rangeEnd1;
-  //   return { offset: _offset, end: _end };
-  // }
+  _render(): void {}
 }
