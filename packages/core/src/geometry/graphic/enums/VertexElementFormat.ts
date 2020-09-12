e18d270f1152179aed579db63c6a0dd69785f64a
chenmo.gl <chenmo.gl@alibaba-inc.com>
2020-09-13 00:53:02 +0800

    add more "VertexElementFormat", "unsigned" and "normalized" format. note:the design  principles is 4-byte alignment,can get better performance.

--- packages/core/src/geometry/graphic/enums/VertexElementFormat.ts
+++ packages/core/src/geometry/graphic/enums/VertexElementFormat.ts
@@ -2,18 +2,36 @@
  * 顶点元素格式。
  */
 export enum VertexElementFormat {
-  /** 单精度浮点数。*/
+  /** 32-bit 浮点数。*/
   Single,
-  /** 二维单精度浮点数。 */
+  /** 二维 32-bit 浮点数。*/
   Vector2,
-  /** 三维单精度浮点数。 */
+  /** 三维 32-bit 浮点数。*/
   Vector3,
-  /** 四维单精度浮点数。 */
+  /** 四维 32-bit 浮点数。*/
   Vector4,
-  /** 四维字节整型。 */
+  /** 四维 8-bit 整型,范围是 [-128,127]。*/
   Byte4,
-  /** 二维 Short 整型。 */
+  /** 四维 8-bit 无符号整型，范围是 [0,255]。*/
+  UByte4,
+  /** 四维 8-bit 归一化整型，范围是 [-1,1]。*/
+  NormalizedByte4,
+  /** 四维 8-bit 归一化无符号整型，范围是 [0,1]。*/
+  NormalizedUByte4,
+  /** 二维 16-bit 整型，范围是 [-32768, 32767]。*/
   Short2,
-  /** 四维 Short 整型。 */
-  Short4
+  /** 二维 16-bit 无符号整型，范围是 [0, 65535]。*/
+  UnsignedShort2,
+  /** 二维 16-bit 归一化整型，范围是 [-1, 1]。*/
+  NormalizedShort2,
+  /** 二维 16-bit 归一化无符号整型，范围是 [0, 1]。*/
+  NormalizedUShort2,
+  /** 四维 16-bit 整型，范围是 [-32768, 32767]。*/
+  Short4,
+  /** 四维 16-bit 无符号整型，范围是 [0, 65535]。*/
+  UShort4,
+  /** 四维 16-bit 归一化整型，范围是 [-1, 1]。*/
+  NormalizedShort4,
+  /** 四维 16-bit 归一化无符号整型，范围是 [0, 1]。*/
+  NormalizedUShort4
 }
