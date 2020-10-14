71a4de0fd89d98421d18f6147cafd8fc6fb74c84
胡松 <kevin.hs@antfin.com>
2020-10-14 18:32:08 +0800

    fix: material value set

--- packages/core/src/material/Material.ts
+++ packages/core/src/material/Material.ts
@@ -133,7 +133,7 @@ export class Material extends ReferenceObject {
     if ((this as any)._generateTechnique && oriIsTexture !== curIsTexture) {
       this._technique = null;
     }
-    if (value) {
+    if (value != null) {
       this._values[name] = value;
     } else {
       this.delValue(name);
