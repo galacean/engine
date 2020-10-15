0a0adca1fc0bcf22334c30657f0e4cc5c65c8502
胡松 <kevin.hs@antfin.com>
2020-10-15 14:26:52 +0800

    refactor: events and event dispatcher

--- packages/core/src/base/EventDispatcher.ts
+++ packages/core/src/base/EventDispatcher.ts
@@ -1,135 +1,190 @@
-import { Listener, Event } from "./Event";
 import { EngineObject } from "./EngineObject";
+import { Event } from "./Event";
 
 /**
  * 事件派发管理，可作为基类继承
  * @class
  */
 export class EventDispatcher extends EngineObject {
-  private _listeners: { [k: string]: Listener[] };
+  private _evts = Object.create(null);
+  private _evtCount = 0;
 
-  constructor() {
-    super();
-    this._listeners = {};
+  /**
+   * 判断是否有事件监听
+   * @param event 事件名
+   * @returns 返回是否有对应事件
+   */
+  hasEvent(event: string): boolean {
+    return this._evts[event] != null;
   }
 
   /**
-   * 添加对指定事件的监听
-   * @param {string} type
-   * @param {function} listener
+   * 返回注册的所有事件名。
+   * @returns 所有的事件名
    */
-  public addEventListener(type: string, listener: Listener): EventDispatcher {
-    const listeners = this._listeners;
+  eventNames(): string[] {
+    const names = [];
+    const events = this._evts;
+    if (this._evtCount === 0) return names;
 
-    if (listeners[type] === undefined) {
-      listeners[type] = [];
-    }
-
-    if (listeners[type].indexOf(listener) === -1) {
-      listeners[type].push(listener);
-    }
-
-    return this;
+    return names.concat(Object.getOwnPropertySymbols(events));
   }
 
   /**
-   * 指定事件发生时，只回调一次
-   * @param {string} type
-   * @param {function} listener
+   * 返回所有的监听函数。
+   * @param event 事件名
+   * @returns 注册的函数
    */
-  public once(type: string, listener: Listener): EventDispatcher {
-    listener.once = true;
-    this.addEventListener(type, listener);
+  listeners(event: string): Function[] {
+    const listeners = this._evts[event];
+    if (!listeners) return [];
+    if (listeners.fn) return [listeners.fn];
 
-    return this;
+    const l = listeners.length;
+    const fns = new Array(l);
+    for (let i = 0; i < l; i++) {
+      fns[i] = listeners[i].fn;
+    }
+    return fns;
   }
 
   /**
-   * 移除对指定事件的监听
-   * @param {string | number} type
-   * @param {function} listener
+   * 返回指定事件名的监听函数的数量。
+   * @param event 事件名
+   * @returns 监听函数的数量
    */
-  public removeEventListener(type: string | number, listener: Listener): EventDispatcher {
-    const listeners = this._listeners;
+  listenerCount(event: string): number {
+    const listeners = this._evts[event];
 
-    if (arguments.length === 0) {
-      this._listeners = {};
-      return this;
+    if (!listeners) return 0;
+    if (listeners.fn) return 1;
+    return listeners.length;
+  }
+
+  /**
+   * 派发指定事件名的事件。
+   * @param event 事件名
+   * @param data 数据
+   * @returns 派发事件是否成功
+   */
+  dispatch(event: string, data?: any): boolean {
+    if (!this._evts[event]) {
+      return false;
     }
 
-    if (arguments.length === 1) {
-      listeners[type] = [];
-      return this;
-    }
+    const listeners = this._evts[event];
 
-    if (arguments.length === 2) {
-      if (listeners[type]) {
-        const index = listeners[type].indexOf(listener);
-        listeners[type].splice(index, 1);
+    if (listeners.fn) {
+      if (listeners.once) this.removeEventListener(event, listeners.fn);
+      listeners.fn(data);
+    } else {
+      const l = listeners.length;
+      for (let i = 0; i < l; i++) {
+        if (listeners[i].once) this.removeEventListener(event, listeners[i].fn);
+        listeners[i].fn(data);
       }
-      return this;
     }
+    return true;
+  }
 
+  /**
+   * 添加监听函数
+   * @param event 事件名
+   * @param fn 函数
+   * @returns this
+   */
+  on(event: string, fn: Function): EventDispatcher {
+    return this.addEventListener(event, fn);
+  }
+
+  /**
+   * 添加一次性的监听函数
+   * @param event 事件名
+   * @param fn 函数
+   * @returns this
+   */
+  once(event: string, fn: Function): EventDispatcher {
+    return this.addEventListener(event, fn, true);
+  }
+
+  /**
+   * 添加指定事件名的监听函数。
+   * @deprecated 使用 on/once 替换
+   * @param event 事件名
+   * @param fn 函数
+   * @param once 是否是一次性监听
+   * @returns this
+   */
+  addEventListener(event: string, fn: Function, once?: boolean): EventDispatcher {
+    const listener = { fn, once };
+    const events = this._evts;
+    if (!events[event]) {
+      events[event] = listener;
+      this._evtCount++;
+    } else if (!events[event].fn) {
+      events[event].push(listener);
+    } else {
+      events[event] = [events[event], listener];
+    }
     return this;
   }
 
   /**
-   * 移除所有事件的监听
-   * @param {string} type
-   * @param {function} listener
+   * 移除指定事件名的事件监听。
+   * @param event 事件名
+   * @param fn 函数，若不传则删除所有对应的事件监听
    */
-  public removeAllEventListeners(): void {
-    this._listeners = {};
-  }
-
-  public hasEvent(type: string | number, listener?: Listener): boolean {
-    const listeners = this._listeners;
-
-    if (arguments.length === 1 && listeners[type] && listeners[type].length !== 0) {
-      return true;
+  removeEventListener(event: string, fn?: Function): EventDispatcher {
+    if (!this._evts[event]) return this;
+    if (!fn) {
+      this._clearEvent(event);
+      return this;
     }
 
-    if (listeners[type] && listeners[type].indexOf(listener) !== -1) {
-      return true;
-    }
+    const listeners = this._evts[event];
 
-    return false;
-  }
-
-  /**
-   * 触发事件
-   * @param {Event} event
-   */
-  public trigger(event: Event): EventDispatcher {
-    const listeners = this._listeners;
-    const listenersArray = listeners[event.type];
-    event.target = this;
-    if (listenersArray) {
-      // copy listeners into copied array
-      const copiedListeners = listenersArray.slice();
-
-      // then loop copied array instead of original listeners
-      for (let i = 0, l = copiedListeners.length; i !== l; ++i) {
-        const listener: Listener = copiedListeners[i];
-        listener.call(this, event);
-
-        if (listener.once) {
-          // remove this listener once called
-          this.removeEventListener(event.type, listener);
+    if (listeners.fn && listeners.fn === fn) {
+      this._clearEvent(event);
+    } else {
+      const index = listeners.indexOf(fn);
+      if (index > -1) {
+        const temp = listeners[listeners.length - 1];
+        listeners[index] = temp;
+        listeners.length--;
+        if (listeners.length === 1) {
+          this._evts[event] = listeners[0];
         }
       }
     }
-
-    if (event.bubbles) {
-      const parent = (this as any).parent;
-      if (parent && !event.propagationStopped) {
-        parent.trigger(event);
-      }
-    }
-
     return this;
   }
-}
 
-(EventDispatcher.prototype as any).on = EventDispatcher.prototype.addEventListener;
-(EventDispatcher.prototype as any).off = EventDispatcher.prototype.removeEventListener;
+  /**
+   * 移除所有的事件监听。
+   * @param event 事件名，若不传则删除所有事件
+   */
+  removeAllEventListeners(event?: string): void {
+    if (event) {
+      if (this._evts[event]) this._clearEvent(event);
+    } else {
+      this._evts = Object.create(null);
+      this._evtCount = 0;
+    }
+  }
+
+  /**
+   * @deprecated
+   * @param 事件
+   */
+  trigger(e: Event) {
+    this.dispatch(e.type as string, e.data);
+  }
+
+  private _clearEvent(event: string) {
+    if (--this._evtCount === 0) {
+      this._evts = Object.create(null);
+    } else {
+      delete this._evts[event];
+    }
+  }
+}
