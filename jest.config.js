32cb6e4145510459ef2c862183e841dfba465e40
胡松 <kevin.hs@antfin.com>
2020-10-21 14:37:52 +0800

    chore: test ci

--- jest.config.js
+++ jest.config.js
@@ -63,10 +63,10 @@ module.exports = {
   // maxWorkers: "50%",
 
   // An array of directory names to be searched recursively up from the requiring module's location
-  moduleDirectories: [
-    "node_modules",
-    "src"
-  ],
+  // moduleDirectories: [
+  //   "node_modules",
+  //   "src"
+  // ],
 
   // An array of file extensions your modules use
   // moduleFileExtensions: [
