5f28a14d66195f3337c8e8536826b2068febfb0b
chenmo.gl <chenmo.gl@alibaba-inc.com>
2020-11-09 22:29:57 +0800

    fix comments

--- packages/core/src/clone/CloneManager.ts
+++ packages/core/src/clone/CloneManager.ts
@@ -1,23 +1,25 @@
 import { CloneMode } from "./enums/CloneMode";
 
 /**
- * 属性装饰器，克隆时对属性进行忽略。
+ * 属性装饰器，克隆时对字段进行忽略。
  */
 export function ignoreClone(target: Object, propertyKey: string): void {
   CloneManager.registerCloneMode(target, propertyKey, CloneMode.Ignore);
 }
 
 /**
- * 属性装饰器，克隆时对属性进行赋值克隆。
- * @remarks 如果是值类型则会拷贝值，如果是引用类型则会拷贝其引用地址。
+ * 属性装饰器，克隆时对字段进行赋值克隆。
+ * @remarks 如果是基本类型则会拷贝值，如果是引用类型则会拷贝其引用地址。
  */
 export function assignmentClone(target: Object, propertyKey: string): void {
   CloneManager.registerCloneMode(target, propertyKey, CloneMode.Assignment);
 }
 
 /**
- * 属性装饰器，克隆时对属性进行浅克隆。
- * @remarks 如果为引用类型，则会保持独立并拷贝其所有字段，如果字段是值类型则会拷贝值，如果字段是引用类型则会拷贝其引用地址。
+ * 属性装饰器，克隆时对字段进行浅克隆。
+ * @remarks
+ * 适用于 Obect、Array 和 Class 类型。
+ * 保持引用独立并使用赋值的方式克隆内部所有字段（如果内部字段是基本类型则会拷贝值，如果内部字段是引用类型则会拷贝其引用地址）。
  */
 export function shallowClone(target: Object, propertyKey: string): void {
   CloneManager.registerCloneMode(target, propertyKey, CloneMode.Shallow);
@@ -25,9 +27,9 @@ export function shallowClone(target: Object, propertyKey: string): void {
 
 /**
  * 属性装饰器，克隆时对属性进行深克隆。
- * 深克隆,适用于 Obect、Array 和 Class 类型。
- * Class 会调用对象的 cloneTo() 实现克隆，需要对象实现 IClone 接口。
- * @remarks 如果深克隆过程遇到 ReferenceObject 则使用浅拷贝。
+ * @remarks
+ * 适用于 Obect、Array 和 Class 类型。
+ * 如果在深克隆过程中遇到 Class 则会调用对象的 cloneTo() 实现克隆，需要对象实现 IClone 接口。
  */
 export function deepClone(target: Object, propertyKey: string): void {
   CloneManager.registerCloneMode(target, propertyKey, CloneMode.Deep);
