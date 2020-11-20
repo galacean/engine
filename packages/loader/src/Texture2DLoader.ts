9af59dd9076bf5547660941446be6cdf727495aa
胡松 <kevin.hs@antfin.com>
2020-11-20 13:44:05 +0800

    fix: name undefined

--- packages/loader/src/Texture2DLoader.ts
+++ packages/loader/src/Texture2DLoader.ts
@@ -18,7 +18,6 @@ class Texture2DLoader extends Loader<Texture2D> {
       })
         .then((image) => {
           const texture = new Texture2D(resourceManager.engine, image.width, image.height);
-          texture.name = name;
           if (!texture._glTexture) return;
           texture.setImageSource(image);
           texture.generateMipmaps();
