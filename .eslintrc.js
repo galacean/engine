0f9346cddc9acf1dde6cdda06eaa79417b3aed79
gz65555 <gz65555@gmail.com>
2021-01-26 11:23:36 +0800

    doc: translate to english

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", //定义ESLint的解析器
-  extends: ["prettier/@typescript-eslint", "plugin:prettier/recommended"], //定义文件继承的子规范
-  plugins: ["@typescript-eslint"], //定义了该eslint文件所依赖的插件
+  parser: "@typescript-eslint/parser", // Eslint TypeScript Parser
+  extends: ["prettier/@typescript-eslint", "plugin:prettier/recommended"],
+  plugins: ["@typescript-eslint"],
   parserOptions: {
     ecmaVersion: 2019,
     sourceType: "module",
@@ -10,7 +10,6 @@ module.exports = {
     }
   },
   env: {
-    //指定代码的运行环境
     browser: true,
     node: true
   }
