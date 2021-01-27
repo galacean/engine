65822174da39156eb63747dba670fce8677dedb7
zhuxudong <callzhuxudong@163.com>
2021-01-27 21:48:44 +0800

    fix:  fix loader error

--- packages/loader/src/scene-loader/temp.compatible.ts
+++ packages/loader/src/scene-loader/temp.compatible.ts
@@ -1,20 +1,5 @@
 import { Color, Vector2, Vector3, Vector4 } from "@oasis-engine/math";
 
-// TODO  temp
-const _vec3Attribute = [
-  "color",
-  "center",
-  "size",
-  "__position",
-  "__positionRandomness",
-  "__color",
-  "__velocity",
-  "__velocityRandomness",
-  "__acceleration",
-  "__accelerationRandomness",
-  "_center"
-];
-
 /**
  * temp compa
  * @param config
@@ -25,7 +10,7 @@ export function compatibleToV2(config) {
   const assetKeys = Object.keys(assets);
 
   for (let i = 0, l = ids.length; i < l; ++i) {
-    handleProps(abilities[ids[i]].props);
+    handleComponents(abilities[ids[i]].props);
   }
 
   for (let i = 0, l = assetKeys.length; i < l; ++i) {
@@ -36,13 +21,13 @@ export function compatibleToV2(config) {
 }
 
 // TODO temp
-function handleProps(props) {
+function handleComponents(props) {
   const keys = Object.keys(props);
   for (let i = 0, l = keys.length; i < l; ++i) {
     const k = keys[i];
     const v = props[k];
 
-    if (v?.length > 1) {
+    if (Array.isArray(v)) {
       if (["color", "diffuseColor", "specularColor"].indexOf(k) !== -1) {
         props[k] = new Color(v[0], v[1], v[2], v[3]);
       } else if (v.length === 4) {
@@ -64,23 +49,21 @@ function handleAssets(props: any = {}) {
   for (let i = 0, l = keys.length; i < l; i++) {
     const key = keys[i];
     const value = props[key];
+
     if (key === "newMaterial" || key === "scripts") {
       continue;
     }
-    if (["ambientColor", "emissiveColor", "diffuseColor", "specularColor", "baseColor"].indexOf(key) !== -1) {
-      props[key] = new Color(value[0], value[1], value[2], value[3]);
-      continue;
-    }
-    switch (value?.length) {
-      case 2:
-        props[key] = new Vector2(value[0], value[1]);
-        break;
-      case 3:
-        props[key] = new Vector3(value[0], value[1], value[2]);
-        break;
-      case 4:
+
+    if (Array.isArray(value)) {
+      if (["ambientColor", "emissiveColor", "diffuseColor", "specularColor", "baseColor"].indexOf(key) !== -1) {
+        props[key] = new Color(value[0], value[1], value[2], value[3]);
+      } else if (value.length === 4) {
         props[key] = new Vector4(value[0], value[1], value[2], value[3]);
-        break;
+      } else if (value.length === 3) {
+        props[key] = new Vector3(value[0], value[1], value[2]);
+      } else if (value.length === 2) {
+        props[key] = new Vector2(value[0], value[1]);
+      }
     }
   }
 }
