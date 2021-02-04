9f009d5d7b55dd43ac273aa047f66be112b934fb
zhuxudong <callzhuxudong@163.com>
2021-02-04 14:47:31 +0800

    feat: add PBRSpecularMaterial

--- packages/core/src/material/index.ts
+++ packages/core/src/material/index.ts
@@ -1,8 +1,9 @@
-export { BlinnPhongMaterial } from "./BlinnPhongMaterial";
 export { AlphaMode } from "./enums/AlphaMode";
 export { RenderQueueType } from "./enums/RenderQueueType";
 export { Material } from "./Material";
+export { BlinnPhongMaterial } from "./BlinnPhongMaterial";
 export { PBRMaterial } from "./PBRMaterial";
+export { PBRSpecularMaterial } from "./PBRSpecularMaterial";
 export { UnlightMaterial } from "./UnlightMaterial";
 
 // @todo: delete
