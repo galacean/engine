0276c11b877cd0a5c806a0b91aeb5f18cb4254e5
gz65555 <gz65555@gmail.com>
2021-02-28 11:56:18 +0800

    fix(material): ambient light is not working with diffuse

--- packages/core/src/shaderlib/begin_mobile_frag.glsl
+++ packages/core/src/shaderlib/begin_mobile_frag.glsl
@@ -2,10 +2,6 @@
     vec4 emission = u_emissiveColor;
     vec4 diffuse = u_diffuseColor;
     vec4 specular = u_specularColor;
-      
-    #ifdef O3_HAS_AMBIENT_LIGHT
-        ambient += vec4(u_ambientLightColor, 1.0);
-    #endif
 
     #ifdef O3_EMISSIVE_TEXTURE
 
@@ -22,5 +18,11 @@
     #ifdef O3_SPECULAR_TEXTURE
 
         specular *= texture2D(u_specularTexture, v_uv);
+
+    #endif
+
+    #ifdef O3_HAS_AMBIENT_LIGHT
+
+        ambient = vec4(u_ambientLightColor, 1.0) * diffuse;
 
     #endif
