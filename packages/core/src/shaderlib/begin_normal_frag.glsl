94f1c2c498c6869f61bdd31b3ad8471935aa7a89
zhuxudong <callzhuxudong@163.com>
2021-03-03 15:32:00 +0800

    refactor: rename to use common normal logic

--- packages/core/src/shaderlib/begin_normal_frag.glsl
+++ packages/core/src/shaderlib/begin_normal_frag.glsl
@@ -1,6 +1,6 @@
     #ifdef O3_HAS_NORMAL
 
-        #if defined( O3_HAS_TANGENT ) && defined( O3_HAS_NORMALMAP )
+        #if defined( O3_HAS_TANGENT ) && defined( O3_NORMAL_TEXTURE )
 
         vec3 N = normalize( v_TBN[ 2 ] );
 
