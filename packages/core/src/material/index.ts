0753253f518138f0f3b7251c132f674ad39ea3f1
zhuxudong <callzhuxudong@163.com>
2021-03-26 14:41:18 +0800

    docs: export pbr base material

--- packages/core/src/material/index.ts
+++ packages/core/src/material/index.ts
@@ -4,6 +4,7 @@ export { BlendMode } from "./enums/BlendMode";
 export { Material } from "./Material";
 export { BaseMaterial } from "./BaseMaterial";
 export { BlinnPhongMaterial } from "./BlinnPhongMaterial";
+export { PBRBaseMaterial } from "./PBRBaseMaterial";
 export { PBRMaterial } from "./PBRMaterial";
 export { PBRSpecularMaterial } from "./PBRSpecularMaterial";
 export { UnlitMaterial } from "./UnlitMaterial";
