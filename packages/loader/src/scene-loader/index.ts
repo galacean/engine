bb1ec82035a4504d18f323447575045d38b022b2
Hu Song <gz65555@gmail.com>
2021-03-30 17:41:47 +0800

    refactor: adapter to runtime loader

--- packages/loader/src/scene-loader/index.ts
+++ packages/loader/src/scene-loader/index.ts
@@ -3,3 +3,6 @@ export { registerResource } from "./ResourceManager";
 export { script, SchemaResource } from "./resources";
 export * from "./types";
 export { Oasis } from "./Oasis";
+export { Model } from "./Model";
+export { GLTFModel } from "./GLTFModel";
+
