c09b291367af2e2274bd3e272bf27016092192fd
singlecoder <z18511751272@gmail.com>
2021-04-01 18:32:15 +0800

    Refactor: adapt editor for sprite renderer (#147)

--- packages/loader/src/scene-loader/index.ts
+++ packages/loader/src/scene-loader/index.ts
@@ -1,8 +1,7 @@
+export { GLTFModel } from "./GLTFModel";
+export { Model } from "./Model";
+export { Oasis } from "./Oasis";
 export { parser, Parser } from "./Parser";
 export { registerResource } from "./ResourceManager";
-export { script, SchemaResource } from "./resources";
+export { SchemaResource, script, SpritePivotType } from "./resources";
 export * from "./types";
-export { Oasis } from "./Oasis";
-export { Model } from "./Model";
-export { GLTFModel } from "./GLTFModel";
-
