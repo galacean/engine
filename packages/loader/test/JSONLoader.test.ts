1df2f964aa80a26d2b8d9d136e5ce7d2fd149fc4
GuoLei <gl3336563@163.com>
2021-04-13 10:28:20 +0800

    test: fix unit test (#184)
    test: improve unit test

--- packages/loader/test/JSONLoader.test.ts
+++ packages/loader/test/JSONLoader.test.ts
@@ -1,9 +1,9 @@
 import "../src/JSONLoader";
-import { Engine } from "@oasis-engine/core";
+import { WebGLEngine } from "../../rhi-webgl/src/WebGLEngine";
 
 describe("text loader test", () => {
   it("text loader test", () => {
-    const engine = new Engine(null, { init: jest.fn(), canIUse: jest.fn() });
+    const engine = new WebGLEngine(document.createElement("canvas"));
     const promise = engine.resourceManager.load(
       "https://gw.alipayobjects.com/os/bmw-prod/73cba7a4-221b-4e32-8db6-c87968cf7ce0.json"
     );
