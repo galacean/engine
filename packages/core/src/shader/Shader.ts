16ec52f3b6e8438a6ce184dcfa1c5dec7e547714
GuoLei1990 <gl3336563@163.com>
2021-04-20 17:57:33 +0800

    feat: add return value for `compileVariant()`

--- packages/core/src/shader/Shader.ts
+++ packages/core/src/shader/Shader.ts
@@ -131,14 +131,15 @@ export class Shader {
    *
    * @param engine - Engine to which the shader variant belongs
    * @param macros - Macro name list
+   * @returns Is the compiled shader variant valid
    */
-  compileVariant(engine: Engine, macros: string[]): void {
+  compileVariant(engine: Engine, macros: string[]): boolean {
     const compileMacros = Shader._compileMacros;
     compileMacros.clear();
     for (let i = 0, n = macros.length; i < n; i++) {
       compileMacros.enable(Shader.getMacroByName(macros[i]));
     }
-    this._getShaderProgram(engine, compileMacros);
+    return this._getShaderProgram(engine, compileMacros).isValid;
   }
 
   /**
