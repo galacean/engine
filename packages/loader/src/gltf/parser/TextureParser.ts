495ea0739ba8e60cf110ad0515a2167b27087a67
zhuxudong <callzhuxudong@163.com>
2021-04-26 23:29:10 +0800

    fix: use gltfTextures

--- packages/loader/src/gltf/parser/TextureParser.ts
+++ packages/loader/src/gltf/parser/TextureParser.ts
@@ -1,20 +1,36 @@
-import { AssetType, Texture2D } from "@oasis-engine/core";
+import { AssetType, Logger, Texture2D, TextureWrapMode } from "@oasis-engine/core";
 import { GLTFResource } from "../GLTFResource";
+import { ISampler } from "../schema";
 import { getBufferViewData, loadImageBuffer, parseRelativeUrl } from "../Util";
 import { Parser } from "./Parser";
 
 export class TextureParser extends Parser {
+  private static _wrapMap = {
+    33071: TextureWrapMode.Clamp,
+    33648: TextureWrapMode.Mirror,
+    10497: TextureWrapMode.Repeat
+  };
+
   parse(context: GLTFResource): void | Promise<void> {
     const { gltf, buffers, engine, url } = context;
 
-    if (gltf.images) {
+    if (gltf.textures) {
       return Promise.all(
-        gltf.images.map(({ uri, bufferView: bufferViewIndex, mimeType }) => {
+        gltf.textures.map(({ sampler, source = 0 }) => {
+          const { uri, bufferView: bufferViewIndex, mimeType } = gltf.images[source];
+
           if (uri) {
-            return engine.resourceManager.load<Texture2D>({
-              url: parseRelativeUrl(url, uri),
-              type: AssetType.Texture2D
-            });
+            return engine.resourceManager
+              .load<Texture2D>({
+                url: parseRelativeUrl(url, uri),
+                type: AssetType.Texture2D
+              })
+              .then((texture) => {
+                if (sampler !== undefined) {
+                  this._parseSampler(texture, gltf.samplers[sampler]);
+                }
+                return texture;
+              });
           } else {
             const bufferView = gltf.bufferViews[bufferViewIndex];
             const bufferViewData = getBufferViewData(bufferView, buffers);
@@ -22,6 +38,9 @@ export class TextureParser extends Parser {
               const texture = new Texture2D(engine, image.width, image.height);
               texture.setImageSource(image);
               texture.generateMipmaps();
+              if (sampler !== undefined) {
+                this._parseSampler(texture, gltf.samplers[sampler]);
+              }
               return texture;
             });
           }
@@ -29,6 +48,22 @@ export class TextureParser extends Parser {
       ).then((textures: Texture2D[]) => {
         context.textures = textures;
       });
+    }
+  }
+
+  private _parseSampler(texture: Texture2D, sampler: ISampler): void {
+    const { magFilter, minFilter, wrapS, wrapT } = sampler;
+
+    if (magFilter || minFilter) {
+      Logger.warn("texture use filterMode in engine");
+    }
+
+    if (wrapS) {
+      texture.wrapModeU = TextureParser._wrapMap[wrapS];
+    }
+
+    if (wrapT) {
+      texture.wrapModeV = TextureParser._wrapMap[wrapT];
     }
   }
 }
