101a0699a5e683355e082a1cefe45eefd0541d15
zhuxudong <callzhuxudong@163.com>
2021-05-15 21:59:52 +0800

    feat: add more vertex format

--- packages/core/src/graphic/enums/VertexElementFormat.ts
+++ packages/core/src/graphic/enums/VertexElementFormat.ts
@@ -10,6 +10,22 @@ export enum VertexElementFormat {
   Vector3,
   /** Four-dimensional 32-bit float */
   Vector4,
+  /** Two-dimensional 8-bit integer,range is [-128,127] */
+  Byte2,
+  /** Two-dimensional 8-bit Unsigned integer, range is [0,255] */
+  UByte2,
+  /** Two-dimensional 8-bit Normalized integer, range is [-1,1] */
+  NormalizedByte2,
+  /** Two-dimensional 8-bit Normalized unsigned integer, range is [0,1] */
+  NormalizedUByte2,
+  /** Three-dimensional 8-bit integer,range is [-128,127] */
+  Byte3,
+  /** Three-dimensional 8-bit Unsigned integer, range is [0,255] */
+  UByte3,
+  /** Three-dimensional 8-bit Normalized integer, range is [-1,1] */
+  NormalizedByte3,
+  /** Three-dimensional 8-bit Normalized unsigned integer, range is [0,1] */
+  NormalizedUByte3,
   /** Four-dimensional 8-bit integer,range is [-128,127] */
   Byte4,
   /** Four-dimensional 8-bit Unsigned integer, range is [0,255] */
@@ -26,6 +42,14 @@ export enum VertexElementFormat {
   NormalizedShort2,
   /** Two-dimensional 16-bit Normalized unsigned integer, range is [0, 1] */
   NormalizedUShort2,
+  /** Three-dimensional 16-bit integer, range is[-32768, 32767] */
+  Short3,
+  /** Three-dimensional 16-bit Unsigned integer, range is [0, 65535] */
+  UShort3,
+  /** Three-dimensional 16-bit Unsigned integer, range is [-1, 1] */
+  NormalizedShort3,
+  /** Three-dimensional 16-bit Normalized unsigned integer, range is [0, 1] */
+  NormalizedUShort3,
   /** Four-dimensional 16-bit integer, range is [-32768, 32767] */
   Short4,
   /** Four-dimensional 16-bit Unsigned integer, range is [0, 65535] */
