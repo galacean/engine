30445bc0cbaf3b6123686b98d1657fdab4db5d58
singlecoder <z18511751272@gmail.com>
2021-05-20 17:06:55 +0800

    fix: fix set geometry invalid error (#293)

--- packages/loader/src/scene-loader/Model.ts
+++ packages/loader/src/scene-loader/Model.ts
@@ -2,38 +2,11 @@ import { BlinnPhongMaterial, Entity, MeshRenderer, PrimitiveMesh } from "@oasis-
 
 // Only for editor
 export class Model extends MeshRenderer {
-  private _geometryType: GeometryType;
-
-  set geometryType(value: GeometryType) {
-    switch (value) {
-      case "Sphere":
-        this.mesh = PrimitiveMesh.createSphere(this._engine);
-        break;
-
-      case "Cylinder":
-        this.mesh = PrimitiveMesh.createCylinder(this._engine);
-        break;
-
-      case "Plane":
-        this.mesh = PrimitiveMesh.createPlane(this._engine);
-        break;
-
-      case "Box":
-        this.mesh = PrimitiveMesh.createCuboid(this._engine);
-        break;
-    }
-
-    this._geometryType = value;
-  }
-
-  get geometryType() {
-    return this._geometryType;
-  }
+  private _props: Object = null;
 
   constructor(entity: Entity) {
     super(entity);
     this.setMaterial(new BlinnPhongMaterial(this.engine));
-    this.geometryType = GeometryType.Box;
   }
 
   get material(): any {
@@ -43,11 +16,47 @@ export class Model extends MeshRenderer {
   set material(mtl: any) {
     this.setMaterial(mtl);
   }
-}
 
-enum GeometryType {
-  Box = "Box",
-  Cylinder = "Cylinder",
-  Plane = "Plane",
-  Sphere = "Sphere"
+  setProps(props: any = {}) {
+    if (this._props !== props) {
+      this._props = props;
+    }
+
+    switch (props.geometryType) {
+      case "Sphere":
+        this.mesh = PrimitiveMesh.createSphere(this._engine, props.sphereRadius, props.sphereSegments);
+        break;
+
+      case "Cylinder":
+        this.mesh = PrimitiveMesh.createCylinder(
+          this._engine,
+          props.cylinderRadiusTop,
+          props.cylinderRadiusBottom,
+          props.cylinderHeight,
+          props.cylinderRadialSegments,
+          props.cylinderHeightSegments
+        );
+        break;
+
+      case "Plane":
+        this.mesh = PrimitiveMesh.createPlane(
+          this._engine,
+          props.planeWidth,
+          props.planeHeight,
+          props.planeHorizontalSegments,
+          props.planeVerticalSegments
+        );
+        break;
+
+      case "Box":
+        this.mesh = PrimitiveMesh.createCuboid(this._engine, props.boxWidth, props.boxHeight, props.boxDepth);
+        break;
+    }
+  }
+
+  updateProp(key: string, value: string | number) {
+    const props = this._props;
+    props[key] = value;
+    this.setProps(props);
+  }
 }
