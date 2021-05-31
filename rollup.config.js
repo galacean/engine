d372d6b79a69bed1f6cf03917d9e66fde688cfb8
gz65555 <gz65555@gmail.com>
2021-05-31 16:15:53 +0800

    build: clean scripts

--- rollup.config.js
+++ rollup.config.js
@@ -17,6 +17,7 @@ const { BUILD_TYPE, NODE_ENV } = process.env;
 const pkgsRoot = path.join(__dirname, "packages");
 const pkgs = fs
   .readdirSync(pkgsRoot)
+  .filter((dir) => dir !== "design")
   .map((dir) => path.join(pkgsRoot, dir))
   .filter((dir) => fs.statSync(dir).isDirectory())
   .map((location) => {
@@ -58,6 +59,7 @@ function config({ location, pkgJson }) {
   const name = pkgJson.name;
   commonPlugins.push(
     replace({
+      preventAssignment: true,
       __buildVersion: pkgJson.version
     })
   );
