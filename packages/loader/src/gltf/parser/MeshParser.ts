233d8af97f900d1af8eb269882356cdbf99cec70
GuoLei1990 <gl3336563@163.com>
2021-07-13 12:04:05 +0800

    refactor: fix bug

--- packages/loader/src/gltf/parser/MeshParser.ts
+++ packages/loader/src/gltf/parser/MeshParser.ts
@@ -215,8 +215,8 @@ export class MeshParser extends Parser {
       const deltaNorBuffer = getBlendShapeData("NORMAL", i);
       const deltaTanBuffer = getBlendShapeData("TANGENT", i);
       const deltaPositions = deltaPosBuffer ? GLTFUtil.floatBufferToVector3Array(<Float32Array>deltaPosBuffer) : null;
-      const deltaNormals = deltaPosBuffer ? GLTFUtil.floatBufferToVector3Array(<Float32Array>deltaNorBuffer) : null;
-      const deltaTangents = deltaPosBuffer ? GLTFUtil.floatBufferToVector3Array(<Float32Array>deltaTanBuffer) : null;
+      const deltaNormals = deltaNorBuffer ? GLTFUtil.floatBufferToVector3Array(<Float32Array>deltaNorBuffer) : null;
+      const deltaTangents = deltaTanBuffer ? GLTFUtil.floatBufferToVector3Array(<Float32Array>deltaTanBuffer) : null;
 
       const blendShape = new BlendShape(name);
       blendShape.addFrame(1.0, deltaPositions, deltaNormals, deltaTangents);
