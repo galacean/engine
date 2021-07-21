907b2f1bcbf32fcab77bddba713a65ccb475752c
azhan <zhanyingwei.zyw@antgroup.com>
2021-07-21 10:33:36 +0800

    feat:Support atlas

--- packages/core/src/2d/atlas/SpriteAtlas.ts
+++ packages/core/src/2d/atlas/SpriteAtlas.ts
@@ -1,21 +1,18 @@
-import { Rect, Vector2 } from "@oasis-engine/math";
 import { RefObject } from "../../asset/RefObject";
 import { Engine } from "../../Engine";
-import { Texture2D } from "../../texture";
-import { AtlasConfig } from "./types";
 import { Sprite } from "../sprite/Sprite";
 
 /**
  * Sprite Atlas.
  */
 export class SpriteAtlas extends RefObject {
-  // Save all sprite instances in the Atlas.
-  private _spritesMap: { [key: string]: Sprite } = {};
+  /** @internal */
+  _spritesMap: Record<string, Sprite> = {};
 
   /**
    * Get the sprite named 'name' from the atlas.
-   * @param name
-   * @returns
+   * @param name - The name of the sprite you want to find
+   * @returns The sprite you want to find
    */
   public getSprite(name: string): Sprite {
     const sprite = this._spritesMap[name];
@@ -27,10 +24,9 @@ export class SpriteAtlas extends RefObject {
 
   /**
    * Get all the sprites in the atlas.
-   * @returns
+   * @returns all the sprites in the atlas
    */
-  public getSprites(): Sprite[] {
-    const sprites: Sprite[] = [];
+  public getSprites(sprites: Sprite[]): Sprite[] {
     const spritesMap = this._spritesMap;
     for (let key in spritesMap) {
       sprites.push(spritesMap[key]);
@@ -38,68 +34,8 @@ export class SpriteAtlas extends RefObject {
     return sprites;
   }
 
-  /**
-   * Pass in atlas data and pictures to parse out the included sprites.
-   * @param config Raw data.
-   * @param originalImgs Original images.
-   * @returns
-   */
-  private _initialization(config: AtlasConfig, originalImgs: HTMLImageElement[]): void {
-    const { engine, _spritesMap: spritesMap } = this;
-    const atlasItems = config.AtlasItems;
-    const atlasItemsLen = atlasItems.length;
-    const texturesArr: Texture2D[] = new Array(atlasItemsLen);
-    for (let idx = atlasItemsLen - 1; idx >= 0; idx--) {
-      // Generate Texture2D according to configuration
-      const originalImg = originalImgs[idx];
-      const { width, height } = originalImg;
-      const texture = new Texture2D(engine, width, height, config.format);
-      /** @ts-ignore */
-      if (!texture._platformTexture) return;
-      texture.setImageSource(originalImg);
-      texture.generateMipmaps();
-      texturesArr[idx] = texture;
-
-      // Generate all the sprites on this texture.
-      const atlasItem = atlasItems[idx];
-      const frames = atlasItem.frames;
-      const [sourceWidth, sourceHeight] = atlasItem.size;
-      const sourceWidthReciprocal = 1.0 / sourceWidth;
-      const sourceHeightReciprocal = 1.0 / sourceHeight;
-      for (var key in frames) {
-        const frame = frames[key];
-        const region = frame.region;
-        const pivot = frame.pivot;
-        const offset = frame.offset;
-        const atlasRegion = frame.atlasRegion;
-        const sprite = new Sprite(
-          engine,
-          texture,
-          new Rect(region.x, region.y, region.w, region.h),
-          new Vector2(pivot.x, pivot.y),
-          frame.pixelsPerUnit
-        );
-        sprite.atlasRegion = new Rect(
-          atlasRegion.x * sourceWidthReciprocal,
-          atlasRegion.y * sourceHeightReciprocal,
-          atlasRegion.w * sourceWidthReciprocal,
-          atlasRegion.h * sourceHeightReciprocal
-        );
-        sprite.offset = new Vector2(offset.x, offset.y);
-        spritesMap[key] = sprite;
-      }
-    }
-  }
-
-  /**
-   * Constructor a sprite.
-   * @param engine
-   * @param atlasData Raw data.
-   * @param imgs Original images.
-   */
-  constructor(engine: Engine, atlasData: AtlasConfig, imgs: HTMLImageElement[]) {
+  constructor(engine: Engine) {
     super(engine);
-    this._initialization(atlasData, imgs);
   }
 
   /**
--- packages/loader/src/SpriteAtlasLoader.ts
+++ packages/loader/src/SpriteAtlasLoader.ts
@@ -4,10 +4,13 @@ import {
   AssetPromise,
   AssetType,
   LoadItem,
-  SpriteAtlas,
-  ResourceManager
+  ResourceManager,
+  Texture2D,
+  Sprite,
+  SpriteAtlas
 } from "@oasis-engine/core";
 import { AtlasConfig } from "@oasis-engine/core/types/2d/atlas/types";
+import { Rect, Vector2 } from "@oasis-engine/math";
 
 @resourceLoader(AssetType.SpriteAtlas, ["atlas"], false)
 class SpriteAtlasLoader extends Loader<SpriteAtlas> {
@@ -33,8 +36,55 @@ class SpriteAtlasLoader extends Loader<SpriteAtlas> {
               })
             )
           ).then((imgs) => {
+            const engine = resourceManager.engine;
+            //
+            const spriteAtlas = new SpriteAtlas(engine);
+            /** @ts-ignore */
+            const spriteMap = spriteAtlas._spritesMap;
+            const format = atlasData.format;
+            const texturesArr: Texture2D[] = new Array(atlasItemsLen);
+            for (let idx = atlasItemsLen - 1; idx >= 0; idx--) {
+              // Generate Texture2D according to configuration
+              const originalImg = imgs[idx];
+              const { width, height } = originalImg;
+              const texture = new Texture2D(engine, width, height, format);
+              /** @ts-ignore */
+              if (!texture._platformTexture) return;
+
+              texture.setImageSource(originalImg);
+              texture.generateMipmaps();
+              texturesArr[idx] = texture;
+              // Generate all the sprites on this texture.
+              const atlasItem = atlasItems[idx];
+              const frames = atlasItem.frames;
+              const [sourceWidth, sourceHeight] = atlasItem.size;
+              const sourceWidthReciprocal = 1.0 / sourceWidth;
+              const sourceHeightReciprocal = 1.0 / sourceHeight;
+              for (var key in frames) {
+                const frame = frames[key];
+                const region = frame.region;
+                const pivot = frame.pivot;
+                const offset = frame.offset;
+                const atlasRegion = frame.atlasRegion;
+                const sprite = new Sprite(
+                  engine,
+                  texture,
+                  new Rect(region.x, region.y, region.w, region.h),
+                  new Vector2(pivot.x, pivot.y),
+                  frame.pixelsPerUnit
+                );
+                sprite.atlasRegion = new Rect(
+                  atlasRegion.x * sourceWidthReciprocal,
+                  atlasRegion.y * sourceHeightReciprocal,
+                  atlasRegion.w * sourceWidthReciprocal,
+                  atlasRegion.h * sourceHeightReciprocal
+                );
+                sprite.offset = new Vector2(offset.x, offset.y);
+                spriteMap[key] = sprite;
+              }
+            }
             // Return a SpriteAtlas instance.
-            resolve(new SpriteAtlas(resourceManager.engine, atlasData, imgs));
+            resolve(spriteAtlas);
           });
         })
         .catch((e) => {
