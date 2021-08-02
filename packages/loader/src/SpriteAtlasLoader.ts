d9f1b9a47a6400133403b40d997c16eebbfb6589
AZhan <njktsmshh@163.com>
2021-08-02 22:41:31 +0800

    feat:Support atlas #197 (#402)
    * feat:Support atlas #197
    
    Co-authored-by: azhan <zhanyingwei.zyw@antgroup.com>
    Co-authored-by: GuoLei1990 <gl3336563@163.com>

---
+++ packages/loader/src/SpriteAtlasLoader.ts
@@ -0,0 +1,85 @@
+import {
+  resourceLoader,
+  Loader,
+  AssetPromise,
+  AssetType,
+  LoadItem,
+  ResourceManager,
+  Texture2D,
+  Sprite,
+  SpriteAtlas
+} from "@oasis-engine/core";
+import { AtlasConfig } from "@oasis-engine/core/types/2d/atlas/types";
+import { Rect, Vector2 } from "@oasis-engine/math";
+
+@resourceLoader(AssetType.SpriteAtlas, ["atlas"], false)
+class SpriteAtlasLoader extends Loader<SpriteAtlas> {
+  load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<SpriteAtlas> {
+    return new AssetPromise((resolve, reject) => {
+      this.request<AtlasConfig>(item.url, {
+        ...item,
+        type: "json"
+      })
+        .then((atlasData) => {
+          const { atlasItems, format } = atlasData;
+          const atlasItemsLen = atlasItems.length;
+          Promise.all(
+            atlasItems.map(({ img }) =>
+              this.request<HTMLImageElement>(img, {
+                ...item,
+                type: "image"
+              })
+            )
+          ).then((imgs) => {
+            const { engine } = resourceManager;
+            // Generate a SpriteAtlas object
+            const tempRect = new Rect();
+            const tempPivot = new Vector2();
+            const spriteAtlas = new SpriteAtlas(engine);
+            for (let i = 0; i < atlasItemsLen; i++) {
+              // Generate Texture2D according to configuration
+              const originalImg = imgs[i];
+              const { width, height } = originalImg;
+              const texture = new Texture2D(engine, width, height, format);
+              texture.setImageSource(originalImg);
+              texture.generateMipmaps();
+              // Generate all the sprites on this texture.
+              const atlasItem = atlasItems[i];
+              const sprites = atlasItem.sprites;
+              const sourceWidthReciprocal = 1.0 / width;
+              const sourceHeightReciprocal = 1.0 / height;
+              for (let j = sprites.length - 1; j >= 0; j--) {
+                const atlasSprite = sprites[j];
+                const { region, pivot, atlasRegionOffset, atlasRegion } = atlasSprite;
+
+                tempRect.setValue(region.x, region.y, region.w, region.h);
+                tempPivot.setValue(pivot.x, pivot.y);
+
+                const sprite = new Sprite(
+                  engine,
+                  texture,
+                  tempRect,
+                  tempPivot,
+                  atlasSprite.pixelsPerUnit,
+                  atlasSprite.name
+                );
+                sprite.atlasRegion.setValue(
+                  atlasRegion.x * sourceWidthReciprocal,
+                  atlasRegion.y * sourceHeightReciprocal,
+                  atlasRegion.w * sourceWidthReciprocal,
+                  atlasRegion.h * sourceHeightReciprocal
+                );
+                sprite.atlasRegionOffset.setValue(atlasRegionOffset.x, atlasRegionOffset.y);
+                /** @ts-ignore */
+                spriteAtlas._addSprite(sprite);
+              }
+            }
+            resolve(spriteAtlas);
+          });
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  }
+}
