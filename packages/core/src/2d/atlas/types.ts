f1bfb2c15f326a9f454af414277763968fd0f0d5
GuoLei1990 <gl3336563@163.com>
2021-08-02 21:01:37 +0800

    refactor: opt code

--- packages/core/src/2d/atlas/types.ts
+++ packages/core/src/2d/atlas/types.ts
@@ -4,19 +4,17 @@ import { TextureFormat } from "../../texture";
  * The original data type of the atlas.
  */
 export interface AtlasConfig {
-  /** The big picture array, each big picture contains multiple sprites. */
-  atlasItems: {
-    /** The url of the big picture. */
-    img: string;
-    /** The size of the big picture. */
-    size: number[];
-    /** Sprites contained in the big picture. */
-    sprites: AtlasSprite[];
-  }[];
-  /** Version of Atlas */
+  /** Version of Atlas. */
   version: number;
   /** Texture format. */
   format: TextureFormat;
+  /** The sub atlas array, each sub atlas contains multiple sprites. */
+  atlasItems: {
+    /** The url of the sub atlas. */
+    img: string;
+    /** Sprites contained in the sub atlas. */
+    sprites: AtlasSprite[];
+  }[];
 }
 
 /**
@@ -25,16 +23,11 @@ export interface AtlasConfig {
 export interface AtlasSprite {
   /** The name the sprite. */
   name: string;
-  /** The original size of the sprite. */
-  sourceSize: { w: number; h: number };
-  /** The range of the sprites on the big picture */
+  /** The range of the sprites on the big picture. */
   atlasRegion: { x: number; y: number; w: number; h: number };
   /** If there is trimming, the offset of the sprite relative to the original sprite. */
   atlasRegionOffset: { x: number; y: number };
   region: { x: number; y: number; w: number; h: number };
   pivot: { x: number; y: number };
   pixelsPerUnit: number;
-  vertices: number[][];
-  uv: number[][];
-  triangles: number[];
 }
