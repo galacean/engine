3711d3cb00e864e5eac8f39c886468c28cbffae9
gz65555 <gz65555@gmail.com>
2021-09-01 11:51:59 +0800

    feat: merge model mesh

--- packages/core/src/mesh/ModelMesh.ts
+++ packages/core/src/mesh/ModelMesh.ts
@@ -85,12 +85,9 @@ export class ModelMesh extends Mesh {
    * @param engine - Engine to which the mesh belongs
    * @param name - Mesh name
    */
-  getIndices(): Uint8Array | Uint16Array | Uint32Array {
-    if (!this._accessible) {
-      throw "Not allowed to access data while accessible is false.";
-    }
-
-    return this._indices;
+  constructor(engine: Engine, name?: string) {
+    super(engine);
+    this.name = name;
   }
 
   /**
