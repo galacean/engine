fa837df87c489b96d8ef1cdb3512b270931801e2
gz65555 <gz65555@gmail.com>
2021-09-15 15:32:10 +0800

    refactor: export decoder

--- packages/loader/src/index.ts
+++ packages/loader/src/index.ts
@@ -9,6 +9,7 @@ import "./SpriteAtlasLoader";
 import "./gltf/extensions/index";
 import "./OasisAssetLoader";
 
+export { decode } from "@oasis-engine/resource-process";
 export { GLTFResource } from "./gltf/GLTFResource";
 export { GLTFModel } from "./scene-loader/GLTFModel";
 export { Model } from "./scene-loader/Model";
