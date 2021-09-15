aa2f10be516f74084da7ec470d11590d033ff048
gz65555 <gz65555@gmail.com>
2021-09-15 14:40:36 +0800

    refactor: use default

--- packages/core/src/sky/SkyBoxMaterial.ts
+++ packages/core/src/sky/SkyBoxMaterial.ts
@@ -14,7 +14,6 @@ export class SkyBoxMaterial extends Material {
 
     this.renderState.rasterState.cullMode = CullMode.Off;
     this.renderState.depthState.compareFunction = CompareFunction.LessEqual;
-    this.renderQueueType = 0;
   }
 
   /** Texture cube map of the sky box material. */
