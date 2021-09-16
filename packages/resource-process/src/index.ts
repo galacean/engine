9d03a9312837fca7ed16290c19aa81725aed060d
gz65555 <gz65555@gmail.com>
2021-09-16 16:36:27 +0800

    refactor: remove mesh

--- packages/resource-process/src/index.ts
+++ packages/resource-process/src/index.ts
@@ -32,7 +32,7 @@ export function decode<T>(arrayBuffer: ArrayBuffer, engine: Engine): Promise<T> 
  * @param meta - meta file data
  * @param byteLength - byte length of estimate array buffer
  */
-export function encode(main: any, meta: any, byteLength?: number): ArrayBuffer {
+export function encode(main: any, meta: any, byteLength: number = 10000): ArrayBuffer {
   const { name, version, type } = meta;
   const bufferWriter = new BufferWriter(new ArrayBuffer(byteLength));
   // write header
