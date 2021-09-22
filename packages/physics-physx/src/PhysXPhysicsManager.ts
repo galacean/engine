75f600074481e0a4b97368d79d27218a0a2523a2
yangfengzzz <yangfengzzz@hotmail.com>
2021-09-22 17:30:05 +0800

    fix: opt code in CR

--- packages/physics-physx/src/PhysXPhysicsManager.ts
+++ packages/physics-physx/src/PhysXPhysicsManager.ts
@@ -38,9 +38,9 @@ export class PhysXPhysicsManager implements IPhysicsManager {
   private readonly _onTriggerEnd?: Function;
   private readonly _onTriggerPersist?: Function;
 
-  private _eventMap: DisorderedArray<TriggerEvent> = new DisorderedArray<TriggerEvent>();
-  private _triggerPool: Object = {};
-  private _internalTriggerPool: TriggerEvent[] = [];
+  private _currentEvents: DisorderedArray<TriggerEvent> = new DisorderedArray<TriggerEvent>();
+  private _eventMap: Record<number, Record<number, TriggerEvent>> = {};
+  private _eventPool: TriggerEvent[] = [];
 
   private _gravity: Vector3 = new Vector3(0, -9.81, 0);
 
@@ -77,37 +77,22 @@ export class PhysXPhysicsManager implements IPhysicsManager {
     this._onTriggerPersist = onTriggerPersist;
 
     const triggerCallback = {
-      onContactBegin: (obj1, obj2) => {
-      },
-      onContactEnd: (obj1, obj2) => {
-      },
-      onContactPersist: (obj1, obj2) => {
-      },
+      onContactBegin: (obj1, obj2) => {},
+      onContactEnd: (obj1, obj2) => {},
+      onContactPersist: (obj1, obj2) => {},
       onTriggerBegin: (obj1, obj2) => {
         const index1 = obj1.getQueryFilterData().word0;
         const index2 = obj2.getQueryFilterData().word0;
-        if (index1 < index2) {
-          const event = this._obtainTrigger(index1, index2);
-          event.state = PhysicsState.TOUCH_FOUND;
-          this._eventMap.add(event);
-        } else {
-          const event = this._obtainTrigger(index2, index1);
-          event.state = PhysicsState.TOUCH_FOUND;
-          this._eventMap.add(event);
-        }
+        const event = index1 < index2 ? this._getTrigger(index1, index2) : this._getTrigger(index2, index1);
+        event.state = PhysicsState.TOUCH_FOUND;
+        this._currentEvents.add(event);
       },
       onTriggerEnd: (obj1, obj2) => {
         const index1 = obj1.getQueryFilterData().word0;
         const index2 = obj2.getQueryFilterData().word0;
-        if (index1 < index2) {
-          const event = this._triggerPool[index1][index2];
-          event.state = PhysicsState.TOUCH_LOST;
-          this._triggerPool[index1][index2] = undefined;
-        } else {
-          const event = this._triggerPool[index2][index1];
-          event.state = PhysicsState.TOUCH_LOST;
-          this._triggerPool[index1][index2] = undefined;
-        }
+        const event = index1 < index2 ? this._eventMap[index1][index2] : this._eventMap[index2][index1];
+        event.state = PhysicsState.TOUCH_LOST;
+        this._eventMap[index1][index2] = undefined;
       }
     };
 
@@ -123,27 +108,15 @@ export class PhysXPhysicsManager implements IPhysicsManager {
     PhysXPhysicsManager._pxFilterData = new PhysXPhysics.PhysX.PxQueryFilterData();
   }
 
-  private _obtainTrigger(index1: number, index2: number): TriggerEvent {
-    if (this._triggerPool[index1][index2] == undefined) {
-      if (this._internalTriggerPool.length == 0) {
-        return (this._triggerPool[index1][index2] = new TriggerEvent(PhysicsState.TOUCH_NONE, index1, index2));
-      } else {
-        return (this._triggerPool[index1][index2] = this._internalTriggerPool.pop());
-      }
-    } else {
-      throw "location have already been set!";
-    }
-  }
-
   //--------------public APIs--------------------------------------------------
   /**
    * Add PhysXCollider into the manager
    * @param collider - PhysXStaticCollider or PhysXDynamicCollider
    */
-  addCollider(collider: PhysXCollider) {
+  addCollider(collider: PhysXCollider): void {
     this._pxScene.addActor(collider._pxActor, null);
     for (let i = 0, len = collider._shapes.length; i < len; i++) {
-      this._triggerPool[collider._shapes[i]._id] = {};
+      this._eventMap[collider._shapes[i]._id] = {};
     }
   }
 
@@ -151,17 +124,17 @@ export class PhysXPhysicsManager implements IPhysicsManager {
    * Remove PhysXCollider
    * @param collider - PhysXStaticCollider or PhysXDynamicCollider
    */
-  removeCollider(collider: PhysXCollider) {
+  removeCollider(collider: PhysXCollider): void {
     this._pxScene.removeActor(collider._pxActor, true);
     for (let i = 0, len = collider._shapes.length; i < len; i++) {
-      delete this._triggerPool[collider._shapes[i]._id];
+      delete this._eventMap[collider._shapes[i]._id];
     }
   }
 
   /**
    * Call on every frame to update pose of objects.
    */
-  update(elapsedTime: number) {
+  update(elapsedTime: number): void {
     this._simulate(elapsedTime);
     this._fetchResults();
     this._resolveEvent();
@@ -188,7 +161,7 @@ export class PhysXPhysicsManager implements IPhysicsManager {
     ray: Ray,
     distance: number,
     hit?: (id: number, distance: number, position: Vector3, normal: Vector3) => void
-  ): boolean {
+  ): Boolean {
     PhysXPhysicsManager._pxFilterData.flags = new PhysXPhysics.PhysX.PxQueryFlags(this._queryFlag);
 
     const { origin, direction } = ray;
@@ -227,29 +200,42 @@ export class PhysXPhysicsManager implements IPhysicsManager {
   }
 
   //--------------private APIs -------------------------------------------------
-  private _simulate(elapsedTime: number = 1 / 60, controlSimulation: boolean = true) {
+  private _simulate(elapsedTime: number = 1 / 60, controlSimulation: boolean = true): void {
     this._pxScene.simulate(elapsedTime, controlSimulation);
   }
 
-  private _fetchResults(block: boolean = true) {
+  private _fetchResults(block: boolean = true): void {
     this._pxScene.fetchResults(block);
   }
 
-  private _advance() {
+  private _advance(): void {
     this._pxScene.advance();
   }
 
-  private _fetchCollision(block: boolean = true) {
+  private _fetchCollision(block: boolean = true): void {
     this._pxScene.fetchCollision(block);
   }
 
-  private _collide(elapsedTime: number = 1 / 60) {
+  private _collide(elapsedTime: number = 1 / 60): void {
     this._pxScene.collide(elapsedTime);
   }
 
-  private _resolveEvent() {
-    for (let i = 0, n = this._eventMap.length; i < n; ) {
-      const event = this._eventMap.get(i);
+  private _getTrigger(index1: number, index2: number): TriggerEvent {
+    const eventMap = this._eventMap;
+    if (eventMap[index1][index2] == undefined) {
+      if (this._eventPool.length == 0) {
+        return (eventMap[index1][index2] = new TriggerEvent(PhysicsState.TOUCH_NONE, index1, index2));
+      } else {
+        return (eventMap[index1][index2] = this._eventPool.pop());
+      }
+    } else {
+      throw "location have already been set!";
+    }
+  }
+
+  private _resolveEvent(): void {
+    for (let i = 0, n = this._currentEvents.length; i < n; ) {
+      const event = this._currentEvents.get(i);
       if (event.state == PhysicsState.TOUCH_FOUND) {
         this._onTriggerBegin(event.index1, event.index2);
         event.state = PhysicsState.TOUCH_PERSISTS;
@@ -259,7 +245,8 @@ export class PhysXPhysicsManager implements IPhysicsManager {
         i++;
       } else if (event.state == PhysicsState.TOUCH_LOST) {
         this._onTriggerEnd(event.index1, event.index2);
-        this._eventMap.deleteByIndex(i);
+        this._currentEvents.deleteByIndex(i);
+        this._eventPool.push(event);
         n--;
       }
     }
