70391eb51237499eb1c52ada4c831426f59402a4
shensi.zxd <shensi.zxd@alibaba-inc.com>
2021-09-22 14:54:54 +0800

    feat: support texture2D with mipmap

--- packages/resource-process/src/resources/texture/TextureDecoder.ts
+++ packages/resource-process/src/resources/texture/TextureDecoder.ts
@@ -1,7 +1,6 @@
 import { Engine, Texture2D } from "@oasis-engine/core";
 import { BufferReader } from "../../utils/BufferReader";
 import { decoder } from "../../utils/Decorator";
-import { loadImageBuffer } from "../../utils/Utils";
 
 @decoder("Texture2D")
 export class Texture2DDecoder {
@@ -15,7 +14,7 @@ export class Texture2DDecoder {
       const bufferReader = new BufferReader(arraybuffer, byteOffset, byteLength);
 
       const objectId = bufferReader.nextStr();
-      const mipmap = bufferReader.nextUint8();
+      const mipmap = !!bufferReader.nextUint8();
       const filterMode = bufferReader.nextUint8();
       const anisoLevel = bufferReader.nextUint8();
       const wrapModeU = bufferReader.nextUint8();
@@ -23,24 +22,30 @@ export class Texture2DDecoder {
       const format = bufferReader.nextUint8();
       const width = bufferReader.nextUint16();
       const height = bufferReader.nextUint16();
-      const imageData = bufferReader.nextImageData();
+      const imageDataCount = bufferReader.nextUint8();
+      const imagesData = bufferReader.nextImagesData(imageDataCount);
 
-      const { buffer, type } = imageData;
-      loadImageBuffer(buffer, type)
-        .then((image) => {
-          const texture2D = new Texture2D(engine, width, height, format, mipmap !== 0);
-          texture2D.filterMode = filterMode;
-          texture2D.anisoLevel = anisoLevel;
-          texture2D.wrapModeU = wrapModeU;
-          texture2D.wrapModeV = wrapModeV;
-          texture2D.setImageSource(image);
-          // @ts-ignore
-          engine.resourceManager._objectPool[objectId] = texture2D;
-          resolve(texture2D);
-        })
-        .catch((err) => {
-          reject(err);
-        });
+      const texture2D = new Texture2D(engine, width, height, format, mipmap);
+      texture2D.filterMode = filterMode;
+      texture2D.anisoLevel = anisoLevel;
+      texture2D.wrapModeU = wrapModeU;
+      texture2D.wrapModeV = wrapModeV;
+
+      const pixelBuffer = new Uint8Array(imagesData[0].buffer);
+      texture2D.setPixelBuffer(pixelBuffer);
+
+      if (mipmap) {
+        texture2D.generateMipmaps();
+        for (let i = 1; i < imageDataCount; i++) {
+          const pixelBuffer = new Uint8Array(imagesData[i].buffer);
+          texture2D.setPixelBuffer(pixelBuffer, i);
+        }
+      }
+
+      // @ts-ignore
+      engine.resourceManager._objectPool[objectId] = texture2D;
+
+      resolve(texture2D);
     });
   }
 }
--- packages/resource-process/src/resources/texture/TextureEncoder.ts
+++ packages/resource-process/src/resources/texture/TextureEncoder.ts
@@ -1,33 +1,40 @@
 import { encoder } from "../..";
 import { BufferWriter } from "../../utils/BufferWriter";
 
-const textureDefaultOptions = {
-  mipmap: 1,
-  filterMode: 1,
-  format: 1,
-  anisoLevel: 0,
-  wrapModeU: 1,
-  wrapModeV: 1
-};
-
 @encoder("Texture2D")
 export class Texture2DEncoder {
-  static encode(bufferWriter: BufferWriter, data: ArrayBuffer, meta: any) {
-    meta = { ...textureDefaultOptions, ...meta };
-
-    const { width, height, type, objectId } = meta;
+  static encode(bufferWriter: BufferWriter, data: ArrayBuffer | ArrayBuffer[], meta: any) {
+    const {
+      type,
+      objectId,
+      mipmap = 1,
+      filterMode = 1,
+      anisoLevel = 1,
+      wrapModeU = 1,
+      wrapModeV = 1,
+      format = 1,
+      width,
+      height
+    } = meta;
 
     // write data
     bufferWriter.writeStr(objectId);
-    bufferWriter.writeUint8(meta.mipmap);
-    bufferWriter.writeUint8(meta.filterMode);
-    bufferWriter.writeUint8(meta.anisoLevel);
-    bufferWriter.writeUint8(meta.wrapModeU);
-    bufferWriter.writeUint8(meta.wrapModeV);
-    bufferWriter.writeUint8(meta.format);
+    bufferWriter.writeUint8(mipmap);
+    bufferWriter.writeUint8(filterMode);
+    bufferWriter.writeUint8(anisoLevel);
+    bufferWriter.writeUint8(wrapModeU);
+    bufferWriter.writeUint8(wrapModeV);
+    bufferWriter.writeUint8(format);
     bufferWriter.writeUint16(width);
     bufferWriter.writeUint16(height);
-    bufferWriter.writeImageData({ type, buffer: data });
+
+    if (!Array.isArray(data)) {
+      data = [data];
+    }
+    // convert to ImageData
+    const imagesData = data.map((buffer) => ({ type, buffer }));
+    bufferWriter.writeUint8(imagesData.length);
+    bufferWriter.writeImagesData(imagesData);
 
     return bufferWriter.buffer;
   }
