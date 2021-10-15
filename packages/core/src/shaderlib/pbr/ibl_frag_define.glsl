206f5bd21980dc85379ac22b7b43aa99b35a8c32
GuoLei1990 <gl3336563@163.com>
2021-10-15 14:04:26 +0800

    refactor: opt code

--- packages/core/src/shaderlib/pbr/ibl_frag_define.glsl
+++ packages/core/src/shaderlib/pbr/ibl_frag_define.glsl
@@ -61,6 +61,9 @@ vec3 getLightProbeRadiance(GeometricContext geometry, float roughness, int maxMI
         #endif
 
         envMapColor.rgb = RGBMToLinear(envMapColor, 5.0).rgb;
+        #ifdef OASIS_COLORSPACE_GAMMA
+            envMapColor = linearTogamma(envMapColor);
+        #endif
         return envMapColor.rgb * specularIntensity;
 
     #endif
