218608262ecfba336dcd705eeccbb3b1a60fd7c0
shensi.zxd <shensi.zxd@alibaba-inc.com>
2021-11-08 16:16:26 +0800

    feat: add rgbm decode mode

--- packages/core/src/shaderlib/extra/skybox.fs.glsl
+++ packages/core/src/shaderlib/extra/skybox.fs.glsl
@@ -1,9 +1,19 @@
+#include <common>
 uniform samplerCube u_cube;
 
 varying vec3 v_cubeUV;
+uniform vec4 u_param;
 
 void main() {
 
-    gl_FragColor = textureCube( u_cube, v_cubeUV );
+    vec4 textureColor = textureCube( u_cube, v_cubeUV );
+
+    if (u_param.x > 0.0){
+       textureColor = RGBMToLinear(textureColor, u_param.y);
+       textureColor = linearToGamma(textureColor);
+    }
+      
+
+    gl_FragColor = textureColor;
 
 }
--- packages/core/src/sky/SkyBoxMaterial.ts
+++ packages/core/src/sky/SkyBoxMaterial.ts
@@ -1,3 +1,4 @@
+import { Vector4 } from "@oasis-engine/math";
 import { Engine } from "../Engine";
 import { Material } from "../material/Material";
 import { CompareFunction } from "../shader/enums/CompareFunction";
@@ -9,11 +10,26 @@ import { TextureCubeMap } from "../texture";
  * SkyboxMaterial
  */
 export class SkyBoxMaterial extends Material {
-  constructor(engine: Engine) {
-    super(engine, Shader.find("skybox"));
+  private _decodeParam: Vector4 = new Vector4(0, 5, 0, 0);
 
-    this.renderState.rasterState.cullMode = CullMode.Off;
-    this.renderState.depthState.compareFunction = CompareFunction.LessEqual;
+  /** Decode from texture with RGBM format. */
+  get decodeFromRGBM() {
+    return Boolean(this._decodeParam.x);
+  }
+
+  set decodeFromRGBM(value: boolean) {
+    this._decodeParam.x = Number(value);
+  }
+
+  /**
+   * RGBM decode factor, default 5.0.
+   */
+  get RGBMFactor() {
+    return this._decodeParam.y;
+  }
+
+  set RGBMFactor(value: number) {
+    this._decodeParam.y = value;
   }
 
   /** Texture cube map of the sky box material. */
@@ -23,5 +39,14 @@ export class SkyBoxMaterial extends Material {
 
   set textureCubeMap(v: TextureCubeMap) {
     this.shaderData.setTexture("u_cube", v);
+  }
+
+  constructor(engine: Engine) {
+    super(engine, Shader.find("skybox"));
+
+    this.renderState.rasterState.cullMode = CullMode.Off;
+    this.renderState.depthState.compareFunction = CompareFunction.LessEqual;
+
+    this.shaderData.setVector4("u_param", this._decodeParam);
   }
 }
