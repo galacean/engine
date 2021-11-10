056cc8d1be44268874bbbc4d815eff018278f047
GuoLei <gl3336563@163.com>
2021-11-10 18:21:35 +0800

    refactor: fix physics package.json (#575)

--- packages/physics-physx/src/PhysXPhysics.ts
+++ packages/physics-physx/src/PhysXPhysics.ts
@@ -62,9 +62,9 @@ export class PhysXPhysics {
       }
 
       if (runtimeMode == PhysXRuntimeMode.JavaScript) {
-        script.src = "https://gw.alipayobjects.com/os/lib/oasis-engine/physics-physx/0.6-alpha.0/dist/physx.release.js";
+        script.src = "https://gw.alipayobjects.com/os/lib/oasis-engine/physics-physx/0.6.0-alpha.1/dist/physx.release.js";
       } else if (runtimeMode == PhysXRuntimeMode.WebAssembly) {
-        script.src = "https://gw.alipayobjects.com/os/lib/oasis-engine/physics-physx/0.6-alpha.0/dist/physx.release.js";
+        script.src = "https://gw.alipayobjects.com/os/lib/oasis-engine/physics-physx/0.6.0-alpha.1/dist/physx.release.js";
       }
     });
 
