32fdb206b05fc5eae42f463d9c67a7bd59b71350
Hu Song <gz65555@gmail.com>
2021-11-15 13:54:43 +0800

    Fix Background UV Error and Update TypeScript Version (#585)
    * fix: update background mesh

--- packages/core/src/asset/request.ts
+++ packages/core/src/asset/request.ts
@@ -122,7 +122,7 @@ function requestRes<T>(url: string, config: RequestConfig): AssetPromise<T> {
         xhr.setRequestHeader(name, headers[name]);
       });
     }
-    xhr.send(config.body);
+    xhr.send(config.body as XMLHttpRequestBodyInit);
   });
 }
 
