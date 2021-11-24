2c5c013a9a66ddef8b782c13a03168a567bc592a
yangfengzzz <yangfengzzz@hotmail.com>
2021-11-24 15:18:49 +0800

    feat: add comments for character controller

--- packages/physics-physx/src/characterkinematic/PhysXCharacterControllerManager.ts
+++ packages/physics-physx/src/characterkinematic/PhysXCharacterControllerManager.ts
@@ -3,14 +3,23 @@ import { Vector3 } from "oasis-engine";
 import { PhysXCapsuleCharacterController } from "./PhysXCapsuleCharacterController";
 import { PhysXCapsuleCharacterControllerDesc } from "./PhysXCapsuleCharacterControllerDesc";
 
+/**
+ * Manages an array of character controllers.
+ */
 export class PhysXCharacterControllerManager implements ICharacterControllerManager {
   /** @internal */
   _pxControllerManager: any;
 
+  /**
+   * {@inheritDoc ICharacterControllerManager.purgeControllers }
+   */
   purgeControllers() {
     this._pxControllerManager.purgeControllers();
   }
 
+  /**
+   * {@inheritDoc ICharacterControllerManager.createController }
+   */
   createController(desc: PhysXCapsuleCharacterControllerDesc): PhysXCapsuleCharacterController {
     let pxController = this._pxControllerManager.createController(desc._pxControllerDesc);
     let controller = new PhysXCapsuleCharacterController();
@@ -18,26 +27,44 @@ export class PhysXCharacterControllerManager implements ICharacterControllerMana
     return controller;
   }
 
+  /**
+   * {@inheritDoc ICharacterControllerManager.computeInteractions }
+   */
   computeInteractions(elapsedTime: number) {
     this._pxControllerManager.computeInteractions(elapsedTime);
   }
 
+  /**
+   * {@inheritDoc ICharacterControllerManager.setTessellation }
+   */
   setTessellation(flag: boolean, maxEdgeLength: number) {
     this._pxControllerManager.setTessellation(flag, maxEdgeLength);
   }
 
+  /**
+   * {@inheritDoc ICharacterControllerManager.setOverlapRecoveryModule }
+   */
   setOverlapRecoveryModule(flag: boolean) {
     this._pxControllerManager.setOverlapRecoveryModule(flag);
   }
 
+  /**
+   * {@inheritDoc ICharacterControllerManager.setPreciseSweeps }
+   */
   setPreciseSweeps(flag: boolean) {
     this._pxControllerManager.setPreciseSweeps(flag);
   }
 
+  /**
+   * {@inheritDoc ICharacterControllerManager.setPreventVerticalSlidingAgainstCeiling }
+   */
   setPreventVerticalSlidingAgainstCeiling(flag: boolean) {
     this._pxControllerManager.setPreventVerticalSlidingAgainstCeiling(flag);
   }
 
+  /**
+   * {@inheritDoc ICharacterControllerManager.shiftOrigin }
+   */
   shiftOrigin(shift: Vector3) {
     this._pxControllerManager.shiftOrigin(shift);
   }
