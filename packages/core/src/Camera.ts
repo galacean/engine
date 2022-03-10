25fb571cdec710256db5c387544aafb362be82b5
cptbtptpbcptdtptp <njktsmshh@163.com>
2022-03-11 00:31:39 +0800

    fix:InputManager

--- packages/core/src/Camera.ts
+++ packages/core/src/Camera.ts
@@ -1,4 +1,4 @@
-import { BoundingFrustum, MathUtil, Matrix, Quaternion, Ray, Vector2, Vector3, Vector4 } from "@oasis-engine/math";
+import { BoundingFrustum, MathUtil, Matrix, Ray, Vector2, Vector3, Vector4 } from "@oasis-engine/math";
 import { Logger } from "./base";
 import { deepClone, ignoreClone } from "./clone/CloneManager";
 import { Component } from "./Component";
