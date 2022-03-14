48d248337eb3dc0b2314a555ffb6c2ab319f66b7
cptbtptpbcptdtptp <njktsmshh@163.com>
2022-03-14 14:59:24 +0800

    fix:InputManager

--- packages/core/src/input/InputManager.ts
+++ packages/core/src/input/InputManager.ts
@@ -6,7 +6,7 @@ import { PointerManager } from "./pointer/PointerManager";
  * InputManager manages device input such as mouse, touch, keyboard, etc.
  */
 export class InputManager {
-  /** Input is enabled by default. */
+  /** Disable input for offscreen rendering. */
   private _enabled: boolean = true;
   private _pointerManager: PointerManager;
 
@@ -14,29 +14,18 @@ export class InputManager {
    * Pointer List.
    */
   get pointers(): Readonly<Pointer[]> {
-    return this._pointerManager ? this._pointerManager._pointers : null;
+    return this._enabled ? this._pointerManager._pointers : null;
   }
 
   /**
    *  Whether to handle multi-pointer.
    */
   get multiPointerEnabled(): boolean {
-    return this._pointerManager ? this._pointerManager._multiPointerEnabled : false;
+    return this._enabled ? this._pointerManager._multiPointerEnabled : false;
   }
 
   set multiPointerEnabled(enabled: boolean) {
-    this._pointerManager && (this._pointerManager._multiPointerEnabled = enabled);
-  }
-
-  /**
-   * Whether to disable input.
-   */
-  get enabled(): boolean {
-    return this._enabled;
-  }
-
-  set enabled(value: boolean) {
-    this._enabled = value;
+    this._enabled && (this._pointerManager._multiPointerEnabled = enabled);
   }
 
   /**
@@ -46,10 +35,10 @@ export class InputManager {
     // @ts-ignore
     const canvas = engine._canvas._webCanvas;
     if (canvas instanceof HTMLCanvasElement) {
-      this.enabled = true;
+      this._enabled = true;
       this._pointerManager = new PointerManager(engine, canvas);
     } else {
-      this.enabled = false;
+      this._enabled = false;
     }
   }
 
@@ -66,6 +55,6 @@ export class InputManager {
    * @internal
    */
   _destroy(): void {
-    this._pointerManager?._destroy();
+    this._enabled && this._pointerManager._destroy();
   }
 }
