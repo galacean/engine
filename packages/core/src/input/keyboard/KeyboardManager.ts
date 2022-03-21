5beef0b688cc7228cdc93167b9404d15c5378c5f
cptbtptpbcptdtptp <njktsmshh@163.com>
2022-03-21 16:47:43 +0800

    fix:update code

--- packages/core/src/input/keyboard/KeyboardManager.ts
+++ packages/core/src/input/keyboard/KeyboardManager.ts
@@ -41,40 +41,40 @@ export class KeyboardManager {
      * @internal
      */
     _update(): void {
-        this._curFrameCount++;
-        const { _nativeEvents, _curFrameDownList, _curFrameUpList } = this;
-        _curFrameDownList.length = 0;
-        _curFrameUpList.length = 0;
-        if (_nativeEvents.length > 0) {
-            const { _curFrameCount, _curHeldDownKeyToIndexMap, _curFrameHeldDownList } = this;
-            for (let i = 0, n = _nativeEvents.length; i < n; i++) {
-                const evt = _nativeEvents[i];
+        const curFrameCount = ++this._curFrameCount;
+        const { _nativeEvents: nativeEvents, _curFrameDownList: curFrameDownList, _curFrameUpList: curFrameUpList } = this;
+        curFrameDownList.length = 0;
+        curFrameUpList.length = 0;
+        if (nativeEvents.length > 0) {
+            const { _curHeldDownKeyToIndexMap: curHeldDownKeyToIndexMap, _curFrameHeldDownList: curFrameHeldDownList, _downKeyToFrameCountMap: downKeyToFrameCountMap, _upKeyToFrameCountMap: upKeyToFrameCountMap } = this;
+            for (let i = 0, n = nativeEvents.length; i < n; i++) {
+                const evt = nativeEvents[i];
                 const codeKey = <Keys>Keys[evt.code];
                 switch (evt.type) {
                     case 'keydown':
                         // Filter the repeated triggers of the keyboard.
-                        if (_curHeldDownKeyToIndexMap[codeKey] == null) {
-                            _curFrameDownList.add(codeKey);
-                            _curFrameHeldDownList.add(codeKey);
-                            _curHeldDownKeyToIndexMap[codeKey] = _curFrameHeldDownList.length - 1;
-                            this._downKeyToFrameCountMap[codeKey] = _curFrameCount;
+                        if (curHeldDownKeyToIndexMap[codeKey] == null) {
+                            curFrameDownList.add(codeKey);
+                            curFrameHeldDownList.add(codeKey);
+                            curHeldDownKeyToIndexMap[codeKey] = curFrameHeldDownList.length - 1;
+                            downKeyToFrameCountMap[codeKey] = curFrameCount;
                         }
                         break;
                     case 'keyup':
-                        const delIndex = _curHeldDownKeyToIndexMap[codeKey];
+                        const delIndex = curHeldDownKeyToIndexMap[codeKey];
                         if (delIndex != null) {
-                            _curHeldDownKeyToIndexMap[codeKey] = null;
-                            const swapCode = _curFrameHeldDownList.deleteByIndex(delIndex);
-                            swapCode && (_curHeldDownKeyToIndexMap[swapCode] = delIndex);
+                            curHeldDownKeyToIndexMap[codeKey] = null;
+                            const swapCode = curFrameHeldDownList.deleteByIndex(delIndex);
+                            swapCode && (curHeldDownKeyToIndexMap[swapCode] = delIndex);
                         }
-                        _curFrameUpList.add(codeKey);
-                        this._upKeyToFrameCountMap[codeKey] = _curFrameCount;
+                        curFrameUpList.add(codeKey);
+                        upKeyToFrameCountMap[codeKey] = curFrameCount;
                         break;
                     default:
                         break;
                 }
             }
-            _nativeEvents.length = 0;
+            nativeEvents.length = 0;
         }
     }
 
