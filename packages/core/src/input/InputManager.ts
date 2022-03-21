6f3e202047e3c77753f1dfa51a6fae8bd89dbd19
AZhan <njktsmshh@163.com>
2022-03-21 17:44:39 +0800

    feat: keyboardevent (#677)
    * feat: keyboardevent

--- packages/core/src/input/InputManager.ts
+++ packages/core/src/input/InputManager.ts
@@ -1,4 +1,6 @@
 import { Engine } from "../Engine";
+import { KeyboardManager } from "./keyboard/KeyboardManager";
+import { Keys } from "./enums/Keys";
 import { Pointer } from "./pointer/Pointer";
 import { PointerManager } from "./pointer/PointerManager";
 
@@ -9,6 +11,7 @@ export class InputManager {
   /** Disable input for offscreen rendering. */
   private _enabled: boolean = true;
   private _pointerManager: PointerManager;
+  private _keyboardManager: KeyboardManager
 
   /**
    * Pointer List.
@@ -29,6 +32,45 @@ export class InputManager {
   }
 
   /**
+   * Whether the key is being held down, if there is no parameter, return whether any key is being held down.
+   * @param key - The keys of the keyboard
+   * @returns Whether ths key is being held down.
+   */
+  isKeyHeldDown(key?: Keys): boolean {
+    if (key === undefined) {
+      return this._keyboardManager._curFrameHeldDownList.length > 0;
+    } else {
+      return !!this._keyboardManager._curHeldDownKeyToIndexMap[key];
+    }
+  }
+
+  /**
+    * Whether the key starts to be pressed down during the current frame, if there is no parameter, return whether any key starts to be pressed down during the current frame.
+    * @param key - The keys of the keyboard
+    * @returns Whether the key starts to be pressed down during the current frame.
+    */
+  isKeyDown(key?: Keys): boolean {
+    if (key === undefined) {
+      return this._keyboardManager._curFrameDownList.length > 0;
+    } else {
+      return this._keyboardManager._downKeyToFrameCountMap[key] === this._keyboardManager._curFrameCount;
+    }
+  }
+
+  /**
+   * Whether the key is released during the current frame, if there is no parameter, return whether any key released during the current frame.
+   * @param key - The keys of the keyboard
+   * @returns Whether the key is released during the current frame.
+   */
+  isKeyUp(key?: Keys): boolean {
+    if (key === undefined) {
+      return this._keyboardManager._curFrameUpList.length > 0;
+    } else {
+      return this._keyboardManager._upKeyToFrameCountMap[key] === this._keyboardManager._curFrameCount;
+    }
+  }
+
+  /**
    * @internal
    */
   constructor(engine: Engine) {
@@ -37,6 +79,10 @@ export class InputManager {
     if (canvas instanceof HTMLCanvasElement) {
       this._enabled = true;
       this._pointerManager = new PointerManager(engine, canvas);
+      this._keyboardManager = new KeyboardManager();
+      window.addEventListener('blur', () => {
+        this._keyboardManager._onBlur();
+      });
     } else {
       this._enabled = false;
     }
@@ -48,6 +94,7 @@ export class InputManager {
   _update(): void {
     if (this._enabled) {
       this._pointerManager._update();
+      this._keyboardManager._update();
     }
   }
 
@@ -55,6 +102,9 @@ export class InputManager {
    * @internal
    */
   _destroy(): void {
-    this._enabled && this._pointerManager._destroy();
+    if (this._enabled) {
+      this._pointerManager._destroy();
+      this._keyboardManager._destroy();
+    }
   }
 }
