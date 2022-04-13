e046205d17b58a6f3fd60039c9a14a4dc64dd9e9
GuoLei1990 <gl3336563@163.com>
2022-04-13 13:59:38 +0800

    refactor: opt code

--- packages/core/src/BoolUpdateFlag.ts
+++ packages/core/src/BoolUpdateFlag.ts
@@ -5,9 +5,6 @@ import { UpdateFlagManager } from "./UpdateFlagManager";
  * Used to update tags.
  */
 export class BoolUpdateFlag extends UpdateFlag {
-  /** @internal */
-  _flagManagers: UpdateFlagManager[] = [];
-
   /** Flag. */
   flag = true;
 
--- packages/core/src/ListenerUpdateFlag.ts
+++ packages/core/src/ListenerUpdateFlag.ts
@@ -5,9 +5,6 @@ import { UpdateFlagManager } from "./UpdateFlagManager";
  * Used to update tags.
  */
 export class ListenerUpdateFlag extends UpdateFlag {
-  /** @internal */
-  _flagManagers: UpdateFlagManager[] = [];
-
   /** Listener. */
   listener: Function;
 
