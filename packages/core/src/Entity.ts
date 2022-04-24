ba4c0824e99f6878dcc26259b8d9dd712815c702
GuoLei1990 <gl3336563@163.com>
2022-04-24 14:32:14 +0800

    feat: test1

--- packages/core/src/Entity.ts
+++ packages/core/src/Entity.ts
@@ -323,6 +323,7 @@ export class Entity extends EngineObject {
     for (let i = 0, len = this._children.length; i < len; i++) {
       const child = children[i];
       cloneEntity.addChild(child.clone());
+      
     }
 
     const components = this._components;
