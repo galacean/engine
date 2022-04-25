1dbde2092521d52eaad9ab4b77d3b4f76c3e2098
gz65555 <kevin.hs@antgroup.com>
2022-04-25 15:13:21 +0800

    ci: use floss as test tool

--- tests/src/rhi-webgl/WebGLEngine.test.ts
+++ tests/src/rhi-webgl/WebGLEngine.test.ts
@@ -1,10 +1,10 @@
-import { WebCanvas } from "../src/WebCanvas";
-import { WebGLEngine } from "../src/WebGLEngine";
+import { expect } from "chai";
+import { WebGLEngine, WebCanvas } from "@oasis-engine/rhi-webgl";
 
 describe("webgl engine test", () => {
   it("create a webgl engine", () => {
     const canvas = document.createElement("canvas");
     const engine = new WebGLEngine(canvas);
-    expect(engine.canvas instanceof WebCanvas).toBeTruthy();
+    expect(engine).not.be.null;
   });
 });
