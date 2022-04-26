f244855590891a1456f7c77b30521492954a7f7a
gz65555 <gz65555@gmail.com>
2022-04-26 10:43:13 +0800

    feat: require all tests.ts

---
+++ tests/index.ts
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+// const fs = require("fs")
+// const path = require("path")
+
+fs.readdirSync(path.join(__dirname, "src")).forEach((file) => {
+  const root = path.join(__dirname, "src", file);
+  const stat = fs.statSync(root);
+  if (stat.isDirectory()) {
+    describe(file, () => {
+      fs.readdirSync(root).forEach((file) => {
+        const filePath = path.join(root, file);
+        const stat = fs.statSync(filePath);
+        if (stat.isFile() && filePath.endsWith(".test.ts")) {
+          require(filePath);
+        }
+      });
+    });
+  }
+});
