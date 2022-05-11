2cf7a2b4cf28d207b45aaccd1b0acbf1c09f3bf8
gz65555 <gz65555@gmail.com>
2022-05-11 11:45:09 +0800

    refactor: add scene loader

--- packages/loader/src/SceneLoader.ts
+++ packages/loader/src/SceneLoader.ts
@@ -1,23 +1,22 @@
-import {
-  AssetPromise,
-  Loader,
-  LoadItem,
-  resourceLoader,
-  ResourceManager,
-  SpriteAtlas,
-  AssetType,
-  Entity,
-  Scene
-} from "@oasis-engine/core";
-import { decode } from "@oasis-engine/resource-process";
+import { AssetPromise, Loader, LoadItem, resourceLoader, ResourceManager, AssetType, Scene } from "@oasis-engine/core";
+import { Vector3 } from "@oasis-engine/math";
+import { SceneParser } from "@oasis-engine/resource-process";
 
-// @resourceLoader(AssetType.Scene, ["prefab"], true)
-// class SceneLoader extends Loader<Scene> {
-//   load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<Scene> {
-//     return new AssetPromise((resolve, reject) => {
-//       this.request<ArrayBuffer>(item.url, { type: "arraybuffer" })
-//         .then((arraybuffer) => decode<Entity>(arraybuffer, resourceManager.engine).then(resolve).catch(reject))
-//         .catch(reject);
-//     });
-//   }
-// }
+@resourceLoader(AssetType.Scene, ["prefab"], true)
+class SceneLoader extends Loader<Scene> {
+  load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<Scene> {
+    const { engine } = resourceManager;
+    return new AssetPromise((resolve, reject) => {
+      this.request<any>(item.url, { type: "json" }).then((data) => {
+        engine.resourceManager.initVirtualResources(data.files);
+        SceneParser.parse(engine, data).then((scene) => {
+          const entity = scene.findEntityByName("Camera");
+          entity.transform.setPosition(10, 10, 10);
+          entity.transform.lookAt(new Vector3());
+          engine.sceneManager.activeScene = scene;
+        });
+      });
+      //
+    });
+  }
+}
