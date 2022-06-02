a16e6d5398a79a50f4e08c46c4cec909dd54c8cf
GuoLei1990 <gl3336563@163.com>
2022-06-02 11:13:45 +0800

    refactor: fix shader

--- packages/core/src/shaderlib/extra/skybox.vs.glsl
+++ packages/core/src/shaderlib/extra/skybox.vs.glsl
@@ -6,7 +6,7 @@ varying vec3 v_cubeUV;
 
 void main() {
 
-    v_cubeUV = vec3( -POSITION.x, POSITION.yz );
+    v_cubeUV = vec3( -POSITION.x, POSITION.yz );// TextureCube is left-hand,so x need inverse
     gl_Position = u_mvpNoscale * vec4( POSITION, 1.0 );
     gl_Position.z = gl_Position.w;
 
