869552f5c6704148aaba4d60d8142cce34737c09
singlecoder <singlecoder2014@gmail.com>
2022-06-06 17:52:38 +0800

    feat(text): add FontAtlas

---
+++ packages/core/src/2d/atlas/FontAtlas.ts
@@ -0,0 +1,57 @@
+import { RefObject } from "../../asset/RefObject";
+import { Engine } from "../../Engine";
+import { Texture2D } from "../../texture/Texture2D";
+
+export interface CharDef {
+  x: number,
+  y: number,
+  w: number,
+  h: number,
+  offsetX: number,
+  offsetY: number,
+  xAdvance: number
+}
+
+export interface CharDefDict {
+  [key: string]: CharDef;
+}
+
+/**
+ * Font Atlas.
+ */
+export class FontAtlas extends RefObject {
+  private _charDefDict: CharDefDict;
+  private _texture: Texture2D;
+
+  /**
+   * Constructor a FontAtlas.
+   * @param engine - Engine to which the FontAtlas belongs
+   */
+  constructor(engine: Engine, texture: Texture2D) {
+    super(engine);
+    this._charDefDict = {};
+    this._texture = texture;
+  }
+
+  /**
+   * @override
+   */
+  _onDestroy(): void {
+    this._texture.destroy();
+    this._texture = null;
+    this._charDefDict = {};
+  }
+
+  addChar(key: string, def: CharDef) {
+    this._charDefDict[key] = def;
+  }
+
+  getChar(key: string): CharDef | null {
+    return this._charDefDict[key];
+  }
+
+  getTexture(): Texture2D {
+    return this._texture;
+  }
+}
+
