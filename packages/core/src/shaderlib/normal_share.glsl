b2e1a3c46329a4acb8af42a275054320470960fe
zhuxudong <callzhuxudong@163.com>
2022-06-10 16:36:25 +0800

    refactor: rename

--- packages/core/src/shaderlib/normal_share.glsl
+++ packages/core/src/shaderlib/normal_share.glsl
@@ -1,7 +1,7 @@
 #ifndef OMIT_NORMAL
     #ifdef O3_HAS_NORMAL
         varying vec3 v_normal;
-        #if defined(O3_HAS_TANGENT) && ( defined(O3_NORMAL_TEXTURE) || defined(HAS_CLEARCOATNORMALTEXTURE) )
+        #if defined(O3_HAS_TANGENT) && ( defined(NORMALTEXTURE) || defined(HAS_CLEARCOATNORMALTEXTURE) )
             varying mat3 v_TBN;
         #endif
     #endif
