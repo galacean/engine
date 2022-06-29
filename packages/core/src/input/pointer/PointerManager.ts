69c11584f38bcc90d87c6956b7c745e7e1b36ed7
cptbtptpbcptdtptp <njktsmshh@163.com>
2022-06-29 14:41:40 +0800

    feat(core): add ability to clear stencil buffer

--- packages/core/src/input/pointer/PointerManager.ts
+++ packages/core/src/input/pointer/PointerManager.ts
@@ -244,7 +244,7 @@ export class PointerManager {
           // TODO: Only check which colliders have listened to the input.
           if (this._engine.physicsManager.raycast(camera.viewportPointToRay(point, ray), hitResult)) {
             return hitResult.entity;
-          } else if (camera.clearFlags === CameraClearFlags.Color) {
+          } else if (camera.clearFlags & CameraClearFlags.Color) {
             return null;
           }
         }
