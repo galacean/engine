186ace804fb39eb88dbc4e4d7f903fee0fe0a0ba
AZhan <njktsmshh@163.com>
2022-07-01 18:36:20 +0800

    Add SpriteRenderer drawMode property(support simple and sliced)  (#828)
    * feat: add sprite draw mode(support simple and sliced)

--- packages/core/src/2d/sprite/Sprite.ts
+++ packages/core/src/2d/sprite/Sprite.ts
@@ -1,37 +1,40 @@
 import { BoundingBox, MathUtil, Rect, Vector2, Vector4 } from "@oasis-engine/math";
 import { RefObject } from "../../asset/RefObject";
-import { BoolUpdateFlag } from "../../BoolUpdateFlag";
 import { Engine } from "../../Engine";
+import { ListenerUpdateFlag } from "../../ListenerUpdateFlag";
 import { Texture2D } from "../../texture/Texture2D";
 import { UpdateFlagManager } from "../../UpdateFlagManager";
+import { SpritePropertyDirtyFlag } from "../enums/SpriteDirtyFlag";
 
 /**
  * 2D sprite.
  */
 export class Sprite extends RefObject {
-  private static _rectangleTriangles: number[] = [0, 2, 1, 2, 0, 3];
+  /** @internal Conversion of space units to pixel units. */
+  static _pixelPerUnit: number = 100;
 
   /** The name of sprite. */
   name: string;
 
-  /** @internal */
-  _uv: Vector2[] = [new Vector2(), new Vector2(), new Vector2(), new Vector2()];
-  /** @internal */
-  _positions: Vector2[] = [new Vector2(), new Vector2(), new Vector2(), new Vector2()];
-  /** @internal */
-  _bounds: BoundingBox = new BoundingBox();
-  /** @internal */
-  _triangles: number[];
   /** @internal temp solution. */
   _assetID: number;
 
-  private _pixelsPerUnit: number;
+  private _width: number = undefined;
+  private _height: number = undefined;
+
+  private _positions: Vector2[] = [new Vector2(), new Vector2(), new Vector2(), new Vector2()];
+  private _uvs: Vector2[] = [new Vector2(), new Vector2(), new Vector2(), new Vector2()];
+  private _bounds: BoundingBox = new BoundingBox();
+
   private _texture: Texture2D = null;
   private _atlasRotated: boolean = false;
-  private _region: Rect = new Rect(0, 0, 1, 1);
-  private _pivot: Vector2 = new Vector2(0.5, 0.5);
   private _atlasRegion: Rect = new Rect(0, 0, 1, 1);
   private _atlasRegionOffset: Vector4 = new Vector4(0, 0, 0, 0);
+
+  private _region: Rect = new Rect(0, 0, 1, 1);
+  private _pivot: Vector2 = new Vector2(0.5, 0.5);
+  private _border: Vector4 = new Vector4(0, 0, 0, 0);
+
   private _dirtyFlag: DirtyFlag = DirtyFlag.all;
   private _updateFlagManager: UpdateFlagManager = new UpdateFlagManager();
 
@@ -45,20 +48,38 @@ export class Sprite extends RefObject {
   set texture(value: Texture2D) {
     if (this._texture !== value) {
       this._texture = value;
-      this._setDirtyFlagTrue(DirtyFlag.positions);
+      this._dispatchSpriteChange(SpritePropertyDirtyFlag.texture);
     }
   }
 
   /**
-   *  Bounding volume of the sprite.
-   *  @remarks The returned bounds should be considered deep-read-only.
+   * The width of the sprite (in world coordinates).
    */
-  get bounds(): Readonly<BoundingBox> {
-    if (this._isContainDirtyFlag(DirtyFlag.positions) && this._texture) {
-      this._updatePositionsAndBounds();
-      this._setDirtyFlagFalse(DirtyFlag.positions);
+  get width(): number {
+    this._width === undefined && this._calDefaultSize();
+    return this._width;
+  }
+
+  set width(value: number) {
+    if (this._width !== value) {
+      this._width = value;
+      this._dispatchSpriteChange(SpritePropertyDirtyFlag.size);
     }
-    return this._bounds;
+  }
+
+  /**
+   * The height of the sprite (in world coordinates).
+   */
+  get height(): number {
+    this._height === undefined && this._calDefaultSize();
+    return this._height;
+  }
+
+  set height(value: number) {
+    if (this._height !== value) {
+      this._height = value;
+      this._dispatchSpriteChange(SpritePropertyDirtyFlag.size);
+    }
   }
 
   /**
@@ -71,7 +92,6 @@ export class Sprite extends RefObject {
   set atlasRotated(value: boolean) {
     if (this._atlasRotated != value) {
       this._atlasRotated = value;
-      this._setDirtyFlagTrue(DirtyFlag.positions | DirtyFlag.uv);
     }
   }
 
@@ -86,7 +106,7 @@ export class Sprite extends RefObject {
     const x = MathUtil.clamp(value.x, 0, 1);
     const y = MathUtil.clamp(value.y, 0, 1);
     this._atlasRegion.set(x, y, MathUtil.clamp(value.width, 0, 1 - x), MathUtil.clamp(value.height, 0, 1 - y));
-    this._setDirtyFlagTrue(DirtyFlag.positions | DirtyFlag.uv);
+    this._dispatchSpriteChange(SpritePropertyDirtyFlag.atlasRegion);
   }
 
   /**
@@ -100,23 +120,7 @@ export class Sprite extends RefObject {
     const x = MathUtil.clamp(value.x, 0, 1);
     const y = MathUtil.clamp(value.y, 0, 1);
     this._atlasRegionOffset.set(x, y, MathUtil.clamp(value.z, 0, 1 - x), MathUtil.clamp(value.w, 0, 1 - y));
-    this._setDirtyFlagTrue(DirtyFlag.positions | DirtyFlag.uv);
-  }
-
-  /**
-   * Location of the sprite's center point in the rectangle region, specified in normalized.
-   */
-  get pivot(): Vector2 {
-    return this._pivot;
-  }
-
-  set pivot(value: Vector2) {
-    const pivot = this._pivot;
-    const { x, y } = value;
-    if (pivot === value || pivot.x !== x || pivot.y !== y) {
-      pivot.set(x, y);
-      this._setDirtyFlagTrue(DirtyFlag.positions);
-    }
+    this._dispatchSpriteChange(SpritePropertyDirtyFlag.atlasRegionOffset);
   }
 
   /**
@@ -131,21 +135,47 @@ export class Sprite extends RefObject {
     const x = MathUtil.clamp(value.x, 0, 1);
     const y = MathUtil.clamp(value.y, 0, 1);
     region.set(x, y, MathUtil.clamp(value.width, 0, 1 - x), MathUtil.clamp(value.height, 0, 1 - y));
-    this._setDirtyFlagTrue(DirtyFlag.positions | DirtyFlag.uv);
+    this._dispatchSpriteChange(SpritePropertyDirtyFlag.region);
   }
 
   /**
-   * The number of pixels in the sprite that correspond to one unit in world space.
+   * Location of the sprite's center point in the rectangle region, specified in normalized.
+   * The origin is at the bottom left and the default value is (0.5, 0.5).
    */
-  get pixelsPerUnit(): number {
-    return this._pixelsPerUnit;
+  get pivot(): Vector2 {
+    return this._pivot;
   }
 
-  set pixelsPerUnit(value: number) {
-    if (this._pixelsPerUnit !== value) {
-      this._pixelsPerUnit = value;
-      this._setDirtyFlagTrue(DirtyFlag.positions);
+  set pivot(value: Vector2) {
+    const pivot = this._pivot;
+    if (pivot === value) {
+      this._dispatchSpriteChange(SpritePropertyDirtyFlag.pivot);
+    } else {
+      const { x, y } = value;
+      if (pivot.x !== x || pivot.y !== y) {
+        pivot.set(x, y);
+        this._dispatchSpriteChange(SpritePropertyDirtyFlag.pivot);
+      }
     }
+  }
+
+  /**
+   * Get the border of the sprite.
+   *  x      y       z     w
+   *  |      |       |     |
+   * Left, bottom, right, top.
+   * @remark only use in sliced mode.
+   */
+  get border(): Vector4 {
+    return this._border;
+  }
+
+  set border(value: Vector4) {
+    const border = this._border;
+    const x = MathUtil.clamp(value.x, 0, 1);
+    const y = MathUtil.clamp(value.y, 0, 1);
+    border.set(x, y, MathUtil.clamp(value.z, 0, 1 - x), MathUtil.clamp(value.w, 0, 1 - y));
+    this._dispatchSpriteChange(SpritePropertyDirtyFlag.border);
   }
 
   /**
@@ -154,7 +184,7 @@ export class Sprite extends RefObject {
    * @param texture - Texture from which to obtain the Sprite
    * @param region - Rectangle region of the texture to use for the Sprite, specified in normalized
    * @param pivot - Sprite's pivot point relative to its graphic rectangle, specified in normalized
-   * @param pixelsPerUnit - The number of pixels in the Sprite that correspond to one unit in world space
+   * @param border - Boundaries when using Slice DrawMode, specified in normalized
    * @param name - The name of Sprite
    */
   constructor(
@@ -162,19 +192,15 @@ export class Sprite extends RefObject {
     texture: Texture2D = null,
     region: Rect = null,
     pivot: Vector2 = null,
-    pixelsPerUnit: number = 128,
+    border: Vector4 = null,
     name: string = null
   ) {
     super(engine);
-
-    this.name = name;
     this._texture = texture;
-    this._pixelsPerUnit = pixelsPerUnit;
-
-    region && this._region.copyFrom(region);
-    pivot && this._pivot.copyFrom(pivot);
-
-    this._triangles = Sprite._rectangleTriangles;
+    region && region.copyFrom(this._region);
+    pivot && pivot.copyFrom(this._pivot);
+    border && border.copyFrom(this._border);
+    this.name = name;
   }
 
   /**
@@ -182,14 +208,7 @@ export class Sprite extends RefObject {
    * @returns Cloned sprite
    */
   clone(): Sprite {
-    const cloneSprite = new Sprite(
-      this._engine,
-      this._texture,
-      this._region,
-      this._pivot,
-      this._pixelsPerUnit,
-      this.name
-    );
+    const cloneSprite = new Sprite(this._engine, this._texture, this._region, this._pivot, this._border, this.name);
     cloneSprite._assetID = this._assetID;
     cloneSprite._atlasRotated = this._atlasRotated;
     cloneSprite._atlasRegion.copyFrom(this._atlasRegion);
@@ -200,8 +219,32 @@ export class Sprite extends RefObject {
   /**
    * @internal
    */
-  _registerUpdateFlag(): BoolUpdateFlag {
-    return this._updateFlagManager.createFlag(BoolUpdateFlag);
+  _registerUpdateFlag(): ListenerUpdateFlag {
+    return this._updateFlagManager.createFlag(ListenerUpdateFlag);
+  }
+
+  /**
+   * @internal
+   */
+  _getPositions(): Vector2[] {
+    this._dirtyFlag & DirtyFlag.positions && this._updatePositions();
+    return this._positions;
+  }
+
+  /**
+   * @internal
+   */
+  _getUVs(): Vector2[] {
+    this._dirtyFlag & DirtyFlag.uvs && this._updateUVs();
+    return this._uvs;
+  }
+
+  /**
+   * @internal
+   */
+  _getBounds(): BoundingBox {
+    this._dirtyFlag & DirtyFlag.positions && this._updatePositions();
+    return this._bounds;
   }
 
   /**
@@ -213,98 +256,98 @@ export class Sprite extends RefObject {
     }
   }
 
-  /**
-   * Update positions and bounds.
-   */
-  private _updatePositionsAndBounds(): void {
-    const { _texture: texture, _bounds: bounds } = this;
-    if (texture) {
-      const { _atlasRegion: atlasRegion, _pivot: pivot } = this;
-      const { x: blankLeft, y: blankTop, z: blankRight, w: blankBottom } = this._atlasRegionOffset;
-      const { x: regionX, y: regionY, width: regionW, height: regionH } = this._region;
-      const regionRight = 1 - regionX - regionW;
-      const regionBottom = 1 - regionY - regionH;
-      // Real rendering size.
-      const realRenderW =
-        (texture.width * atlasRegion.width * regionW) / ((1 - blankLeft - blankRight) * this._pixelsPerUnit);
-      const realRenderH =
-        (texture.height * atlasRegion.height * regionH) / ((1 - blankTop - blankBottom) * this._pixelsPerUnit);
-      // Coordinates of the four boundaries.
-      const left = (Math.max(blankLeft - regionX, 0) / regionW - pivot.x) * realRenderW;
-      const right = (1 - Math.max(blankRight - regionRight, 0) / regionW - pivot.x) * realRenderW;
-      const top = (1 - Math.max(blankTop - regionBottom, 0) / regionH - pivot.y) * realRenderH;
-      const bottom = (Math.max(blankBottom - regionY, 0) / regionH - pivot.y) * realRenderH;
-      // Assign values ​​to _positions
-      const positions = this._positions;
-      // Top-left.
-      positions[0].set(left, top);
-      // Top-right.
-      positions[1].set(right, top);
-      // Bottom-right.
-      positions[2].set(right, bottom);
-      // Bottom-left.
-      positions[3].set(left, bottom);
-
-      // Update bounds.
-      bounds.min.set(left, bottom, 0);
-      bounds.max.set(right, top, 0);
-    } else {
-      // Update bounds.
-      bounds.min.set(0, 0, 0);
-      bounds.max.set(0, 0, 0);
+  private _calDefaultSize(): void {
+    if (this._texture) {
+      const { _texture, _atlasRegion, _atlasRegionOffset, _region } = this;
+      this.width =
+        (((_texture.width * _atlasRegion.width) / (1 - _atlasRegionOffset.x - _atlasRegionOffset.z)) * _region.width) /
+        Sprite._pixelPerUnit;
+      this.height =
+        (((_texture.height * _atlasRegion.height) / (1 - _atlasRegionOffset.y - _atlasRegionOffset.w)) *
+          _region.height) /
+        Sprite._pixelPerUnit;
     }
   }
 
-  /**
-   * @internal
-   */
-  _updateMesh(): void {
-    if (this._isContainDirtyFlag(DirtyFlag.positions)) {
-      this._updatePositionsAndBounds();
+  private _updatePositions(): void {
+    const blank = this._atlasRegionOffset;
+    const { x: regionX, y: regionY, width: regionW, height: regionH } = this._region;
+    const regionRight = 1 - regionX - regionW;
+    const regionBottom = 1 - regionY - regionH;
+    const left = Math.max(blank.x - regionX, 0) / regionW;
+    const bottom = Math.max(blank.w - regionY, 0) / regionH;
+    const right = 1 - Math.max(blank.z - regionRight, 0) / regionW;
+    const top = 1 - Math.max(blank.y - regionBottom, 0) / regionH;
+
+    // Update positions.
+    // ---------------
+    //  2 - 3
+    //  |   |
+    //  0 - 1
+    // ---------------
+    const positions = this._positions;
+    positions[0].set(left, bottom);
+    positions[1].set(right, bottom);
+    positions[2].set(left, top);
+    positions[3].set(right, top);
+
+    const { min, max } = this._bounds;
+    min.set(left, bottom, 0);
+    max.set(right, top, 0);
+    this._dirtyFlag &= ~DirtyFlag.positions;
+  }
+
+  private _updateUVs(): void {
+    const { _uvs: uv, _atlasRegionOffset: atlasRegionOffset } = this;
+    const { x: regionX, y: regionY, width: regionW, height: regionH } = this._region;
+    const regionRight = 1 - regionX - regionW;
+    const regionBottom = 1 - regionY - regionH;
+    const { x: atlasRegionX, y: atlasRegionY, width: atlasRegionW, height: atlasRegionH } = this._atlasRegion;
+    const { x: offsetLeft, y: offsetTop, z: offsetRight, w: offsetBottom } = atlasRegionOffset;
+    const realWidth = atlasRegionW / (1 - offsetLeft - offsetRight);
+    const realHeight = atlasRegionH / (1 - offsetTop - offsetBottom);
+    // Coordinates of the four boundaries.
+    const left = Math.max(regionX - offsetLeft, 0) * realWidth + atlasRegionX;
+    const top = Math.max(regionBottom - offsetTop, 0) * realHeight + atlasRegionY;
+    const right = atlasRegionW + atlasRegionX - Math.max(regionRight - offsetRight, 0) * realWidth;
+    const bottom = atlasRegionH + atlasRegionY - Math.max(regionY - offsetBottom, 0) * realHeight;
+    const { x: borderLeft, y: borderBottom, z: borderRight, w: borderTop } = this._border;
+    // Left-Bottom
+    uv[0].set(left, bottom);
+    // Border ( Left-Bottom )
+    uv[1].set(
+      (regionX - offsetLeft + borderLeft * regionW) * realWidth + atlasRegionX,
+      atlasRegionH + atlasRegionY - (regionY - offsetBottom + borderBottom * regionH) * realHeight
+    );
+    // Border ( Right-Top )
+    uv[2].set(
+      atlasRegionW + atlasRegionX - (regionRight - offsetRight + borderRight * regionW) * realWidth,
+      (regionBottom - offsetTop + borderTop * regionH) * realHeight + atlasRegionY
+    );
+    // Right-Top
+    uv[3].set(right, top);
+    this._dirtyFlag &= ~DirtyFlag.uvs;
+  }
+
+  private _dispatchSpriteChange(type: SpritePropertyDirtyFlag): void {
+    switch (type) {
+      case SpritePropertyDirtyFlag.atlasRegionOffset:
+      case SpritePropertyDirtyFlag.region:
+        this._dirtyFlag |= DirtyFlag.all;
+        break;
+      case SpritePropertyDirtyFlag.atlasRegion:
+      case SpritePropertyDirtyFlag.border:
+        this._dirtyFlag |= DirtyFlag.uvs;
+        break;
+      default:
+        break;
     }
-
-    if (this._isContainDirtyFlag(DirtyFlag.uv)) {
-      const { _uv: uv, _atlasRegionOffset: atlasRegionOffset } = this;
-      const { x: regionX, y: regionY, width: regionW, height: regionH } = this._region;
-      const regionRight = 1 - regionX - regionW;
-      const regionBottom = 1 - regionY - regionH;
-      const { x: atlasRegionX, y: atlasRegionY, width: atlasRegionW, height: atlasRegionH } = this._atlasRegion;
-      const { x: offsetLeft, y: offsetTop, z: offsetRight, w: offsetBottom } = atlasRegionOffset;
-      const realWidth = atlasRegionW / (1 - offsetLeft - offsetRight);
-      const realHeight = atlasRegionH / (1 - offsetTop - offsetBottom);
-      // Coordinates of the four boundaries.
-      const left = Math.max(regionX - offsetLeft, 0) * realWidth + atlasRegionX;
-      const top = Math.max(regionBottom - offsetTop, 0) * realHeight + atlasRegionY;
-      const right = atlasRegionW + atlasRegionX - Math.max(regionRight - offsetRight, 0) * realWidth;
-      const bottom = atlasRegionH + atlasRegionY - Math.max(regionY - offsetBottom, 0) * realHeight;
-      // Top-left.
-      uv[0].set(left, top);
-      // Top-right.
-      uv[1].set(right, top);
-      // Bottom-right.
-      uv[2].set(right, bottom);
-      // Bottom-left.
-      uv[3].set(left, bottom);
-    }
-    this._setDirtyFlagFalse(DirtyFlag.all);
-  }
-
-  private _isContainDirtyFlag(type: number): boolean {
-    return (this._dirtyFlag & type) != 0;
-  }
-
-  private _setDirtyFlagTrue(type: number): void {
-    this._dirtyFlag |= type;
-    this._updateFlagManager.dispatch();
-  }
-
-  private _setDirtyFlagFalse(type: number): void {
-    this._dirtyFlag &= ~type;
+    this._updateFlagManager.dispatch(type);
   }
 }
 
 enum DirtyFlag {
   positions = 0x1,
-  uv = 0x2,
+  uvs = 0x2,
   all = 0x3
 }
