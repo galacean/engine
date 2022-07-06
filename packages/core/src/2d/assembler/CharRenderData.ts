26ea7c496df35d29c80befdd9d1d83c42865301d
singlecoder <singlecoder2014@gmail.com>
2022-07-06 16:42:03 +0800

    feat(text): opt code

--- packages/core/src/2d/assembler/CharRenderData.ts
+++ packages/core/src/2d/assembler/CharRenderData.ts
@@ -2,11 +2,11 @@ import { Vector3 } from "@oasis-engine/math";
 import { Texture2D } from "../../texture";
 import { RenderData2D } from "../data/RenderData2D";
 
+/**
+ * @internal
+ */
 export interface CharRenderData {
-  /** @internal */
   texture: Texture2D;
-  /** @internal */
   renderData: RenderData2D;
-  /** @intarnal */
   localPositions: Vector3[];
 }
