fc0e330c5f348b28bea053cb461b68b13496b21a
Hu Song <gz65555@gmail.com>
2022-07-20 18:16:00 +0800

    Dev/asset file (#878)
    * feat: add scene loader

--- packages/loader/src/SceneLoader.ts
+++ packages/loader/src/SceneLoader.ts
@@ -1,4 +1,15 @@
-import { AssetPromise, Loader, LoadItem, resourceLoader, ResourceManager, AssetType, Scene } from "@oasis-engine/core";
+import {
+  AssetPromise,
+  Loader,
+  LoadItem,
+  resourceLoader,
+  ResourceManager,
+  AssetType,
+  Scene,
+  BackgroundMode,
+  SkyBoxMaterial,
+  PrimitiveMesh
+} from "@oasis-engine/core";
 import { Vector3 } from "@oasis-engine/math";
 import { SceneParser } from "@oasis-engine/resource-process";
 
@@ -11,9 +22,42 @@ class SceneLoader extends Loader<Scene> {
         engine.resourceManager.initVirtualResources(data.files);
         return SceneParser.parse(engine, data).then((scene) => {
           const entity = scene.findEntityByName("Camera");
-          entity.transform.setPosition(10, 10, 10);
-          entity.transform.lookAt(new Vector3());
-          engine.sceneManager.activeScene = scene;
+          const ambient = data.scene.ambient;
+          if (ambient.ambientLight) {
+            resourceManager.getResourceByRef<any>(data.scene.ambient.ambientLight).then((light) => {
+              scene.ambientLight = light;
+            });
+          }
+          scene.ambientLight.diffuseIntensity = ambient.diffuseIntensity;
+          scene.ambientLight.specularIntensity = ambient.specularIntensity;
+
+          const background = data.scene.background;
+          scene.background.mode = background.mode;
+          switch (scene.background.mode) {
+            case BackgroundMode.SolidColor:
+              scene.background.solidColor.copyFrom(background.color);
+              break;
+            case BackgroundMode.Sky:
+              if (background.sky) {
+                resourceManager.getResourceByRef<any>(background.sky).then((light) => {
+                  const sky = scene.background.sky;
+                  const skyMaterial = new SkyBoxMaterial(engine);
+                  skyMaterial.textureCubeMap = light.specularTexture;
+                  skyMaterial.textureDecodeRGBM = true;
+                  sky.material = skyMaterial;
+                  sky.mesh = PrimitiveMesh.createCuboid(engine, 1, 1, 1);
+                });
+              }
+              break;
+            case BackgroundMode.Texture:
+              if (background.texture) {
+                resourceManager.getResourceByRef<any>(background.texture).then((texture) => {
+                  scene.background.texture = texture;
+                });
+              }
+              break;
+          }
+
           resolve(scene);
         });
       });
