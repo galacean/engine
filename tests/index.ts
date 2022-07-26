b2ac2078d59fde5ff317682aa25d3b57fbf4cc56
yangfengzzz <yangfengzzz@hotmail.com>
2022-07-26 17:52:54 +0800

    feat: texture and material unit tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,18 +1,18 @@
 import fs from "fs";
 import path from "path";
 
-fs.readdirSync(path.join(__dirname, "src")).forEach((file) => {
-  const root = path.join(__dirname, "src", file);
-  const stat = fs.statSync(root);
-  if (stat.isDirectory()) {
-    describe(file, () => {
-      fs.readdirSync(root).forEach((file) => {
-        const filePath = path.join(root, file);
-        const stat = fs.statSync(filePath);
-        if (stat.isFile() && filePath.endsWith(".test.ts")) {
-          require(filePath);
-        }
+function searchTests(root: string) {
+  fs.readdirSync(root).forEach((file) => {
+    const filePath = path.join(root, file);
+    const stat = fs.statSync(filePath);
+    if (stat.isFile() && filePath.endsWith(".test.ts")) {
+      require(filePath);
+    } else if (stat.isDirectory()) {
+      describe(file, () => {
+        searchTests(filePath);
       });
-    });
-  }
-});
+    }
+  });
+}
+
+searchTests(path.join(__dirname, "src"));
