dd05cbf4e151285151794d8fc4b00b3472c3ef0a
ChenMo <gl3336563@163.com>
2022-08-22 14:08:48 +0800

    Optmization TextRender performance cause by sort (#966)
    * refactor: opt text sort performance

--- packages/core/src/RenderPipeline/RenderQueue.ts
+++ packages/core/src/RenderPipeline/RenderQueue.ts
@@ -4,11 +4,10 @@ import { Layer } from "../Layer";
 import { Material } from "../material/Material";
 import { Shader } from "../shader";
 import { ShaderMacroCollection } from "../shader/ShaderMacroCollection";
+import { MeshRenderElement } from "./MeshRenderElement";
 import { RenderElement } from "./RenderElement";
 import { SpriteBatcher } from "./SpriteBatcher";
 import { SpriteElement } from "./SpriteElement";
-
-type Item = RenderElement | SpriteElement;
 
 /**
  * Render queue.
@@ -17,18 +16,18 @@ export class RenderQueue {
   /**
    * @internal
    */
-  static _compareFromNearToFar(a: Item, b: Item): number {
+  static _compareFromNearToFar(a: RenderElement, b: RenderElement): number {
     return a.component.priority - b.component.priority || a.component._distanceForSort - b.component._distanceForSort;
   }
 
   /**
    * @internal
    */
-  static _compareFromFarToNear(a: Item, b: Item): number {
+  static _compareFromFarToNear(a: RenderElement, b: RenderElement): number {
     return a.component.priority - b.component.priority || b.component._distanceForSort - a.component._distanceForSort;
   }
 
-  readonly items: Item[] = [];
+  readonly items: RenderElement[] = [];
   private _spriteBatcher: SpriteBatcher;
 
   constructor(engine: Engine) {
@@ -38,7 +37,7 @@ export class RenderQueue {
   /**
    * Push a render element.
    */
-  pushPrimitive(element: RenderElement | SpriteElement): void {
+  pushPrimitive(element: RenderElement): void {
     this.items.push(element);
   }
 
@@ -62,11 +61,11 @@ export class RenderQueue {
         continue;
       }
 
-      if (!!(item as RenderElement).mesh) {
+      if (!!(item as MeshRenderElement).mesh) {
         this._spriteBatcher.flush(camera, replaceMaterial);
 
         const compileMacros = Shader._compileMacros;
-        const element = <RenderElement>item;
+        const element = <MeshRenderElement>item;
         const renderer = element.component;
         const material = element.material;
         const rendererData = renderer.shaderData;
@@ -109,7 +108,7 @@ export class RenderQueue {
           } else if (switchProgram) {
             program.uploadTextures(program.cameraUniformBlock, cameraData);
           }
-          
+
           if (program._uploadRenderer !== renderer) {
             program.uploadAll(program.rendererUniformBlock, rendererData);
             program._uploadRenderer = renderer;
