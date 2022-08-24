f1e52cdedcb64ce74dc5a0cc1675cfe34fc5ad09
GuoLei1990 <gl3336563@163.com>
2022-08-25 01:19:19 +0800

    refactor: opt code

--- packages/loader/src/gltf/parser/AnimationParser.ts
+++ packages/loader/src/gltf/parser/AnimationParser.ts
@@ -115,10 +115,6 @@ export class AnimationParser extends Parser {
         }
 
         const curve = this._addCurve(interpolableValueType, gltfChannel, sampleDataCollection);
-        if (compType === SkinnedMeshRenderer) {
-          console.log(relativePath + "    " + propertyName);
-          debugger;
-        }
         animationClip.addCurveBinding(relativePath, compType, propertyName, curve);
       }
 
