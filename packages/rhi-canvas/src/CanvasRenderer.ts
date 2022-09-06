2690f8e07db0efd1eff4fd6e3739cf10a0c211b0
singlecoder <singlecoder2014@gmail.com>
2022-09-06 19:40:18 +0800

    feat(canvas2d): opt code

--- packages/rhi-canvas/src/CanvasRenderer.ts
+++ packages/rhi-canvas/src/CanvasRenderer.ts
@@ -5,13 +5,14 @@ import {
   Engine,
   IHardwareRenderer,
   IPlatformTexture2D,
+  Renderer,
   RenderTarget,
   SpriteElement,
   SpriteRenderer,
   TextRenderer,
   Texture2D
 } from "@oasis-engine/core";
-import { Color, Quaternion, Vector3 } from "@oasis-engine/math";
+import { Color, Quaternion, Vector2, Vector3, Vector4 } from "@oasis-engine/math";
 import { Canvas2dCanvas } from "./Canvas2dCanvas";
 import { CanvasCapability } from "./CanvasCapability";
 import { CanvasTexture2D } from "./CanvasTexture2D";
@@ -22,7 +23,11 @@ export class CanvasRenderer implements IHardwareRenderer {
   private static _tempVec32: Vector3 = new Vector3();
   private static _tempVec33: Vector3 = new Vector3();
   private static _tempVec34: Vector3 = new Vector3();
+  private static _tempVec4: Vector4 = new Vector4();
   private static _tempQuat: Quaternion = new Quaternion();
+  private static _PI: number = Math.PI;
+  private static _doublePI: number = Math.PI * 2;
+  private static _reciprocalPI: number = 1.0 / Math.PI;
 
   private _ctx: CanvasRenderingContext2D;
   private _capability: CanvasCapability;
@@ -81,10 +86,43 @@ export class CanvasRenderer implements IHardwareRenderer {
 
   private _drawImage(element: SpriteElement, camera: Camera) {
     const renderer = <SpriteRenderer>element.component;
-    const { sprite } = renderer;
+    const { sprite, width, height } = renderer;
+    const { renderData } = element;
+    const { uvs } = renderData;
+    const { pivot } = sprite;
     // @ts-ignore
     const image = sprite.texture._platformTexture._canvasTexture;
+    const pivotWidth = pivot.x * width;
+    const pivotHeight = pivot.y * height;
+    const vec4 = CanvasRenderer._tempVec4;
+    vec4.set(-pivotWidth, height - pivotHeight, width - pivotWidth, -pivotHeight);
+    this._draw(camera, renderer, image, uvs[2], uvs[1], vec4, renderData.color);
+  }
 
+  private _drawText(element: SpriteElement, camera: Camera) {
+    const renderer = <TextRenderer>element.component;
+    const { renderData } = element;
+    const { uvs } = renderData;
+    // @ts-ignore
+    const image = element.texture._platformTexture._canvasTexture;
+    const vec4 = CanvasRenderer._tempVec4;
+    // @ts-ignore
+    const positions = renderer._charRenderDatas[element.dataIndex].localPositions;
+    const ltPosition = positions[0];
+    const rbPosition = positions[2];
+    vec4.set(ltPosition.x, ltPosition.y, rbPosition.x, rbPosition.y);
+    this._draw(camera, renderer, image, uvs[0], uvs[2], vec4, renderData.color);
+  }
+
+  private _draw(
+    camera: Camera,
+    renderer: Renderer,
+    image: HTMLCanvasElement,
+    ltUV: Vector2,
+    rbUV: Vector2,
+    vec4: Vector4,
+    color: Color
+  ) {
     const transform = renderer.entity.transform;
     const worldMatrix = transform.worldMatrix;
     const translate = CanvasRenderer._tempVec30;
@@ -98,127 +136,50 @@ export class CanvasRenderer implements IHardwareRenderer {
     const offsetY = translate.y * ratio;
     camera.worldToScreenPoint(translate, translate);
 
+    // Handle transform.
     const { ctx } = this;
     const { x, y } = translate;
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.translate(x, y);
     ctx.rotate(-euler.z);
+    // @ts-ignore
     const scaleX = renderer.flipX ? -scale.x : scale.x;
+    // @ts-ignore
     const scaleY = renderer.flipY ? -scale.y : scale.y;
-    // Handle rotation axis X and Y.
-    const percentX = Math.abs(euler.x % (Math.PI * 2));
-    let rotationScaleY = 1;
-    if (percentX <= Math.PI) {
-      const t = percentX / Math.PI;
-      rotationScaleY = 1 - 2 * t;
-    } else {
-      const t = (percentX - Math.PI) / Math.PI;
-      rotationScaleY = -1 + 2 * t;
-    }
-    const percentY = Math.abs(euler.y % (Math.PI * 2));
-    let rotationScaleX = 1;
-    if (percentY <= Math.PI) {
-      const t = percentY / Math.PI;
-      rotationScaleX = 1 - 2 * t;
-    } else {
-      const t = (percentY - Math.PI) / Math.PI;
-      rotationScaleX = -1 + 2 * t;
-    }
-    ctx.scale(scaleX * rotationScaleX, scaleY * rotationScaleY);
+    // Handle rotation around axis X and Y.
+    ctx.scale(scaleX * this._calculateScaleByRadian(euler.y), scaleY * this._calculateScaleByRadian(euler.x));
 
-    const { renderData } = element;
-    const { uvs } = renderData;
+    // Handle UV.
     const { width, height } = image;
-    const ltUV = uvs[2];
-    const rbUV = uvs[1];
     const sx = ltUV.x * width;
     const sy = ltUV.y * height;
     const sWidth = (rbUV.x - ltUV.x) * width;
     const sHeight = (rbUV.y - ltUV.y) * height;
 
-    const { pivot } = sprite;
-    const { width: renderWidth, height: renderHeight } = renderer;
-    const pivotWidth = pivot.x * renderWidth;
-    const pivotHeight = pivot.y * renderHeight;
     const ltVec3 = CanvasRenderer._tempVec33;
-    ltVec3.set(-pivotWidth, renderHeight - pivotHeight, 0);
+    ltVec3.set(vec4.x, vec4.y, 0);
     camera.worldToScreenPoint(ltVec3, ltVec3);
     const rbVec3 = CanvasRenderer._tempVec34;
-    rbVec3.set(renderWidth - pivotWidth, -pivotHeight, 0);
+    rbVec3.set(vec4.z, vec4.w, 0);
     camera.worldToScreenPoint(rbVec3, rbVec3);
     const { x: ltX, y: ltY } = ltVec3;
 
-    ctx.globalAlpha = renderData.color.a;
+    ctx.globalAlpha = color.a;
     ctx.drawImage(image, sx, sy, sWidth, sHeight, ltX - x + offsetX, ltY - y - offsetY, rbVec3.x - ltX, rbVec3.y - ltY);
     ctx.globalAlpha = 1.0;
   }
 
-  private _drawText(element: SpriteElement, camera: Camera) {
-    const { component } = element;
-    // @ts-ignore
-    const image = element.texture._platformTexture._canvasTexture;
-
-    const transform = component.entity.transform;
-    const worldMatrix = transform.worldMatrix;
-    const translate = CanvasRenderer._tempVec30;
-    const quat = CanvasRenderer._tempQuat;
-    const scale = CanvasRenderer._tempVec31;
-    worldMatrix.decompose(translate, quat, scale);
-    const euler = CanvasRenderer._tempVec32;
-    quat.toEuler(euler);
-    const ratio = (this._webCanvas.height * 0.5) / camera.orthographicSize;
-    const offsetX = translate.x * ratio;
-    const offsetY = translate.y * ratio;
-    camera.worldToScreenPoint(translate, translate);
-
-    const { ctx } = this;
-    const { x, y } = translate;
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.translate(x, y);
-    ctx.rotate(-euler.z);
-    // Handle rotation axis X and Y.
-    const percentX = Math.abs(euler.x % (Math.PI * 2));
-    let rotationScaleY = 1;
-    if (percentX <= Math.PI) {
-      const t = percentX / Math.PI;
-      rotationScaleY = 1 - 2 * t;
+  private _calculateScaleByRadian(radian: number): number {
+    const { _PI: PI } = CanvasRenderer;
+    radian = Math.abs(radian % CanvasRenderer._doublePI);
+    let scale = 1;
+    if (radian <= PI) {
+      const t = radian * CanvasRenderer._reciprocalPI;
+      scale = 1 - 2 * t;
     } else {
-      const t = (percentX - Math.PI) / Math.PI;
-      rotationScaleY = -1 + 2 * t;
+      const t = (radian - PI) * CanvasRenderer._reciprocalPI;
+      scale = -1 + 2 * t;
     }
-    const percentY = Math.abs(euler.y % (Math.PI * 2));
-    let rotationScaleX = 1;
-    if (percentY <= Math.PI) {
-      const t = percentY / Math.PI;
-      rotationScaleX = 1 - 2 * t;
-    } else {
-      const t = (percentY - Math.PI) / Math.PI;
-      rotationScaleX = -1 + 2 * t;
-    }
-    ctx.scale(scale.x, scale.y);
-
-    const { renderData } = element;
-    const { uvs } = renderData;
-    const { width, height } = image;
-    const ltUV = uvs[0];
-    const rbUV = uvs[2];
-    const sx = ltUV.x * width;
-    const sy = ltUV.y * height;
-    const sWidth = (rbUV.x - ltUV.x) * width;
-    const sHeight = (rbUV.y - ltUV.y) * height;
-
-    // @ts-ignore
-    const positions = component._charRenderDatas[element.dataIndex].localPositions;
-    const ltVec3 = CanvasRenderer._tempVec33;
-    ltVec3.set(positions[0].x, positions[0].y, 0);
-    camera.worldToScreenPoint(ltVec3, ltVec3);
-    const rbVec3 = CanvasRenderer._tempVec34;
-    rbVec3.set(positions[2].x, positions[2].y, 0);
-    camera.worldToScreenPoint(rbVec3, rbVec3);
-    const { x: ltX, y: ltY } = ltVec3;
-
-    ctx.globalAlpha = renderData.color.a;
-    ctx.drawImage(image, sx, sy, sWidth, sHeight, ltX - x + offsetX, ltY - y - offsetY, rbVec3.x - ltX, rbVec3.y - ltY);
-    ctx.globalAlpha = 1.0;
+    return scale;
   }
 }
