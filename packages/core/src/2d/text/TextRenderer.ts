f7189f1ecf0e2a2a553212bcbc85786fabbcf9bb
singlecoder <singlecoder2014@gmail.com>
2022-09-09 11:27:35 +0800

    Fix set font size not valid error (#1019)
    * fix(text): fix set font size not valid error

--- packages/core/src/2d/text/TextRenderer.ts
+++ packages/core/src/2d/text/TextRenderer.ts
@@ -27,13 +27,12 @@ export class TextRenderer extends Renderer implements ICustomClone {
 
   /** @internal */
   @assignmentClone
-  _charFont: Font = null;
+  _styleFont: Font = null;
   /** @internal */
   @ignoreClone
-  _charRenderDatas: Array<CharRenderData> = [];
-
+  _charRenderDatas: CharRenderData[] = [];
   @ignoreClone
-  _dirtyFlag: number = DirtyFlag.Font | DirtyFlag.LocalPositionBounds | DirtyFlag.WorldPosition | DirtyFlag.WorldBounds;
+  _dirtyFlag: number = DirtyFlag.Font | DirtyFlag.WorldBounds;
   /** @internal */
   @ignoreClone
   _isWorldMatrixDirty: ListenerUpdateFlag;
@@ -266,15 +265,12 @@ export class TextRenderer extends Renderer implements ICustomClone {
    * The bounding volume of the TextRenderer.
    */
   get bounds(): BoundingBox {
-    const isFontDirty = this._isContainDirtyFlag(DirtyFlag.Font);
-    const isLocalPositionBoundsDirty = this._isContainDirtyFlag(DirtyFlag.LocalPositionBounds);
-    const isWorldBoundsDirty = this._isContainDirtyFlag(DirtyFlag.WorldBounds);
-    if (isFontDirty || isLocalPositionBoundsDirty || isWorldBoundsDirty) {
-      isFontDirty && this._resetCharFont();
-      isLocalPositionBoundsDirty && this._updateLocalData();
-      isWorldBoundsDirty && this._updateBounds(this._bounds);
-      this._setDirtyFlagFalse(DirtyFlag.Font | DirtyFlag.LocalPositionBounds | DirtyFlag.WorldBounds);
-    }
+    this._isContainDirtyFlag(DirtyFlag.StyleFont) && this._resetStyleFont();
+    this._isContainDirtyFlag(DirtyFlag.LocalPositionBounds) && this._updateLocalData();
+    this._isContainDirtyFlag(DirtyFlag.WorldPosition) && this._updatePosition();
+    this._isContainDirtyFlag(DirtyFlag.WorldBounds) && this._updateBounds(this._bounds);
+    this._setDirtyFlagFalse(DirtyFlag.Font | DirtyFlag.WorldBounds);
+
     return this._bounds;
   }
 
@@ -306,18 +302,17 @@ export class TextRenderer extends Renderer implements ICustomClone {
       this._setDirtyFlagFalse(DirtyFlag.MaskInteraction);
     }
 
-    const isFontDirty = this._isContainDirtyFlag(DirtyFlag.Font);
-    if (isFontDirty) {
-      this._resetCharFont();
-      this._setDirtyFlagFalse(DirtyFlag.Font);
+    if (this._isContainDirtyFlag(DirtyFlag.StyleFont)) {
+      this._resetStyleFont();
+      this._setDirtyFlagFalse(DirtyFlag.StyleFont);
     }
 
-    if (this._isContainDirtyFlag(DirtyFlag.LocalPositionBounds) || isFontDirty) {
+    if (this._isContainDirtyFlag(DirtyFlag.LocalPositionBounds)) {
       this._updateLocalData();
       this._setDirtyFlagFalse(DirtyFlag.LocalPositionBounds);
     }
 
-    if (this._isContainDirtyFlag(DirtyFlag.WorldPosition) || isFontDirty) {
+    if (this._isContainDirtyFlag(DirtyFlag.WorldPosition)) {
       this._updatePosition();
       this._setDirtyFlagFalse(DirtyFlag.WorldPosition);
     }
@@ -416,17 +411,17 @@ export class TextRenderer extends Renderer implements ICustomClone {
     }
   }
 
-  private _resetCharFont(): void {
-    const lastCharFont = this._charFont;
-    if (lastCharFont) {
-      lastCharFont._addRefCount(-1);
-      lastCharFont.destroy();
+  private _resetStyleFont(): void {
+    const lastStyleFont = this._styleFont;
+    if (lastStyleFont) {
+      lastStyleFont._addRefCount(-1);
+      lastStyleFont.destroy();
     }
-    this._charFont = Font.createFromOS(
+    this._styleFont = Font.createFromOS(
       this.engine,
       TextUtils.getNativeFontHash(this.font.name, this.fontSize, this.fontStyle)
     );
-    this._charFont._addRefCount(1);
+    this._styleFont._addRefCount(1);
   }
 
   private _updatePosition(): void {
@@ -482,7 +477,7 @@ export class TextRenderer extends Renderer implements ICustomClone {
     max.set(0, 0, 0);
     const { _pixelsPerUnit } = Engine;
     const pixelsPerUnitReciprocal = 1.0 / _pixelsPerUnit;
-    const charFont = this._charFont;
+    const charFont = this._styleFont;
     const rendererWidth = this.width * _pixelsPerUnit;
     const halfRendererWidth = rendererWidth * 0.5;
     const rendererHeight = this.height * _pixelsPerUnit;
@@ -586,9 +581,11 @@ export class TextRenderer extends Renderer implements ICustomClone {
 }
 
 export enum DirtyFlag {
-  Font = 0x1,
+  StyleFont = 0x1,
   LocalPositionBounds = 0x2,
   WorldPosition = 0x4,
   WorldBounds = 0x8,
-  MaskInteraction = 0x10
+  MaskInteraction = 0x10,
+
+  Font = StyleFont | LocalPositionBounds | WorldPosition
 }
