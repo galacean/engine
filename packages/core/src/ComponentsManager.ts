b5fb4665a4386537ae65803aa4b1bdb9383aea8a
singlecoder <singlecoder2014@gmail.com>
2022-09-15 13:30:25 +0800

    fix(renderer): fix remove renderer error when set entity active from true to fasle in onAwake

--- packages/core/src/ComponentsManager.ts
+++ packages/core/src/ComponentsManager.ts
@@ -38,8 +38,12 @@ export class ComponentsManager {
   }
 
   removeRenderer(renderer: Renderer) {
-    const replaced = this._renderers.deleteByIndex(renderer._rendererIndex);
-    replaced && (replaced._rendererIndex = renderer._rendererIndex);
+    const { _rendererIndex } = renderer;
+    if (_rendererIndex === -1) {
+      return;
+    }
+    const replaced = this._renderers.deleteByIndex(_rendererIndex);
+    replaced && (replaced._rendererIndex = _rendererIndex);
     renderer._rendererIndex = -1;
   }
 
