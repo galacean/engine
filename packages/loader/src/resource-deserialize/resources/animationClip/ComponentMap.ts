f1556d76dd1d0c52c9639435d239d085cd87c22a
luzhuang <364439895@qq.com>
2022-09-20 14:40:45 +0800

    feat: animation clip editor

---
+++ packages/loader/src/resource-deserialize/resources/animationClip/ComponentMap.ts
@@ -0,0 +1,31 @@
+import {
+  Animator,
+  Camera,
+  Component,
+  DirectLight,
+  Entity,
+  MeshRenderer,
+  ParticleRenderer,
+  Transform,
+  PointLight,
+  SpotLight,
+  Script,
+  SpriteMask,
+  SpriteRenderer,
+  TextRenderer
+} from "@oasis-engine/core";
+
+export const ComponentMap: Record<string, new (entity: Entity) => Component> = {
+  Transform,
+  Animator,
+  DirectLight,
+  Camera,
+  MeshRenderer,
+  ParticleRenderer,
+  PointLight,
+  SpotLight,
+  Script,
+  SpriteMask,
+  SpriteRenderer,
+  TextRenderer
+};
