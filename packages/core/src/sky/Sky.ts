c7ca75890144e6d97e72592f05a6deecb730aecc
yangfengzzz <yangfengzzz@hotmail.com>
2022-09-20 12:01:18 +0800

    fix: cr opt

--- packages/core/src/sky/Sky.ts
+++ packages/core/src/sky/Sky.ts
@@ -10,7 +10,7 @@ import { ShaderMacroCollection } from "../shader/ShaderMacroCollection";
  * Sky.
  */
 export class Sky {
-  private static _epsilon = 1e-6;
+  private static _epsilon: number = 1e-6;
   private static _viewProjMatrix: Matrix = new Matrix();
   private static _projectionMatrix: Matrix = new Matrix(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, Sky._epsilon - 1, -1, 0, 0, 0, 0);
 
@@ -33,34 +33,41 @@ export class Sky {
       return;
     }
 
-    const { engine, aspectRatio, fieldOfView, viewMatrix } = camera;
+    const { engine, aspectRatio, fieldOfView, viewMatrix, shaderData: cameraShaderData } = camera;
     const { _viewProjMatrix: viewProjMatrix, _projectionMatrix: projectionMatrix } = Sky;
     const rhi = engine._hardwareRenderer;
-    const { shaderData, shader, renderState } = material;
+    const { shaderData: materialShaderData, shader, renderState } = material;
 
     // no-scale view matrix
     viewProjMatrix.copyFrom(viewMatrix);
     const e = viewProjMatrix.elements;
     e[12] = e[13] = e[14] = 0;
 
-    // epsilon-infinity projection matrix
+    // epsilon-infinity projection matrix http://terathon.com/gdc07_lengyel.pdf
     const f = 1.0 / Math.tan(MathUtil.degreeToRadian(fieldOfView) / 2);
     projectionMatrix.elements[0] = f / aspectRatio;
     projectionMatrix.elements[5] = f;
 
     // view-proj matrix
     Matrix.multiply(projectionMatrix, viewProjMatrix, viewProjMatrix);
-    shaderData.setMatrix("u_mvpNoscale", viewProjMatrix);
+    const originViewProjMatrix = cameraShaderData.getMatrix("u_VPMat");
+    cameraShaderData.setMatrix("u_VPMat", viewProjMatrix);
 
     const compileMacros = Shader._compileMacros;
-    ShaderMacroCollection.unionCollection(camera._globalShaderMacro, shaderData._macroCollection, compileMacros);
+    ShaderMacroCollection.unionCollection(
+      camera._globalShaderMacro,
+      materialShaderData._macroCollection,
+      compileMacros
+    );
     const program = shader.passes[0]._getShaderProgram(engine, compileMacros);
     program.bind();
     program.groupingOtherUniformBlock();
-    program.uploadAll(program.materialUniformBlock, shaderData);
+    program.uploadAll(program.cameraUniformBlock, cameraShaderData);
+    program.uploadAll(program.materialUniformBlock, materialShaderData);
     program.uploadUnGroupTextures();
 
     renderState._apply(engine, false);
     rhi.drawPrimitive(mesh, mesh.subMesh, program);
+    cameraShaderData.setMatrix("u_VPMat", originViewProjMatrix);
   }
 }
