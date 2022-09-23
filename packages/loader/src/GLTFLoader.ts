d978caa74b51f5e62bca6779d5deb8d67b148e7e
Hu Song <gz65555@gmail.com>
2022-09-23 17:48:36 +0800

    Add loader params(Texture, Mesh) (#890)
    * feat: Add loader params

--- packages/loader/src/GLTFLoader.ts
+++ packages/loader/src/GLTFLoader.ts
@@ -9,6 +9,7 @@ export class GLTFLoader extends Loader<GLTFResource> {
     return new AssetPromise((resolve, reject) => {
       const resource = new GLTFResource(resourceManager.engine);
       resource.url = url;
+      resource._keepMeshData = item.params?.keepMeshData ?? false;
 
       GLTFParser.instance
         .parse(resource)
@@ -19,4 +20,12 @@ export class GLTFLoader extends Loader<GLTFResource> {
         });
     });
   }
+}
+
+/**
+ * GlTF loader params.
+ */
+export interface GLTFParams {
+  /** Keep raw mesh data for glTF parser, default is false. */
+  keepMeshData: boolean;
 }
