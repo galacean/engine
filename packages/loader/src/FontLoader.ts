5ad216851aab33ad80426215792f6ae05a01ab73
singlecoder <singlecoder2014@gmail.com>
2022-09-26 11:00:03 +0800

    feat(ttf): add font loader

--- packages/loader/src/FontLoader.ts
+++ packages/loader/src/FontLoader.ts
@@ -1,31 +1,23 @@
-import {
-  resourceLoader,
-  Loader,
-  AssetType,
-  Font,
-  LoadItem,
-  ResourceManager,
-  AssetPromise
-} from "@oasis-engine/core";
+import { resourceLoader, Loader, AssetType, Font, LoadItem, ResourceManager, AssetPromise } from "@oasis-engine/core";
 
 @resourceLoader(AssetType.Font, ["font"], false)
 class FontLoader extends Loader<Font> {
   load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<Font> {
     return new AssetPromise((resolve, reject) => {
-      // const { url } = item;
-      // const name = Font.getFontNameFromTTF(url);
-      // if (name) {
-      //   TextUtils.registerTTF(name, url)
-      //     .then(() => {
-      //       const font = new Font(resourceManager.engine, name);
-      //       resolve(font);
-      //     })
-      //     .catch((e) => {
-      //       reject(e);
-      //     });
-      // } else {
-      //   reject(new Error(`the ttf url is illegal ${url}`));
-      // }
+      this.request<any>(item.url, { type: "json" })
+        .then((data) => {
+          const { fontName, fontUrl } = data;
+          Font.create(resourceManager.engine, fontName, fontUrl)
+            .then((ret) => {
+              resolve(ret);
+            })
+            .catch((e) => {
+              reject(e);
+            });
+        })
+        .catch((e) => {
+          reject(e);
+        });
     });
   }
 }
