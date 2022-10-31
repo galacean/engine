1190aede34c43689c55783afe053afcf5a5d29c3
yangfengzzz <yangfengzzz@hotmail.com>
2022-10-31 15:30:25 +0800

    feat: add calculateTangents in model mesh

--- packages/core/src/mesh/PrimitiveMesh.ts
+++ packages/core/src/mesh/PrimitiveMesh.ts
@@ -1,4 +1,4 @@
-import { Vector2, Vector3, Vector4 } from "@oasis-engine/math";
+import { Vector2, Vector3 } from "@oasis-engine/math";
 import { GLCapabilityType } from "../base/Constant";
 import { Engine } from "../Engine";
 import { ModelMesh } from "./ModelMesh";
