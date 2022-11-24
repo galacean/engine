91767e71691d0bb755d273ee782ca930271a7535
ChenMo <gl3336563@163.com>
2022-11-24 20:12:50 +0800

    Add a force fludh bug to avoid iOS bug (#1222)
    * fix:  iOS 15.4.1 bug

--- packages/rhi-webgl/src/WebGLRenderer.ts
+++ packages/rhi-webgl/src/WebGLRenderer.ts
@@ -48,6 +48,11 @@ export enum WebGLMode {
 export interface WebGLRendererOptions extends WebGLContextAttributes {
   /** WebGL mode.*/
   webGLMode?: WebGLMode;
+  /**
+   * @internal
+   * iOS 15 webgl implement has bug, maybe should force call flush command buffer, for exmaple iPhone13(iOS 15.4.1).
+   */
+  _forceFlush?: boolean;
 }
 
 /**
@@ -115,6 +120,7 @@ export class WebGLRenderer implements IHardwareRenderer {
     const option = this._options;
     option.alpha === undefined && (option.alpha = false);
     option.stencil === undefined && (option.stencil = true);
+    option._forceFlush === undefined && (option._forceFlush = false);
     const webCanvas = (this._webCanvas = (canvas as WebCanvas)._webCanvas);
     const webGLMode = option.webGLMode || WebGLMode.Auto;
     let gl: (WebGLRenderingContext & WebGLExtension) | WebGL2RenderingContext;
@@ -158,8 +164,6 @@ export class WebGLRenderer implements IHardwareRenderer {
     if (debugRenderInfo != null) {
       this._renderer = gl.getParameter(debugRenderInfo.UNMASKED_RENDERER_WEBGL);
     }
-
-    this._options = null;
   }
 
   createPlatformPrimitive(primitive: Mesh): IPlatformPrimitive {
@@ -322,6 +326,10 @@ export class WebGLRenderer implements IHardwareRenderer {
       gl.disable(gl.POLYGON_OFFSET_FILL);
     }
     this._enableGlobalDepthBias = enable;
+  }
+
+  flush(): void {
+    this._gl.flush();
   }
 
   destroy() {}
