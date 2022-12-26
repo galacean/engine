2c03e0e3129cb6c05a3562dcf2890b389316738b
GuoLei1990 <gl3336563@163.com>
2022-12-26 16:11:11 +0800

    refactor: opt code

--- packages/loader/src/gltf/parser/MaterialParser.ts
+++ packages/loader/src/gltf/parser/MaterialParser.ts
@@ -141,7 +141,6 @@ export class MaterialParser extends Parser {
       }
 
       if (OASIS_materials_remap) {
-        debugger;
         gltf.extensions = gltf.extensions ?? {};
         gltf.extensions["OASIS_materials_remap"] = gltf.extensions["OASIS_materials_remap"] ?? {};
         gltf.extensions["OASIS_materials_remap"][i] = Parser.createEngineResource(
