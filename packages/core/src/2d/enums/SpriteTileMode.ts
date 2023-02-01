cdfe35d18a48e5fc1c7e487466157c006807f851
cptbtptpbcptdtptp <njktsmshh@163.com>
2023-02-01 15:48:50 +0800

    feat: udpate code

--- packages/core/src/2d/enums/SpriteTileMode.ts
+++ packages/core/src/2d/enums/SpriteTileMode.ts
@@ -3,6 +3,9 @@
  * Only works in `SpriteDrawMode.tiled` mode.
  */
 export enum SpriteTileMode {
+  /** SpriteRenderer will tile continuously. */
   Continuous,
+  /** When the size of the intermediate expansion is greater than or equal to `SpriteRenderer._tileStretchValue`, 
+   * a tile will be added，and the number of tiles can only be an integer. */ 
   Adaptive
 }
