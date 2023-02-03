bc7461767ed25adb143f15e61db707670653d97c
cptbtptpbcptdtptp <njktsmshh@163.com>
2023-02-03 11:36:50 +0800

    feat: opt input

--- packages/core/src/input/enums/InputType.ts
+++ packages/core/src/input/enums/InputType.ts
@@ -2,18 +2,32 @@
  * Several known input types and other types to be extended.
  */
 export enum InputType {
+  /** The pointer input will be triggered when the user touches (such as a mouse, pen or contact point on a touch-enable surface). */
   Pointer = 0,
+  /** The wheel input fires when the user scrolls the mouse wheel or similar hardware device. */
   Wheel = 1,
+  /** The keyboard input fires when the user presses or lifts a keyboard key. */
   Keyboard = 2,
+  /** Input to be expanded. */
   XInput1 = 3,
+  /** Input to be expanded. */
   XInput2 = 4,
+  /** Input to be expanded. */
   XInput3 = 5,
+  /** Input to be expanded. */
   XInput4 = 6,
+  /** Input to be expanded. */
   XInput5 = 7,
+  /** Input to be expanded. */
   XInput6 = 8,
+  /** Input to be expanded. */
   XInput7 = 9,
+  /** Input to be expanded. */
   XInput8 = 10,
+  /** Input to be expanded. */
   XInput9 = 11,
+  /** Input to be expanded. */
   XInput10 = 12,
+  /** The maximum count of inputs at the same time. */
   length = 13
 }
