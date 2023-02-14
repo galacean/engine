3939ba9e9e2f4e08e1167f60baf3dc4bddf8ab30
GuoLei1990 <gl3336563@163.com>
2023-02-14 18:40:21 +0800

    refactor: opt code

--- packages/core/src/asset/GraphicsResource.ts
+++ packages/core/src/asset/GraphicsResource.ts
@@ -1,11 +1,20 @@
 import { Engine } from "../Engine";
+import { RebuildInfo } from "./RebuildInfo";
 import { ReferResource } from "./ReferResource";
 
 export abstract class GraphicsResource extends ReferResource {
+  /** @internal */
+  _rebuildInfo: RebuildInfo;
+
   protected constructor(engine: Engine) {
     super(engine);
     engine.resourceManager._addGraphicResource(this.instanceId, this);
   }
+
+  /**
+   * @internal
+   */
+  _rebuild(): void {}
 
   // destroy(force: boolean = false): boolean {
   //   const success= super.destroy(force);
---
+++ packages/core/src/asset/RebuildInfo.ts
@@ -0,0 +1,6 @@
+/**
+ * @internal
+ */
+export class RebuildInfo {
+  constructor(public url: string, public bufferOffset?: number, public bufferLength?: number) {}
+}
