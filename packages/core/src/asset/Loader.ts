9a6ee18c59fa5df597b55c6230da164361985790
GuoLei1990 <gl3336563@163.com>
2023-02-17 15:54:25 +0800

    docs: add comments for loader

--- packages/core/src/asset/Loader.ts
+++ packages/core/src/asset/Loader.ts
@@ -1,9 +1,9 @@
 import { EngineObject } from "../base";
 import { AssetPromise } from "./AssetPromise";
+import { ContentRestoreInfo } from "./ContentRestoreInfo";
 import { LoadItem } from "./LoadItem";
 import { request, RequestConfig } from "./request";
 import { ResourceManager } from "./ResourceManager";
-import { ContentRestoreInfo } from "./ContentRestoreInfo";
 /**
  * Loader abstract class.
  */
@@ -33,11 +33,22 @@ export abstract class Loader<T> {
   request: <U>(url: string, config: RequestConfig) => AssetPromise<U> = request;
   abstract load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<T> | Record<string, AssetPromise<any>>;
 
+  /**
+   * Add content restore info to the engine object.
+   * @param engineObject - The engine object
+   * @param restoreInfo - The restore info
+   */
   addContentRestoreInfo(engineObject: EngineObject, restoreInfo: ContentRestoreInfo): void {
     restoreInfo._loader = this;
     engineObject.engine.resourceManager._addRestoreContentInfo(engineObject.instanceId, restoreInfo);
   }
 
+  /**
+   * Called when the content of the host object needs to be restored.
+   * @param host - The host object to restore content
+   * @param restoreInfo - The restore info
+   * @returns The promise of the restore content
+   */
   restoreContent(
     host: EngineObject,
     restoreInfo: ContentRestoreInfo
