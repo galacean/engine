db1e17c2ded52e8810e63d2fdffc5274b66efa93
GuoLei1990 <gl3336563@163.com>
2023-02-22 22:14:38 +0800

    refactor: opt code

--- packages/core/src/asset/ContentRestoreInfo.ts
+++ packages/core/src/asset/ContentRestoreInfo.ts
@@ -1,5 +1,8 @@
 import { Loader } from "./Loader";
 
+/**
+ * ContentRestoreInfo is a base class for all content restore info classes.
+ */
 export abstract class ContentRestoreInfo<T> {
   /** @internal */
   _loader: Loader<T>;
