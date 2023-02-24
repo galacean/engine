f5ec167e90032e949739569bceac6b5eddd1f440
GuoLei1990 <gl3336563@163.com>
2023-02-24 19:48:30 +0800

    feat: opt code

--- packages/loader/src/TextureCubeLoader.ts
+++ packages/loader/src/TextureCubeLoader.ts
@@ -1,26 +1,31 @@
 import {
   AssetPromise,
   AssetType,
+  ContentRestorer,
   Loader,
   LoadItem,
+  request,
   resourceLoader,
   ResourceManager,
-  TextureCubeFace,
-  TextureCube
+  TextureCube,
+  TextureCubeFace
 } from "@oasis-engine/core";
+import { RequestConfig } from "@oasis-engine/core/types/asset/request";
 
 @resourceLoader(AssetType.TextureCube, [""])
 class TextureCubeLoader extends Loader<TextureCube> {
+  /**
+   * @override
+   */
   load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<TextureCube> {
     return new AssetPromise((resolve, reject) => {
-      Promise.all(
-        item.urls.map((url) =>
-          this.request<HTMLImageElement>(url, {
-            ...item,
-            type: "image"
-          })
-        )
-      )
+      const urls = item.urls;
+      const requestConfig = <RequestConfig>{
+        ...item,
+        type: "image"
+      };
+
+      Promise.all(urls.map((url) => this.request<HTMLImageElement>(url, requestConfig)))
         .then((images) => {
           const { width, height } = images[0];
 
@@ -29,17 +34,41 @@ class TextureCubeLoader extends Loader<TextureCube> {
             return;
           }
 
-          const tex = new TextureCube(resourceManager.engine, width);
-
-          /** @ts-ignore */
-          if (!tex._platformTexture) return;
+          const texture = new TextureCube(resourceManager.engine, width);
 
           for (let faceIndex = 0; faceIndex < 6; faceIndex++) {
-            tex.setImageSource(TextureCubeFace.PositiveX + faceIndex, images[faceIndex], 0);
+            texture.setImageSource(TextureCubeFace.PositiveX + faceIndex, images[faceIndex], 0);
           }
 
-          tex.generateMipmaps();
-          resolve(tex);
+          texture.generateMipmaps();
+          resourceManager.addContentRestorer(new TextureCubeContentRestorer(texture, urls, requestConfig));
+          resolve(texture);
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  }
+}
+
+class TextureCubeContentRestorer extends ContentRestorer<TextureCube> {
+  constructor(resource: TextureCube, public urls: string[], public requestConfig: RequestConfig) {
+    super(resource);
+  }
+
+  /**
+   * @override
+   */
+  restoreContent(): AssetPromise<TextureCube> {
+    return new AssetPromise((resolve, reject) => {
+      Promise.all(this.urls.map((url) => request<HTMLImageElement>(url, this.requestConfig)))
+        .then((images) => {
+          const resource = this.resource;
+          for (let faceIndex = 0; faceIndex < 6; faceIndex++) {
+            resource.setImageSource(TextureCubeFace.PositiveX + faceIndex, images[faceIndex], 0);
+          }
+          resource.generateMipmaps();
+          resolve(resource);
         })
         .catch((e) => {
           reject(e);
