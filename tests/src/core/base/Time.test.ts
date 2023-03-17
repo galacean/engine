2d9376d9040c9a44fff6d70ae2c4d16b0de4cec4
GuoLei1990 <gl3336563@163.com>
2023-03-17 16:29:41 +0800

    test: add Time basic unit test

---
+++ tests/src/core/base/Time.test.ts
@@ -0,0 +1,25 @@
+import { WebGLEngine } from "@oasis-engine/rhi-webgl";
+import { expect } from "chai";
+
+describe("Time", () => {
+  it("Time basic", () => {
+    const engine = new WebGLEngine(document.createElement("canvas"));
+
+    expect(engine.time.frameCount).to.be.equal(0);
+    expect(engine.time.elapsedTime).to.be.equal(0);
+    expect(engine.time.deltaTime).to.be.equal(0);
+    expect(engine.time.unscaledElapsedTime).to.be.equal(0);
+    expect(engine.time.unscaledDeltaTime).to.be.equal(0);
+
+    expect(engine.time.timeScale).to.be.equal(1);
+    engine.time.timeScale = 2;
+    expect(engine.time.timeScale).to.be.equal(2);
+
+    expect(engine.time.maximumDeltaTime).to.be.equal(0.333333);
+    engine.time.maximumDeltaTime = 0.5;
+    expect(engine.time.maximumDeltaTime).to.be.equal(0.5);
+
+    engine.update();
+    expect(engine.time.frameCount).to.be.equal(1);
+  });
+});
