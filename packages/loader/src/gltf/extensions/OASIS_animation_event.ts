be4daeca4179edc185171aadb9210ecf3773f52e
luzhuang <364439895@qq.com>
2023-03-22 17:58:46 +0800

    feat: opt code

---
+++ packages/loader/src/gltf/extensions/OASIS_animation_event.ts
@@ -0,0 +1,28 @@
+import { AnimationClip, AnimationEvent } from "@oasis-engine/core";
+import { GLTFResource } from "../GLTFResource";
+import { GLTFExtensionOwnerSchema } from "../GLTFSchema";
+import { registerGLTFExtension } from "../parser/GLTFParser";
+import { GLTFParserContext } from "../parser/GLTFParserContext";
+import { GLTFExtensionMode, GLTFExtensionParser } from "./GLTFExtensionParser";
+import { IOasisAnimation } from "./GLTFExtensionSchema";
+
+@registerGLTFExtension("OASIS_animation_event", GLTFExtensionMode.AdditiveParse)
+class OASIS_animation_event extends GLTFExtensionParser {
+  /**
+   * @override
+   */
+  additiveParse(context: GLTFParserContext, animationClip: AnimationClip, schema: IOasisAnimation): void {
+    const { engine } = context.glTFResource;
+    const { events } = schema;
+    events.map((eventData) => {
+      return new Promise<void>((resolve) => {
+        const event = new AnimationEvent();
+        event.functionName = eventData.functionName;
+        event.time = eventData.time;
+        event.parameter = eventData.parameter;
+        animationClip.addEvent(event);
+        resolve();
+      });
+    });
+  }
+}
