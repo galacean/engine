03dbaeb5f661026aab2c06365c54943fe0290e4c
xiaofeng.mxf <xiaofeng.mxf@taobao.com>
2023-04-17 10:31:14 +0800

    feat: add test for requester

---
+++ tests/src/loader/customRequester.test.ts
@@ -0,0 +1,36 @@
+import { AssetType, AssetPromise } from "@galacean/engine-core";
+import { WebGLEngine } from "@galacean/engine-rhi-webgl";
+import { expect } from "chai";
+
+let engine: WebGLEngine;
+before(async () => {
+  const canvasDOM = document.createElement("canvas");
+  canvasDOM.width = 1024;
+  canvasDOM.height = 1024;
+  engine = await WebGLEngine.create({ canvas: canvasDOM });
+});
+
+describe("custom requester", function () {
+  it("invoke", async () => {
+    const mockReturn = { result: "123" };
+    const ret: any = await engine.resourceManager.load({
+      type: AssetType.GLTF,
+      url: "/not/relevant",
+      params: {
+        requester: (url) => {
+          return new AssetPromise((resolve, reject, setProgress) => {
+            setTimeout(() => {
+              resolve(mockReturn);
+            }, 1000);
+
+            setTimeout(() => {
+              setProgress(0.5);
+            }, 500);
+          });
+        }
+      }
+    });
+
+    expect(ret).to.equal(mockReturn);
+  });
+});
