01af4f5fbdfc4aa22777b2bc0726fa5be38027e2
zhuxudong <callzhuxudong@163.com>
2023-05-18 18:24:00 +0800

    fix: cherry pick error

--- packages/loader/src/gltf/extensions/KHR_materials_ior.ts
+++ packages/loader/src/gltf/extensions/KHR_materials_ior.ts
@@ -1,12 +1,15 @@
 import { PBRMaterial } from "@galacean/engine-core";
-import { IKHRMaterialsIor } from "./GLTFExtensionSchema";
 import { GLTFParserContext } from "../parser";
 import { registerGLTFExtension } from "../parser/GLTFParser";
 import { GLTFExtensionMode, GLTFExtensionParser } from "./GLTFExtensionParser";
+import { IKHRMaterialsIor } from "./GLTFExtensionSchema";
 
 @registerGLTFExtension("KHR_materials_ior", GLTFExtensionMode.AdditiveParse)
 class KHR_materials_ior extends GLTFExtensionParser {
-  parseEngineResource(schema: IKHRMaterialsIor, material: PBRMaterial, context: GLTFParserContext): void {
+  /**
+   * @override
+   */
+  additiveParse(context: GLTFParserContext, material: PBRMaterial, schema: IKHRMaterialsIor): void {
     const { ior = 1.5 } = schema;
 
     material.ior = ior;
