5d0dd0d4f90afe0585f4ac5bae1d40273af67ebd
lixinhang <lixinhang@icloud.com>
2023-05-26 12:24:47 +0800

    Revert "extend the test execution timeout of the test framework"
    This reverts commit 30d26fe28f3b5293d57fec188ca90395f968011f.

--- tests/src/core/Animator.test.ts
+++ tests/src/core/Animator.test.ts
@@ -27,11 +27,10 @@ describe("Animator test", function () {
   });
 
   after(function(){
-    animator.destroy();
     engine.destroy();
   });
 
-  it("animator constructor", () => {
+  it("constructor", () => {
     // Test default values
     expect(animator).not.to.be.undefined;
     expect(animator.cullingMode).to.eq(0);
@@ -87,7 +86,7 @@ describe("Animator test", function () {
     expect(animatorState.name).to.eq('Walk');
   });
 
-  it("animator enabled", () => {
+  it("animation enabled", () => {
     // Test animator play.
     animator.play('Survey');
 
@@ -112,7 +111,7 @@ describe("Animator test", function () {
     expect(animatorState.name).to.eq(expectedStateName);
   });
 
-  it("animator getCurrentAnimatorState", () => {
+  it("animation getCurrentAnimatorState", () => {
     //get random animation element from gltf resource
     const min = 0;
     const max = resource.animations.length - 1;
@@ -126,7 +125,7 @@ describe("Animator test", function () {
     expect(currentAnimatorState.name).to.eq(expectedStateName);
   });
 
-  it("animator cross fade", () => {
+  it("animation cross fade", () => {
     //@ts-ignore
     animator._reset();
     animator.play('Walk');
@@ -141,10 +140,10 @@ describe("Animator test", function () {
     expect(layerState).to.eq(2);
   });
 
-  it("animator fix cross fade", () => {
+  it("animation fix cross fade", () => {
     //@ts-ignore
     animator._reset();
-    animator.play('Walk');
+    animator.play('');
     animator.update(1);
     animator.crossFade('Survey', 5);
     animator.crossFade('Run', 0.5);
