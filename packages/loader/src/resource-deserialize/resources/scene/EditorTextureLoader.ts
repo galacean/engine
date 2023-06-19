4ecb0c501a7c7e629e576758f1f0e01a5dbca739
zhuxudong <callzhuxudong@163.com>
2023-06-19 20:34:03 +0800

    fix: throw error in AssetPromise

--- packages/loader/src/resource-deserialize/resources/scene/EditorTextureLoader.ts
+++ packages/loader/src/resource-deserialize/resources/scene/EditorTextureLoader.ts
@@ -4,12 +4,12 @@ import { decode } from "../..";
 @resourceLoader("EditorTexture2D", ["prefab"], true)
 export class EditorTextureLoader extends Loader<Texture2D> {
   load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<Texture2D> {
-    return new AssetPromise((resolve) => {
+    return new AssetPromise((resolve, reject) => {
       this.request<ArrayBuffer>(item.url, { type: "arraybuffer" }).then((data) => {
         decode<Texture2D>(data, resourceManager.engine).then((texture) => {
           resolve(texture);
         });
-      });
+      }).catch(reject)
     });
   }
 }
