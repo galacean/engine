2adf21acb779c2aed9f02f77e3437c3955af77c1
GuoLei1990 <gl3336563@163.com>
2023-07-16 20:02:45 +0800

    types: fix lint

--- packages/shader-lab/src/ShaderLab.ts
+++ packages/shader-lab/src/ShaderLab.ts
@@ -2,7 +2,6 @@ import { AstNodeUtils } from "./AstNodeUtils";
 import { IShaderInfo, IShaderLab } from "./interface";
 
 export class ShaderLab implements IShaderLab {
-  
   parseShader(shaderSource: string): IShaderInfo {
     return AstNodeUtils.parseShader(shaderSource);
   }
