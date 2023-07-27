e6c4fd78c68dd7bb3f3249663bed490154f37105
GuoLei1990 <gl3336563@163.com>
2023-07-27 11:27:07 +0800

    refactor: opt code

--- packages/core/src/enums/DepthTextureMode.ts
+++ packages/core/src/enums/DepthTextureMode.ts
@@ -5,7 +5,7 @@ export enum DepthTextureMode {
   /* No depth texture. */
   None,
   /* Generate depth texture by pre-pass rendering. */
-  PrePass,
-  /* Generate depth texture by copy depth buffer after opaque pass. */
-  CopyAfterOpaque
+  PrePass
+  // /* Generate depth texture by copy depth buffer after opaque pass. */
+  // CopyAfterOpaque
 }
