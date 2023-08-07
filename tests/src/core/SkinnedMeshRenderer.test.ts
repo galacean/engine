0a7dca7f56199f2d095ccd8889a9bf12b25153a0
jilin <jtabibito@qq.com>
2023-08-07 17:25:47 +0800

    test(SkinnedMeshRenderer): update unit test case of

--- tests/src/core/SkinnedMeshRenderer.test.ts
+++ tests/src/core/SkinnedMeshRenderer.test.ts
@@ -1,13 +1,100 @@
-import { BlendShape, ModelMesh, SkinnedMeshRenderer } from "@galacean/engine-core";
-import { Vector3 } from "@galacean/engine-math";
+import { BlendShape, ModelMesh, SkinnedMeshRenderer, Entity, Skin } from "@galacean/engine-core";
+import { BoundingBox, Vector3, Matrix } from "@galacean/engine-math";
 import { WebGLEngine } from "@galacean/engine-rhi-webgl";
 import { expect } from "chai";
 
 describe("SkinnedMeshRenderer", async () => {
-  const engine = await WebGLEngine.create({ canvas: document.createElement("canvas") });
-  const scene = engine.sceneManager.activeScene;
-  const rootEntity = scene.createRootEntity();
-  engine.run();
+  let engine: WebGLEngine;
+  let rootEntity: Entity;
+
+  before(async () => {
+    engine = await WebGLEngine.create({ canvas: document.createElement("canvas") });
+    const scene = engine.sceneManager.activeScene;
+    rootEntity = scene.createRootEntity();
+
+    engine.run();
+  });
+
+  it("blend shape weight", () => {
+    const modelMesh = new ModelMesh(engine);
+    const blendShape = new BlendShape("BlendShape");
+    modelMesh.addBlendShape(blendShape);
+    modelMesh.addBlendShape(blendShape);
+    modelMesh.addBlendShape(blendShape);
+    modelMesh.addBlendShape(blendShape);
+
+    const entity = rootEntity.createChild("WeigthEntity");
+    const skinnedMR = entity.addComponent(SkinnedMeshRenderer);
+    skinnedMR.mesh = modelMesh;
+
+    // Test that default weight is 0.
+    expect(skinnedMR.blendShapeWeights.length).to.equal(4);
+    expect(skinnedMR.blendShapeWeights).to.deep.equal(new Float32Array([0, 0, 0, 0]));
+
+    skinnedMR.blendShapeWeights = new Float32Array([2, 2, 1, 1]);
+    expect(skinnedMR.blendShapeWeights.length).to.equal(4);
+    expect(skinnedMR.blendShapeWeights).to.deep.equal(new Float32Array([2, 2, 1, 1]));
+
+    const blendShape2 = new BlendShape("BlendShape2");
+    blendShape2.addFrame(0.9, [new Vector3(1, 0, 1), new Vector3(2, 1, 2), new Vector3(3, -1, 3)]);
+    modelMesh.addBlendShape(blendShape2);
+    expect(skinnedMR.blendShapeWeights.length).to.equal(5);
+    expect(skinnedMR.blendShapeWeights).to.deep.equal(new Float32Array([2, 2, 1, 1, 0]));
+
+    modelMesh.clearBlendShapes();
+    modelMesh.addBlendShape(blendShape);
+    expect(skinnedMR.blendShapeWeights.length).to.equal(1);
+    expect(skinnedMR.blendShapeWeights).to.deep.equal(new Float32Array([2]));
+
+    // Test that the weight is set correctly.
+    skinnedMR.blendShapeWeights = new Float32Array([0.4, 1, 0.5, 0.7, 0.8, 0.2]);
+    expect(skinnedMR.blendShapeWeights.length).to.equal(1);
+    expect(skinnedMR.blendShapeWeights).to.deep.equal(new Float32Array([0.4]));
+  });
+
+  it("localBounds", () => {
+    // Test that the localBounds is set correctly.
+    const entity = rootEntity.createChild("LocalBoundsEntity");
+    const skinnedMR = entity.addComponent(SkinnedMeshRenderer);
+
+    skinnedMR.localBounds = new BoundingBox(new Vector3(-1, -1, -1), new Vector3(1, 1, 1));
+    expect(skinnedMR.localBounds.min).to.deep.include({ x: -1, y: -1, z: -1 });
+    expect(skinnedMR.localBounds.max).to.deep.include({ x: 1, y: 1, z: 1 });
+  });
+
+  it("skin and rootBone", () => {
+    const skin = new Skin("TestSkin");
+    skin.skeleton = "RootBone";
+    skin.joints = ["Joint0", "Joint1", "Joint2"];
+    skin.inverseBindMatrices = [
+      new Matrix(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0).invert(),
+      new Matrix(1, 0, 0, 0.6, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0).invert(),
+      new Matrix(1, 0, 0, -1, 0, 0, 0, -1, 0, 0, -2, -1).invert()
+    ];
+
+    const modelMesh = new ModelMesh(engine);
+    const entity = rootEntity.createChild("SkinEntity");
+    const rootBone = entity.createChild("RootBone");
+    rootBone.createChild("Joint0");
+    rootBone.createChild("Joint1");
+    rootBone.createChild("Joint3");
+    rootBone.createChild("Joint4");
+    const skinnedMR = entity.addComponent(SkinnedMeshRenderer);
+    skinnedMR.mesh = modelMesh;
+
+    skinnedMR.skin = skin;
+    engine.update();
+
+    // Test that the skin is set correctly.
+    expect(skinnedMR.skin).to.equal(skin);
+
+    const rootBone0 = entity.createChild("RootBone0");
+    skinnedMR.rootBone = rootBone0;
+    engine.update();
+
+    // Test that the rootBone is set correctly.
+    expect(skinnedMR.rootBone).to.equal(rootBone0);
+  });
 
   it("clone", () => {
     // @ts-ignore
