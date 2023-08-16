3a814c1f1641236072f6f6616033b55bd4c0046e
时霖 <linji.jl@digital-engine.com>
2023-08-16 09:29:25 +0800

    update unit test case of

--- tests/src/core/SkinnedMeshRenderer.test.ts
+++ tests/src/core/SkinnedMeshRenderer.test.ts
@@ -28,28 +28,28 @@ describe("SkinnedMeshRenderer", async () => {
     skinnedMR.mesh = modelMesh;
 
     // Test that default weight is 0.
-    expect(skinnedMR.blendShapeWeights.length).to.equal(4);
-    expect(skinnedMR.blendShapeWeights).to.deep.equal(new Float32Array([0, 0, 0, 0]));
+    expect(skinnedMR.blendShapeWeights.length).to.be.equal(4);
+    expect(skinnedMR.blendShapeWeights).to.be.deep.equal(new Float32Array([0, 0, 0, 0]));
 
     skinnedMR.blendShapeWeights = new Float32Array([2, 2, 1, 1]);
-    expect(skinnedMR.blendShapeWeights.length).to.equal(4);
-    expect(skinnedMR.blendShapeWeights).to.deep.equal(new Float32Array([2, 2, 1, 1]));
+    expect(skinnedMR.blendShapeWeights.length).to.be.equal(4);
+    expect(skinnedMR.blendShapeWeights).to.be.deep.equal(new Float32Array([2, 2, 1, 1]));
 
     const blendShape2 = new BlendShape("BlendShape2");
     blendShape2.addFrame(0.9, [new Vector3(1, 0, 1), new Vector3(2, 1, 2), new Vector3(3, -1, 3)]);
     modelMesh.addBlendShape(blendShape2);
-    expect(skinnedMR.blendShapeWeights.length).to.equal(5);
-    expect(skinnedMR.blendShapeWeights).to.deep.equal(new Float32Array([2, 2, 1, 1, 0]));
+    expect(skinnedMR.blendShapeWeights.length).to.be.equal(5);
+    expect(skinnedMR.blendShapeWeights).to.be.deep.equal(new Float32Array([2, 2, 1, 1, 0]));
 
     modelMesh.clearBlendShapes();
     modelMesh.addBlendShape(blendShape);
-    expect(skinnedMR.blendShapeWeights.length).to.equal(1);
-    expect(skinnedMR.blendShapeWeights).to.deep.equal(new Float32Array([2]));
+    expect(skinnedMR.blendShapeWeights.length).to.be.equal(1);
+    expect(skinnedMR.blendShapeWeights).to.be.deep.equal(new Float32Array([2]));
 
     // Test that the weight is set correctly.
     skinnedMR.blendShapeWeights = new Float32Array([0.4, 1, 0.5, 0.7, 0.8, 0.2]);
-    expect(skinnedMR.blendShapeWeights.length).to.equal(1);
-    expect(skinnedMR.blendShapeWeights).to.deep.equal(new Float32Array([0.4]));
+    expect(skinnedMR.blendShapeWeights.length).to.be.equal(1);
+    expect(skinnedMR.blendShapeWeights).to.be.deep.equal(new Float32Array([0.4]));
   });
 
   it("localBounds", () => {
@@ -58,8 +58,8 @@ describe("SkinnedMeshRenderer", async () => {
     const skinnedMR = entity.addComponent(SkinnedMeshRenderer);
 
     skinnedMR.localBounds = new BoundingBox(new Vector3(-1, -1, -1), new Vector3(1, 1, 1));
-    expect(skinnedMR.localBounds.min).to.deep.include({ x: -1, y: -1, z: -1 });
-    expect(skinnedMR.localBounds.max).to.deep.include({ x: 1, y: 1, z: 1 });
+    expect(skinnedMR.localBounds.min).to.be.deep.include({ x: -1, y: -1, z: -1 });
+    expect(skinnedMR.localBounds.max).to.be.deep.include({ x: 1, y: 1, z: 1 });
   });
 
   it("skin and rootBone", () => {
@@ -86,14 +86,14 @@ describe("SkinnedMeshRenderer", async () => {
     engine.update();
 
     // Test that the skin is set correctly.
-    expect(skinnedMR.skin).to.equal(skin);
+    expect(skinnedMR.skin).to.be.equal(skin);
 
     const rootBone0 = entity.createChild("RootBone0");
     skinnedMR.rootBone = rootBone0;
     engine.update();
 
     // Test that the rootBone is set correctly.
-    expect(skinnedMR.rootBone).to.equal(rootBone0);
+    expect(skinnedMR.rootBone).to.be.equal(rootBone0);
   });
 
   it("clone", () => {
@@ -115,7 +115,7 @@ describe("SkinnedMeshRenderer", async () => {
     const cloneEntity = entity.clone();
     skinnedMeshRenderer.blendShapeWeights[0] = 0.6;
 
-    expect(cloneEntity.getComponent(SkinnedMeshRenderer).blendShapeWeights).to.not.deep.equal(
+    expect(cloneEntity.getComponent(SkinnedMeshRenderer).blendShapeWeights).to.be.not.deep.equal(
       skinnedMeshRenderer.blendShapeWeights
     );
   });
