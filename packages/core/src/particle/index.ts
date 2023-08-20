ee7ce252bab8372e880d3b27ae9b720ec2f70a0a
GuoLei1990 <gl3336563@163.com>
2023-08-20 15:35:55 +0800

    refactor: opt code

--- packages/core/src/particle/index.ts
+++ packages/core/src/particle/index.ts
@@ -4,6 +4,8 @@ export { ParticleCurveMode } from "./enums/ParticleCurveMode";
 export { ParticleGradientMode } from "./enums/ParticleGradientMode";
 export { ParticleScaleMode } from "./enums/ParticleScaleMode";
 export { ParticleSimulationSpace } from "./enums/ParticleSimulationSpace";
+export { Burst } from "./modules/Burst";
+export { ParticleCompositeCurve } from "./modules/ParticleCompositeCurve";
 export { Key, ParticleCurve } from "./modules/ParticleCurve";
 export * from "./modules/shape/index";
 
