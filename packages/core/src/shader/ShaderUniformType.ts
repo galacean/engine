767caf8b22951ef68b98045437c5edbb63626033
大聪明 <601670314@qq.com>
2023-08-23 20:28:53 -0700

    Create ShaderUniformType.ts

---
+++ packages/core/src/shader/ShaderUniformType.ts
@@ -0,0 +1,13 @@
+export enum ShaderUniformType {
+  Float = "Float",
+  Texture = "Texture",
+  Int = "Int",
+  FloatArray = "FloatArray",
+  IntArray = "IntArray",
+  TextureArray = "TextureArray",
+  Vector2 = "Vector2",
+  Vector3 = "Vector3",
+  Vector4 = "Vector4",
+  Color = "Color",
+  Matrix = "Matrix",
+}
