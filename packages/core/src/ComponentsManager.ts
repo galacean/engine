3deaf476d2faf62bf76da1f4504f0ef2508b7816
GuoLei1990 <gl3336563@163.com>
2023-09-01 17:39:55 +0800

    fix: opt code

--- packages/core/src/ComponentsManager.ts
+++ packages/core/src/ComponentsManager.ts
@@ -167,9 +167,7 @@ export class ComponentsManager {
     const elements = this._onUpdateAnimations._elements;
     for (let i = this._onUpdateAnimations.length - 1; i >= 0; --i) {
       const animator = elements[i];
-      animator._playFrameCount === animator.engine.time.frameCount
-        ? animator.update(0)
-        : animator.update(deltaTime);
+      animator.update(animator._playFrameCount === animator.engine.time.frameCount ? 0 : deltaTime);
     }
   }
 
