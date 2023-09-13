9b48edc4915c1557cbf80e59904d05aa46a3939c
GuoLei1990 <gl3336563@163.com>
2023-09-13 20:27:10 +0800

    test: fix unit test timeout

--- tests/src/core/physics/Collider.test.ts
+++ tests/src/core/physics/Collider.test.ts
@@ -39,6 +39,7 @@ class MoveScript extends Script {
 }
 
 describe("physics collider test", function () {
+  this.timeout(5000);
   let engine: WebGLEngine;
   let rootEntity: Entity;
   let boxEntity: Entity;
