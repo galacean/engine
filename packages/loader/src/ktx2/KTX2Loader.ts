fd4ec5992bf40da87aa70dda43f6bca4b575a7e7
ChenMo <gl3336563@163.com>
2023-10-17 18:02:13 +0800

    Update KTX2Loader.ts

--- packages/loader/src/ktx2/KTX2Loader.ts
+++ packages/loader/src/ktx2/KTX2Loader.ts
@@ -223,7 +223,7 @@ export class KTX2Loader extends Loader<Texture2D | TextureCube> {
  * KTX2 loader params interface.
  */
 export interface KTX2Params {
-  /** Priority transcoding format queue which is preferred options, default is BC7/ASTC/BC3_BC1/ETC/PVRTC/RGBA8. */
+  /** Priority transcoding format queue which is preferred options, default is BC7/ASTC/BC3_BC1/ETC/PVRTC/R8G8B8A8. */
   priorityFormats: KTX2TargetFormat[];
 }
 
@@ -233,7 +233,7 @@ declare module "@galacean/engine-core" {
     ktx2Loader?: {
       /** Worker count for transcoder, default is 4. */
       workerCount?: number;
-      /** Global transcoding format queue which will be used if not specified in params/KV data, default is BC7/ASTC/BC3_BC1/ETC/PVRTC/RGBA8. */
+      /** Global transcoding format queue which will be used if not specified in params/KV data, default is BC7/ASTC/BC3_BC1/ETC/PVRTC/R8G8B8A8. */
       priorityFormats?: KTX2TargetFormat[];
     };
   }
