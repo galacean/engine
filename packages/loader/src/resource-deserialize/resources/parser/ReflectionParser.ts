1cd2a5639372d6e2ab90169ce8794966cffd42b0
Hu Song <gz65555@gmail.com>
2023-11-06 17:32:38 +0800

    Add recursive class and methods (#1865)
    * feat: add recursive class and methods

--- packages/loader/src/resource-deserialize/resources/parser/ReflectionParser.ts
+++ packages/loader/src/resource-deserialize/resources/parser/ReflectionParser.ts
@@ -25,8 +25,9 @@ export class ReflectionParser {
   parseClassObject(item: IClassObject) {
     const Class = Loader.getClass(item.class);
     const params = item.constructParams ?? [];
-    const instance = new Class(...params);
-    return this.parsePropsAndMethods(instance, item);
+    return Promise.all(params.map((param) => this.parseBasicType(param)))
+      .then((resultParams) => new Class(...resultParams))
+      .then((instance) => this.parsePropsAndMethods(instance, item));
   }
 
   parsePropsAndMethods(instance: any, item: Omit<IClassObject, "class">) {
@@ -35,9 +36,7 @@ export class ReflectionParser {
       for (let methodName in item.methods) {
         const methodParams = item.methods[methodName];
         for (let i = 0, count = methodParams.length; i < count; i++) {
-          const params = methodParams[i];
-          const promise = this.parseMethod(instance, methodName, params);
-          promises.push(promise);
+          promises.push(this.parseMethod(instance, methodName, methodParams[i]));
         }
       }
     }
@@ -82,7 +81,19 @@ export class ReflectionParser {
       } else if (originValue) {
         const promises: Promise<any>[] = [];
         for (let key in value as any) {
-          promises.push(this.parseBasicType(value[key], originValue[key]).then((v) => (originValue[key] = v)));
+          if (key === "methods") {
+            const methods: any = value[key];
+            for (let methodName in methods) {
+              const methodParams = methods[methodName];
+              for (let i = 0, count = methodParams.length; i < count; i++) {
+                const params = methodParams[i];
+                const promise = this.parseMethod(originValue, methodName, params);
+                promises.push(promise);
+              }
+            }
+          } else {
+            promises.push(this.parseBasicType(value[key], originValue[key]).then((v) => (originValue[key] = v)));
+          }
         }
         return Promise.all(promises).then(() => originValue);
       }
