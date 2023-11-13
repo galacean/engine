634236f90337ca8e0a1643c933e7cb2caf54506d
Sway007 <yansw007@gmail.com>
2023-11-13 14:19:40 +0800

    fix(shader-lab): glsl type pattern

--- packages/shader-lab/src/parser/tokens/GLSLTypes.ts
+++ packages/shader-lab/src/parser/tokens/GLSLTypes.ts
@@ -1,26 +1,26 @@
 import { createToken } from "chevrotain";
 
-export const glsl_mat2 = createToken({ name: "glsl_mat2", pattern: /mat2/ });
-export const glsl_mat3 = createToken({ name: "glsl_mat3", pattern: /mat3/ });
-export const glsl_mat4 = createToken({ name: "glsl_mat4", pattern: /mat4/ });
+export const glsl_mat2 = createToken({ name: "glsl_mat2", pattern: /mat2\s/ });
+export const glsl_mat3 = createToken({ name: "glsl_mat3", pattern: /mat3\s/ });
+export const glsl_mat4 = createToken({ name: "glsl_mat4", pattern: /mat4\s/ });
 
-export const glsl_vec2 = createToken({ name: "glsl_vec2", pattern: /vec2/ });
-export const glsl_vec3 = createToken({ name: "glsl_vec3", pattern: /vec3/ });
-export const glsl_vec4 = createToken({ name: "glsl_vec4", pattern: /vec4/ });
+export const glsl_vec2 = createToken({ name: "glsl_vec2", pattern: /vec2\s/ });
+export const glsl_vec3 = createToken({ name: "glsl_vec3", pattern: /vec3\s/ });
+export const glsl_vec4 = createToken({ name: "glsl_vec4", pattern: /vec4\s/ });
 
-export const glsl_ivec2 = createToken({ name: "glsl_ivec2", pattern: /ivec2/ });
-export const glsl_ivec3 = createToken({ name: "glsl_ivec3", pattern: /ivec3/ });
-export const glsl_ivec4 = createToken({ name: "glsl_ivec4", pattern: /ivec4/ });
+export const glsl_ivec2 = createToken({ name: "glsl_ivec2", pattern: /ivec2\s/ });
+export const glsl_ivec3 = createToken({ name: "glsl_ivec3", pattern: /ivec3\s/ });
+export const glsl_ivec4 = createToken({ name: "glsl_ivec4", pattern: /ivec4\s/ });
 
-export const glsl_float = createToken({ name: "glsl_float", pattern: /float/ });
-export const glsl_int = createToken({ name: "glsl_int", pattern: /int/ });
+export const glsl_float = createToken({ name: "glsl_float", pattern: /float\s/ });
+export const glsl_int = createToken({ name: "glsl_int", pattern: /int\s/ });
 
 export const glsl_sampler2D = createToken({
   name: "glsl_sampler2D",
-  pattern: /sampler2D/
+  pattern: /sampler2D\s/
 });
 
-export const glsl_sampler2DArray = createToken({ name: "sampler2DArray", pattern: /sampler2DArray/ });
+export const glsl_sampler2DArray = createToken({ name: "sampler2DArray", pattern: /sampler2DArray / });
 
 export const tokenList = [
   glsl_ivec2,
