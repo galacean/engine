c90101eb19b7d8d846bdf124089b00455cab329c
GuoLei1990 <gl3336563@163.com>
2023-11-14 17:14:50 +0800

    refactor: opt code

--- packages/core/src/animation/Animator.ts
+++ packages/core/src/animation/Animator.ts
@@ -298,18 +298,17 @@ export class Animator extends Component {
       const curve = curves[i];
       const targetEntity = curve.relativePath === "" ? entity : entity.findByPath(curve.relativePath);
       if (targetEntity) {
+        const component = entity.getComponent(curve.type);
+        if (!component) {
+          continue;
+        }
+
         const { property } = curve;
         const { instanceId } = targetEntity;
 
         // Get owner
         const propertyOwners = (curveOwnerPool[instanceId] ||= Object.create(null));
-        const owner = (propertyOwners[property] ||= curve._createCurveOwner(targetEntity));
-
-        // Entity don't have target component
-        if (!owner) {
-          delete propertyOwners[property];
-          continue;
-        }
+        const owner = (propertyOwners[property] ||= curve._createCurveOwner(targetEntity, component));
 
         // Get layer owner
         const layerPropertyOwners = (layerCurveOwnerPool[instanceId] ||= Object.create(null));
