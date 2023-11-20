cb3e0a559c664466b16ff9636072f927a8b80f1c
cptbtptpbcptdtptp <njktsmshh@163.com>
2023-11-20 11:12:46 +0800

    fix: lint error

--- packages/core/src/xr/input/XRTargetRayMode.ts
+++ packages/core/src/xr/input/XRTargetRayMode.ts
@@ -3,10 +3,10 @@
  *  the input source should be generated and how it should be presented to the user.
  */
 export enum XRTargetRayMode {
-    /** Eye */
-    Gaze,
-    /** Controller */
-    TrackedPointer,
-    /** Game pad */
-    Screen
+  /** Eye */
+  Gaze,
+  /** Controller */
+  TrackedPointer,
+  /** Game pad */
+  Screen
 }
