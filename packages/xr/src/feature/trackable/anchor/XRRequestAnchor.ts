030f26de47b5ad5472e04321a81643020ec7ac1e
cptbtptpbcptdtptp <njktsmshh@163.com>
2023-12-11 18:33:48 +0800

    feat: update code

--- packages/xr/src/feature/trackable/anchor/XRRequestAnchor.ts
+++ packages/xr/src/feature/trackable/anchor/XRRequestAnchor.ts
@@ -1,11 +1,12 @@
 import { Quaternion, Vector3 } from "@galacean/engine";
+import { IXRRequestAnchor } from "@galacean/engine-design";
 import { XRRequestTracking } from "../XRRequestTracking";
 import { XRAnchor } from "./XRAnchor";
 
 /**
  * The request anchor in XR space.
  */
-export class XRRequestAnchor extends XRRequestTracking<XRAnchor> {
+export class XRRequestAnchor extends XRRequestTracking<XRAnchor> implements IXRRequestAnchor {
   /**
    * @param position - Requests the position of the anchor to be added
    * @param rotation - Requests the rotation of the anchor to be added
--- packages/xr/src/feature/trackable/image/XRRequestImage.ts
+++ packages/xr/src/feature/trackable/image/XRRequestImage.ts
@@ -1,3 +1,4 @@
+import { IXRRequestImage } from "@galacean/engine-design";
 import { XRRequestTracking } from "../XRRequestTracking";
 import { XRReferenceImage } from "./XRReferenceImage";
 import { XRTrackedImage } from "./XRTrackedImage";
@@ -5,7 +6,7 @@ import { XRTrackedImage } from "./XRTrackedImage";
 /**
  * The request image in XR space.
  */
-export class XRRequestImage extends XRRequestTracking<XRTrackedImage> {
+export class XRRequestImage extends XRRequestTracking<XRTrackedImage> implements IXRRequestImage {
   /**
    * @param image - The image to be tracked
    */
