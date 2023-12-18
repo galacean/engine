e74db44c4ba910307f48938c8888e960ce6dcb50
JujieX <jx2320@columbia.edu>
2023-12-18 12:01:28 +0800

    feat: add audio contentRestorer

---
+++ packages/loader/src/AudioContentRestorer.ts
@@ -0,0 +1,27 @@
+import { AssetPromise, ContentRestorer, request, AudioClip, TextureCubeFace } from "@galacean/engine-core";
+import { RequestConfig } from "@galacean/engine-core/types/asset/request";
+/**
+ * @internal
+ */
+export class AudioContentRestorer extends ContentRestorer<AudioClip> {
+  constructor(
+    resource: AudioClip,
+    public url: string,
+    public requestConfig: RequestConfig
+  ) {
+    super(resource);
+  }
+
+  override restoreContent(): AssetPromise<AudioClip> {
+    return request<ArrayBuffer>(this.url, this.requestConfig)
+      .then((arrayBuffer) => {
+        // @ts-ignore
+        return resource._context.decodeAudioData(arrayBuffer);
+      })
+      .then((audioBuffer) => {
+        const resource = this.resource;
+        resource.setData(audioBuffer);
+        return resource;
+      });
+  }
+}
--- packages/loader/src/AudioLoader.ts
+++ packages/loader/src/AudioLoader.ts
@@ -4,20 +4,35 @@ import {
   AssetPromise,
   AssetType,
   LoadItem,
-  AudioManager,
   AudioClip,
   ResourceManager
 } from "@galacean/engine-core";
+import { RequestConfig } from "@galacean/engine-core/types/asset/request";
+import { AudioContentRestorer } from "./AudioContentRestorer";
 @resourceLoader(AssetType.Audio, ["mp3", "ogg", "wav"], false)
 class AudioLoader extends Loader<AudioClip> {
   load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<AudioClip> {
     return new AssetPromise((resolve, reject) => {
-      this.request<ArrayBuffer>(item.url, { type: "arraybuffer" }).then((arrayBuffer) => {
-        AudioManager.context
+      const url = item.url;
+      const requestConfig = <RequestConfig>{
+        ...item,
+        type: "arraybuffer"
+      };
+
+      this.request<ArrayBuffer>(url, requestConfig).then((arrayBuffer) => {
+        const audioClip = new AudioClip(resourceManager.engine);
+        // @ts-ignore
+        audioClip._context
           .decodeAudioData(arrayBuffer)
           .then((result: AudioBuffer) => {
-            const audioClip = new AudioClip(resourceManager.engine);
             audioClip.setData(result);
+
+            if (url.indexOf("data:") !== 0) {
+              const index = url.lastIndexOf("/");
+              audioClip.name = url.substring(index + 1);
+            }
+
+            resourceManager.addContentRestorer(new AudioContentRestorer(audioClip, url, requestConfig));
             resolve(audioClip);
           })
           .catch((e) => {
