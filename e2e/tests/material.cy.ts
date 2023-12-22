f3a70690343a1d2ec90affc385c8fbfe21250d63
zhuxudong <callzhuxudong@163.com>
2023-12-22 16:01:41 +0800

    test: modify threshold

--- e2e/tests/material.cy.ts
+++ e2e/tests/material.cy.ts
@@ -1,29 +1,29 @@
 describe("Material", () => {
   it("PBR", () => {
-    cy.screenshotWithThreshold("Material", "material-pbr", 0.1);
+    cy.screenshotWithThreshold("Material", "material-pbr", 0.3);
   });
 
   it("PBR Clearcoat", () => {
-    cy.screenshotWithThreshold("Material", "material-pbr-clearcoat", 0.1);
+    cy.screenshotWithThreshold("Material", "material-pbr-clearcoat", 0.3);
   });
 
   it("Unlit", () => {
-    cy.screenshotWithThreshold("Material", "material-unlit", 0.1);
+    cy.screenshotWithThreshold("Material", "material-unlit", 0.3);
   });
 
   it("Blinn Phong", () => {
-    cy.screenshotWithThreshold("Material", "material-blinn-phong", 0.1);
+    cy.screenshotWithThreshold("Material", "material-blinn-phong", 0.3);
   });
 
   it("Blend Mode", () => {
-    cy.screenshotWithThreshold("Material", "material-blendMode", 0.1);
+    cy.screenshotWithThreshold("Material", "material-blendMode", 0.3);
   });
 
   it("ShaderLab", () => {
-    cy.screenshotWithThreshold("Material", "material-shaderLab", 0.1);
+    cy.screenshotWithThreshold("Material", "material-shaderLab", 0.3);
   });
 
   it("Shader Replacement", () => {
-    cy.screenshotWithThreshold("Material", "material-shaderReplacement", 0.1);
+    cy.screenshotWithThreshold("Material", "material-shaderReplacement", 0.3);
   });
 });
