8f08e128d920c07c05e832525f693c0dec44b5d7
cptbtptpbcptdtptp <njktsmshh@163.com>
2023-12-27 19:59:28 +0800

    feat: use texture if the atlas fails to load

--- packages/loader/src/SpriteLoader.ts
+++ packages/loader/src/SpriteLoader.ts
@@ -13,55 +13,41 @@ import {
 @resourceLoader(AssetType.Sprite, ["sprite"], false)
 class SpriteLoader extends Loader<Sprite> {
   load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<Sprite> {
-    return new AssetPromise((resolve, reject) => {
-      this.request<any>(item.url, {
-        ...item,
-        type: "json"
-      })
-        .then((data) => {
-          (data.belongToAtlas
-            ? this._loadFromAtlas(resourceManager, data)
-            : this._loadFromTexture(resourceManager, data)
-          )
-            .then(resolve)
-            .catch(reject);
-        })
-        .catch(reject);
+    return this.request<any>(item.url, {
+      ...item,
+      type: "json"
+    }).then((data) => {
+      return data.belongToAtlas
+        ? this._loadFromAtlas(resourceManager, data)
+        : this._loadFromTexture(resourceManager, data);
     });
   }
 
-  private _loadFromAtlas(resourceManager: ResourceManager, data: any): Promise<Sprite> {
-    return new Promise<Sprite>((resolve, reject) => {
+  private _loadFromAtlas(resourceManager: ResourceManager, data: any): AssetPromise<Sprite> {
+    return (
       resourceManager
         // @ts-ignore
         .getResourceByRef(data.belongToAtlas)
         .then((atlas: SpriteAtlas) => {
-          const sprite = atlas.getSprite(data.fullPath);
-          if (sprite) {
-            resolve(sprite);
-          } else {
-            this._loadFromTexture(resourceManager, data).then((sprite) => {
-              resolve(sprite);
-            });
-          }
+          return atlas.getSprite(data.fullPath) || this._loadFromTexture(resourceManager, data);
         })
-        .catch(reject);
-    });
+    );
   }
 
-  private _loadFromTexture(resourceManager: ResourceManager, data: any): Promise<Sprite> {
+  private _loadFromTexture(resourceManager: ResourceManager, data: any): AssetPromise<Sprite> {
     if (data.texture) {
-      return new Promise<Sprite>((resolve, reject) => {
+      return (
         resourceManager
           // @ts-ignore
           .getResourceByRef(data.texture)
           .then((texture: Texture2D) => {
-            resolve(new Sprite(resourceManager.engine, texture, data.region, data.pivot, data.border));
+            return new Sprite(resourceManager.engine, texture, data.region, data.pivot, data.border);
           })
-          .catch(reject);
-      });
+      );
     } else {
-      return Promise.resolve(new Sprite(resourceManager.engine, null, data.region, data.pivot, data.border));
+      return new AssetPromise((resolve) => {
+        resolve(new Sprite(resourceManager.engine, null, data.region, data.pivot, data.border));
+      });
     }
   }
 }
