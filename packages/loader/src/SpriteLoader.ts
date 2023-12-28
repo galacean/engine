7430c7c0a1c63d3e9570db72ba056b875216c1de
AZhan <njktsmshh@163.com>
2023-12-28 10:33:07 +0800

    Use texture if the atlas fails to load (#1931)
    * feat: use texture if the atlas fails to load

--- packages/loader/src/SpriteLoader.ts
+++ packages/loader/src/SpriteLoader.ts
@@ -1,45 +1,53 @@
 import {
-  resourceLoader,
-  Loader,
   AssetPromise,
   AssetType,
   LoadItem,
-  Sprite,
-  Texture2D,
+  Loader,
   ResourceManager,
-  SpriteAtlas
+  Sprite,
+  SpriteAtlas,
+  Texture2D,
+  resourceLoader
 } from "@galacean/engine-core";
 
 @resourceLoader(AssetType.Sprite, ["sprite"], false)
 class SpriteLoader extends Loader<Sprite> {
   load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<Sprite> {
-    return new AssetPromise((resolve, reject) => {
-      this.request<any>(item.url, {
-        ...item,
-        type: "json"
-      })
-        .then((data) => {
-          if (data.belongToAtlas) {
-            resourceManager
-              // @ts-ignore
-              .getResourceByRef<SpriteAtlas>(data.belongToAtlas)
-              .then((atlas) => {
-                resolve(atlas.getSprite(data.fullPath));
-              })
-              .catch(reject);
-          } else if (data.texture) {
-            resourceManager
-              // @ts-ignore
-              .getResourceByRef<Texture2D>(data.texture)
-              .then((texture) => {
-                resolve(new Sprite(resourceManager.engine, texture, data.region, data.pivot, data.border));
-              })
-              .catch(reject);
-          } else {
-            resolve(new Sprite(resourceManager.engine, null, data.region, data.pivot, data.border));
-          }
-        })
-        .catch(reject);
+    return this.request<any>(item.url, {
+      ...item,
+      type: "json"
+    }).then((data) => {
+      return data.belongToAtlas
+        ? this._loadFromAtlas(resourceManager, data)
+        : this._loadFromTexture(resourceManager, data);
     });
+  }
+
+  private _loadFromAtlas(resourceManager: ResourceManager, data: any): AssetPromise<Sprite> {
+    return (
+      resourceManager
+        // @ts-ignore
+        .getResourceByRef(data.belongToAtlas)
+        .then((atlas: SpriteAtlas) => {
+          return atlas.getSprite(data.fullPath) || this._loadFromTexture(resourceManager, data);
+        })
+    );
+  }
+
+  private _loadFromTexture(resourceManager: ResourceManager, data: any): AssetPromise<Sprite> {
+    if (data.texture) {
+      return (
+        resourceManager
+          // @ts-ignore
+          .getResourceByRef(data.texture)
+          .then((texture: Texture2D) => {
+            return new Sprite(resourceManager.engine, texture, data.region, data.pivot, data.border);
+          })
+      );
+    } else {
+      return new AssetPromise((resolve) => {
+        resolve(new Sprite(resourceManager.engine, null, data.region, data.pivot, data.border));
+      });
+    }
   }
 }
