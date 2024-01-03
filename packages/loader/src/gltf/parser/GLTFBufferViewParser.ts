bea7da59fc3ab0b5d2cb2e796ad860263ca8b433
gz65555 <gz65555@gmail.com>
2024-01-03 16:17:22 +0800

    refactor: follow instruction

--- packages/loader/src/gltf/parser/GLTFBufferViewParser.ts
+++ packages/loader/src/gltf/parser/GLTFBufferViewParser.ts
@@ -5,13 +5,13 @@ import { GLTFParser } from "./GLTFParser";
 export class GLTFBufferViewParser extends GLTFParser {
   parse(context: GLTFParserContext, index: number): Promise<Uint8Array> {
     const bufferView = context.glTF.bufferViews[index];
-    const { extensions, byteOffset = 0, byteLength, buffer } = bufferView;
+    const { extensions, byteOffset = 0, byteLength, buffer: bufferIndex } = bufferView;
     let bufferViewDataPromise: Promise<Uint8Array>;
     bufferViewDataPromise = extensions
       ? <Promise<Uint8Array>>GLTFParser.executeExtensionsCreateAndParse(extensions, context, bufferView)
       : context
-          .get<ArrayBuffer>(GLTFParserType.Buffer)
-          .then((buffers) => new Uint8Array(buffers[buffer], byteOffset, byteLength));
+          .get<ArrayBuffer>(GLTFParserType.Buffer, bufferIndex)
+          .then((buffer) => new Uint8Array(buffer, byteOffset, byteLength));
     return bufferViewDataPromise;
   }
 }
