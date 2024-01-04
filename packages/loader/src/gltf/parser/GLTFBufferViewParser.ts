4e611dfc31642904a77a14dad195149531fb5c04
gz65555 <gz65555@gmail.com>
2024-01-04 14:45:11 +0800

    refactor: remove buffer view promise

--- packages/loader/src/gltf/parser/GLTFBufferViewParser.ts
+++ packages/loader/src/gltf/parser/GLTFBufferViewParser.ts
@@ -6,12 +6,10 @@ export class GLTFBufferViewParser extends GLTFParser {
   parse(context: GLTFParserContext, index: number): Promise<Uint8Array> {
     const bufferView = context.glTF.bufferViews[index];
     const { extensions, byteOffset = 0, byteLength, buffer: bufferIndex } = bufferView;
-    let bufferViewDataPromise: Promise<Uint8Array>;
-    bufferViewDataPromise = extensions
+    return extensions
       ? <Promise<Uint8Array>>GLTFParser.executeExtensionsCreateAndParse(extensions, context, bufferView)
       : context
           .get<ArrayBuffer>(GLTFParserType.Buffer, bufferIndex)
           .then((buffer) => new Uint8Array(buffer, byteOffset, byteLength));
-    return bufferViewDataPromise;
   }
 }
