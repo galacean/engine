d026725ee4b2fb7b4c302684489a544d01d4a40d
gz65555 <gz65555@gmail.com>
2024-01-09 11:16:58 +0800

    ci: fix lint error

--- packages/core/src/Utils.ts
+++ packages/core/src/Utils.ts
@@ -65,7 +65,7 @@ export class Utils {
       return relativeUrl;
     }
 
-    return relativeUrl ? baseUrl.replace(/\/+$/, '') + '/' + relativeUrl.replace(/^\/+/, '') : baseUrl
+    return relativeUrl ? baseUrl.replace(/\/+$/, '') + '/' + relativeUrl.replace(/^\/+/, '') : baseUrl;
   }
 
   /**
