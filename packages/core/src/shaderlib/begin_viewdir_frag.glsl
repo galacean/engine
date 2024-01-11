7821b58c92bbbeeb6bf60b333c30df8dba149937
zhuxudong <callzhuxudong@163.com>
2024-01-11 15:48:08 +0800

    fix: view dir in ortho projection

--- packages/core/src/shaderlib/begin_viewdir_frag.glsl
+++ packages/core/src/shaderlib/begin_viewdir_frag.glsl
@@ -1,3 +1,5 @@
-#ifdef MATERIAL_NEED_WORLD_POS
+#ifdef CAMERA_ORTHOGRAPHIC
+    vec3 V = -camera_Forward;
+#elif defined(MATERIAL_NEED_WORLD_POS)
     vec3 V =  normalize( camera_Position - v_pos );
 #endif
