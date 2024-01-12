14f9e5a23def54a4ead4dad71b40717a4f6ab396
zhuxudong <callzhuxudong@163.com>
2024-01-12 15:24:44 +0800

    refactor: format code

--- packages/core/src/shaderlib/begin_viewdir_frag.glsl
+++ packages/core/src/shaderlib/begin_viewdir_frag.glsl
@@ -1,5 +1,7 @@
 #ifdef CAMERA_ORTHOGRAPHIC
     vec3 V = -camera_Forward;
-#elif defined(MATERIAL_NEED_WORLD_POS)
-    vec3 V =  normalize( camera_Position - v_pos );
+#else
+    #ifdef MATERIAL_NEED_WORLD_POS
+        vec3 V =  normalize( camera_Position - v_pos );
+    #endif
 #endif
