71bf9dd0d94e468e9a791e1541d27851947fdc7a
GuoLei1990 <gl3336563@163.com>
2024-02-23 12:03:54 +0800

    refactor: opt code

--- packages/core/src/shaderlib/extra/Blit.fs.glsl
+++ packages/core/src/shaderlib/extra/Blit.fs.glsl
@@ -1,12 +1,14 @@
 uniform mediump sampler2D renderer_BlitTexture;
-uniform float renderer_BlitMipLevel;
+#ifdef HAS_TEX_LOD
+	uniform float renderer_BlitMipLevel;
+#endif
 varying vec2 v_uv;
 
 void main() {
 	#ifdef HAS_TEX_LOD
 		gl_FragColor = texture2DLodEXT( renderer_BlitTexture, v_uv, renderer_BlitMipLevel );
 	#else
-		gl_FragColor = texture2D( renderer_BlitTexture, v_uv, renderer_BlitMipLevel );
+		gl_FragColor = texture2D( renderer_BlitTexture, v_uv );
 	#endif
 }
 
