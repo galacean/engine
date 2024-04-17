af56040da26863fcb5ce6143c75e85957551b773
zhuxudong <callzhuxudong@163.com>
2024-04-17 16:35:18 +0800

    feat: support transparent shadow

--- packages/core/src/shaderlib/extra/shadow-map.fs.glsl
+++ packages/core/src/shaderlib/extra/shadow-map.fs.glsl
@@ -1,22 +1,52 @@
+#include <BayerDither>
+
 #ifdef ENGINE_NO_DEPTH_TEXTURE
-/**
- * Decompose and save depth value.
-*/
-vec4 pack (float depth) {
-  // Use rgba 4 bytes with a total of 32 bits to store the z value, and the accuracy of 1 byte is 1/256.
-  const vec4 bitShift = vec4(1.0, 256.0, 256.0 * 256.0, 256.0 * 256.0 * 256.0);
-  const vec4 bitMask = vec4(1.0/256.0, 1.0/256.0, 1.0/256.0, 0.0);
+  /**
+   * Decompose and save depth value.
+  */
+  vec4 pack (float depth) {
+    // Use rgba 4 bytes with a total of 32 bits to store the z value, and the accuracy of 1 byte is 1/256.
+    const vec4 bitShift = vec4(1.0, 256.0, 256.0 * 256.0, 256.0 * 256.0 * 256.0);
+    const vec4 bitMask = vec4(1.0/256.0, 1.0/256.0, 1.0/256.0, 0.0);
 
-  vec4 rgbaDepth = fract(depth * bitShift); // Calculate the z value of each point
+    vec4 rgbaDepth = fract(depth * bitShift); // Calculate the z value of each point
 
-  // Cut off the value which do not fit in 8 bits
-  rgbaDepth -= rgbaDepth.gbaa * bitMask;
+    // Cut off the value which do not fit in 8 bits
+    rgbaDepth -= rgbaDepth.gbaa * bitMask;
 
-  return rgbaDepth;
-}
+    return rgbaDepth;
+  }
 #endif
 
+
+uniform vec4 material_BaseColor;
+uniform sampler2D material_BaseTexture;
+varying vec2 v_uv;
+uniform float material_AlphaCutoff;
+
 void main() {
+
+
+#if defined(MATERIAL_IS_ALPHA_CUTOFF) || (defined(SCENE_ENABLE_TRANSPARENT_SHADOW) && defined(MATERIAL_IS_TRANSPARENT))
+  float alpha = material_BaseColor.a;
+  #ifdef MATERIAL_HAS_BASETEXTURE
+      alpha *= texture2D(material_BaseTexture, v_uv).a;
+  #endif
+
+  #ifdef MATERIAL_IS_ALPHA_CUTOFF
+    if(alpha < material_AlphaCutoff){
+      discard;
+    }
+  #endif
+
+  #if defined(SCENE_ENABLE_TRANSPARENT_SHADOW) && defined(MATERIAL_IS_TRANSPARENT)
+    if (alpha <= (bayerDither8(floor(mod(gl_FragCoord.xy, 8.0)))) / 64.0) {
+      discard;
+    };
+  #endif
+
+#endif
+
 #ifdef ENGINE_NO_DEPTH_TEXTURE
     gl_FragColor = pack(gl_FragCoord.z);
 #else
--- packages/core/src/shaderlib/shadow/index.ts
+++ packages/core/src/shaderlib/shadow/index.ts
@@ -3,11 +3,13 @@ import ShadowFragmentDeclaration from "./ShadowFragmentDeclaration.glsl";
 import shadow_sample_tent from "./shadow_sample_tent.glsl";
 import ShadowVertexDeclaration from "./ShadowVertexDeclaration.glsl";
 import ShadowVertex from "./ShadowVertex.glsl";
+import BayerDither from "./bayerDither.glsl";
 
 export default {
   ShadowCoord,
   ShadowFragmentDeclaration,
   shadow_sample_tent,
   ShadowVertexDeclaration,
-  ShadowVertex
+  ShadowVertex,
+  BayerDither
 };
