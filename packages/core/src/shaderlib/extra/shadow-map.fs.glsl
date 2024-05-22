87d0d483bae45a2878f9543a88fa6f58561bd883
zhuxudong <callzhuxudong@163.com>
2024-05-22 12:29:40 +0800

    refactor: use interleaved gradient noise

--- packages/core/src/shaderlib/extra/shadow-map.fs.glsl
+++ packages/core/src/shaderlib/extra/shadow-map.fs.glsl
@@ -1,5 +1,3 @@
-#include <BayerDither>
-
 #ifdef ENGINE_NO_DEPTH_TEXTURE
   /**
    * Decompose and save depth value.
@@ -21,35 +19,34 @@
 
 uniform vec4 material_BaseColor;
 uniform sampler2D material_BaseTexture;
-varying vec2 v_uv;
 uniform float material_AlphaCutoff;
+varying vec2 v_uv;
 
 void main() {
-
-
-#if defined(MATERIAL_IS_ALPHA_CUTOFF) || (defined(SCENE_ENABLE_TRANSPARENT_SHADOW) && defined(MATERIAL_IS_TRANSPARENT))
-  float alpha = material_BaseColor.a;
-  #ifdef MATERIAL_HAS_BASETEXTURE
-      alpha *= texture2D(material_BaseTexture, v_uv).a;
+  #if defined(MATERIAL_IS_ALPHA_CUTOFF) || (defined(SCENE_ENABLE_TRANSPARENT_SHADOW) && defined(MATERIAL_IS_TRANSPARENT))
+    float alpha = material_BaseColor.a;
+    #ifdef MATERIAL_HAS_BASETEXTURE
+        alpha *= texture2D(material_BaseTexture, v_uv).a;
+    #endif
+  
+    #ifdef MATERIAL_IS_ALPHA_CUTOFF
+      if(alpha < material_AlphaCutoff){
+        discard;
+      }
+    #endif
+  
+    #if defined(SCENE_ENABLE_TRANSPARENT_SHADOW) && defined(MATERIAL_IS_TRANSPARENT)
+      // Interleaved gradient noise
+      float noise = fract(52.982919 * fract(dot(vec2(0.06711, 0.00584), gl_FragCoord.xy)));
+      if (alpha <= noise) {
+        discard;
+      };
+    #endif
   #endif
 
-  #ifdef MATERIAL_IS_ALPHA_CUTOFF
-    if(alpha < material_AlphaCutoff){
-      discard;
-    }
+  #ifdef ENGINE_NO_DEPTH_TEXTURE
+      gl_FragColor = pack(gl_FragCoord.z);
+  #else
+      gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
   #endif
-
-  #if defined(SCENE_ENABLE_TRANSPARENT_SHADOW) && defined(MATERIAL_IS_TRANSPARENT)
-    if (alpha <= (bayerDither8(floor(mod(gl_FragCoord.xy, 8.0)))) / 64.0) {
-      discard;
-    };
-  #endif
-
-#endif
-
-#ifdef ENGINE_NO_DEPTH_TEXTURE
-    gl_FragColor = pack(gl_FragCoord.z);
-#else
-    gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
-#endif
 }
--- packages/core/src/shaderlib/shadow/index.ts
+++ packages/core/src/shaderlib/shadow/index.ts
@@ -3,13 +3,11 @@ import ShadowFragmentDeclaration from "./ShadowFragmentDeclaration.glsl";
 import shadow_sample_tent from "./shadow_sample_tent.glsl";
 import ShadowVertexDeclaration from "./ShadowVertexDeclaration.glsl";
 import ShadowVertex from "./ShadowVertex.glsl";
-import BayerDither from "./bayerDither.glsl";
 
 export default {
   ShadowCoord,
   ShadowFragmentDeclaration,
   shadow_sample_tent,
   ShadowVertexDeclaration,
-  ShadowVertex,
-  BayerDither
+  ShadowVertex
 };
