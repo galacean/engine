3a5ad2d8cada7ed84715aef346038b366fd02821
GuoLei1990 <gl3336563@163.com>
2024-06-17 11:33:04 +0800

    fix: unit test

--- tests/src/core/resource/ResourceManager.test.ts
+++ tests/src/core/resource/ResourceManager.test.ts
@@ -32,7 +32,7 @@ describe("ResourceManager", () => {
   describe("findResourcesByType", () => {
     it("findResourcesByType", () => {
       const textures = engine.resourceManager.findResourcesByType(Texture2D);
-      expect(textures.length).equal(6);
+      expect(textures.length).equal(4);
     });
   });
 
