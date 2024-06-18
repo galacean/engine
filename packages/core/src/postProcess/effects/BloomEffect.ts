3e00e8e1af2e1a5c59212c28607a544e1727478d
zhuxudong <callzhuxudong@163.com>
2024-06-18 18:08:14 +0800

    fix: gamma to linear

--- packages/core/src/postProcess/effects/BloomEffect.ts
+++ packages/core/src/postProcess/effects/BloomEffect.ts
@@ -34,9 +34,10 @@ export class BloomEffect extends PostProcessEffect {
   set threshold(value: number) {
     if (value !== this._threshold) {
       this._threshold = value;
-      const thresholdKnee = value * 0.5; // Hardcoded soft knee
+      const threshold = Color.gammaToLinearSpace(value);
+      const thresholdKnee = threshold * 0.5; // Hardcoded soft knee
       const params = this._material.shaderData.getVector4(BloomEffect._bloomParams);
-      params.x = value;
+      params.x = threshold;
       params.y = thresholdKnee;
     }
   }
@@ -146,67 +147,111 @@ export class BloomEffect extends PostProcessEffect {
 }
 
 const fragPrefilter = `
-varying vec2 v_uv;
-uniform sampler2D renderer_BlitTexture;
-uniform vec4 material_BloomParams;  // x: threshold (gamma), y: threshold knee, z: scatter, w: intensity
-uniform vec4 renderer_texelSize;    // x: 1/width, y: 1/height, z: width, w: height
+  varying vec2 v_uv;
+  uniform sampler2D renderer_BlitTexture;
+  uniform vec4 material_BloomParams;  // x: threshold (gamma), y: threshold knee, z: scatter, w: intensity
+  uniform vec4 renderer_texelSize;    // x: 1/width, y: 1/height, z: width, w: height
 
-#define HALF_MAX  65504.0 // (2 - 2^-10) * 2^15
-#define Threshold      material_BloomParams.x
-#define ThresholdKnee  material_BloomParams.y
-#define Scatter        material_BloomParams.z
+  #define HALF_MAX  65504.0 // (2 - 2^-10) * 2^15
+  #define Threshold      material_BloomParams.x
+  #define ThresholdKnee  material_BloomParams.y
 
-float max3(float a, float b, float c){
-    return max(max(a, b), c);
-}
+  float max3(float a, float b, float c){
+      return max(max(a, b), c);
+  }
 
-void main(){
-  #ifdef BLOOM_HQ
-    vec2 texelSize = renderer_texelSize.xy;
-    vec4 A = texture2D(renderer_BlitTexture, v_uv + texelSize * vec2(-1.0, -1.0));
-    vec4 B = texture2D(renderer_BlitTexture, v_uv + texelSize * vec2(0.0, -1.0));
-    vec4 C = texture2D(renderer_BlitTexture, v_uv + texelSize * vec2(1.0, -1.0));
-    vec4 D = texture2D(renderer_BlitTexture, v_uv + texelSize * vec2(-0.5, -0.5));
-    vec4 E = texture2D(renderer_BlitTexture, v_uv + texelSize * vec2(0.5, -0.5));
-    vec4 F = texture2D(renderer_BlitTexture, v_uv + texelSize * vec2(-1.0, 0.0));
-    vec4 G = texture2D(renderer_BlitTexture, v_uv);
-    vec4 H = texture2D(renderer_BlitTexture, v_uv + texelSize * vec2(1.0, 0.0));
-    vec4 I = texture2D(renderer_BlitTexture, v_uv + texelSize * vec2(-0.5, 0.5));
-    vec4 J = texture2D(renderer_BlitTexture, v_uv + texelSize * vec2(0.5, 0.5));
-    vec4 K = texture2D(renderer_BlitTexture, v_uv + texelSize * vec2(-1.0, 1.0));
-    vec4 L = texture2D(renderer_BlitTexture, v_uv + texelSize * vec2(0.0, 1.0));
-    vec4 M = texture2D(renderer_BlitTexture, v_uv + texelSize * vec2(1.0, 1.0));
+  vec4 gammaToLinear(vec4 srgbIn){
+    return vec4( pow(srgbIn.rgb, vec3(2.2)), srgbIn.a);
+  }
 
-    vec2 scale = vec2(0.5, 0.125);
-    vec2 div = (1.0 / 4.0) * scale;
+  vec4 linearToGamma(vec4 linearIn){
+    return vec4( pow(linearIn.rgb, vec3(1.0 / 2.2)), linearIn.a);
+  }
 
-    vec4 samplerColor = (D + E + I + J) * div.x;
-    samplerColor += (A + B + G + F) * div.y;
-    samplerColor += (B + C + H + G) * div.y;
-    samplerColor += (F + G + L + K) * div.y;
-    samplerColor += (G + H + M + L) * div.y;
-  #else
-    vec4 samplerColor = texture2D(renderer_BlitTexture, v_uv);
-  #endif
+  vec4 sampleTexture(sampler2D tex, vec2 uv){
+    vec4 color = texture2D(tex, uv);
 
-  vec3 color = samplerColor.rgb;
+    #ifndef ENGINE_IS_COLORSPACE_GAMMA
+      color = gammaToLinear(color);
+    #endif 
 
-  // User controlled clamp to limit crazy high broken spec
-  color = min(color, HALF_MAX);
+    return color;
+  }
 
-  // Thresholding
-  float brightness = max3(color.r, color.g, color.b);
-  float softness = clamp(brightness - Threshold + ThresholdKnee, 0.0, 2.0 * ThresholdKnee);
-  softness = (softness * softness) / (4.0 * ThresholdKnee + 1e-4);
-  float multiplier = max(brightness - Threshold, softness) / max(brightness, 1e-4);
-  color *= multiplier;
+  void main(){
+    #ifdef BLOOM_HQ
+      vec2 texelSize = renderer_texelSize.xy;
+      mediump vec4 A = sampleTexture(renderer_BlitTexture, v_uv + texelSize * vec2(-1.0, -1.0));
+      mediump vec4 B = sampleTexture(renderer_BlitTexture, v_uv + texelSize * vec2(0.0, -1.0));
+      mediump vec4 C = sampleTexture(renderer_BlitTexture, v_uv + texelSize * vec2(1.0, -1.0));
+      mediump vec4 D = sampleTexture(renderer_BlitTexture, v_uv + texelSize * vec2(-0.5, -0.5));
+      mediump vec4 E = sampleTexture(renderer_BlitTexture, v_uv + texelSize * vec2(0.5, -0.5));
+      mediump vec4 F = sampleTexture(renderer_BlitTexture, v_uv + texelSize * vec2(-1.0, 0.0));
+      mediump vec4 G = sampleTexture(renderer_BlitTexture, v_uv);
+      mediump vec4 H = sampleTexture(renderer_BlitTexture, v_uv + texelSize * vec2(1.0, 0.0));
+      mediump vec4 I = sampleTexture(renderer_BlitTexture, v_uv + texelSize * vec2(-0.5, 0.5));
+      mediump vec4 J = sampleTexture(renderer_BlitTexture, v_uv + texelSize * vec2(0.5, 0.5));
+      mediump vec4 K = sampleTexture(renderer_BlitTexture, v_uv + texelSize * vec2(-1.0, 1.0));
+      mediump vec4 L = sampleTexture(renderer_BlitTexture, v_uv + texelSize * vec2(0.0, 1.0));
+      mediump vec4 M = sampleTexture(renderer_BlitTexture, v_uv + texelSize * vec2(1.0, 1.0));
 
-  // Clamp colors to positive once in prefilter. Encode can have a sqrt, and sqrt(-x) == NaN. Up/Downsample passes would then spread the NaN.
-  color = max(color, 0.0);
+      mediump vec2 scale = vec2(0.5, 0.125);
+      mediump vec2 div = (1.0 / 4.0) * scale;
 
-  gl_FragColor = vec4(color, samplerColor.a);
+      mediump vec4 samplerColor = (D + E + I + J) * div.x;
+      samplerColor += (A + B + G + F) * div.y;
+      samplerColor += (B + C + H + G) * div.y;
+      samplerColor += (F + G + L + K) * div.y;
+      samplerColor += (G + H + M + L) * div.y;
+    #else
+      mediump vec4 samplerColor = sampleTexture(renderer_BlitTexture, v_uv);
+    #endif
 
-}
+    mediump vec3 color = samplerColor.rgb;
+
+    // User controlled clamp to limit crazy high broken spec
+    color = min(color, HALF_MAX);
+
+    // Thresholding
+    mediump float brightness = max3(color.r, color.g, color.b);
+    mediump float softness = clamp(brightness - Threshold + ThresholdKnee, 0.0, 2.0 * ThresholdKnee);
+    softness = (softness * softness) / (4.0 * ThresholdKnee + 1e-4);
+    mediump float multiplier = max(brightness - Threshold, softness) / max(brightness, 1e-4);
+    color *= multiplier;
+
+    // Clamp colors to positive once in prefilter. Encode can have a sqrt, and sqrt(-x) == NaN. Up/Downsample passes would then spread the NaN.
+    color = max(color, 0.0);
+
+    gl_FragColor = vec4(color, samplerColor.a);
+    
+    #ifndef ENGINE_IS_COLORSPACE_GAMMA
+      gl_FragColor = linearToGamma(gl_FragColor);
+    #endif
+  }
 `;
 
-Shader.create(BloomEffect.SHADER_NAME, [new ShaderPass("Bloom Prefilter", blitVs, fragPrefilter)]);
+const fragBlurH = `
+  varying vec2 v_uv;
+  uniform vec4 renderer_texelSize;    // x: 1/width, y: 1/height, z: width, w: height
+
+  void main(){
+
+  }
+`;
+
+const fragBlurV = `
+`;
+
+const fragUpsample = `
+`;
+
+const fragUber = `
+`;
+
+Shader.create(BloomEffect.SHADER_NAME, [
+  new ShaderPass("Bloom Prefilter", blitVs, fragPrefilter),
+  new ShaderPass("Bloom Blur Horizontal", blitVs, fragBlurH),
+  new ShaderPass("Bloom Blur Vertical", blitVs, fragBlurV),
+  new ShaderPass("Bloom Upsample", blitVs, fragUpsample),
+  new ShaderPass("Bloom Uber", blitVs, fragUber)
+]);
