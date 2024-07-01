46588594389f9e2f924b14bdab24bbd20f224c84
zhubei.zb <zhubei.zb@antgroup.com>
2024-07-01 18:35:47 +0800

    feat: add constructor to sub primitive

--- packages/core/src/graphic/SubPrimitive.ts
+++ packages/core/src/graphic/SubPrimitive.ts
@@ -7,4 +7,15 @@ export class SubPrimitive {
   count: number;
   /** Drawing topology. */
   topology: MeshTopology;
+  /**
+   * Create a sub-primitive.
+   * @param start - Start drawing offset
+   * @param count - Drawing count
+   * @param topology - Drawing topology
+   */
+  constructor(start: number = 0, count: number = 0, topology: MeshTopology = MeshTopology.Triangles) {
+    this.start = start;
+    this.count = count;
+    this.topology = topology;
+  }
 }
