409ad66c6a80f4c1bfb5dce13e26155ba2de0091
zhuxudong <callzhuxudong@163.com>
2024-07-02 19:39:08 +0800

    refactor: neutral shader refactor

--- packages/core/src/postProcess/effects/TonemappingEffect.ts
+++ packages/core/src/postProcess/effects/TonemappingEffect.ts
@@ -1,4 +1,3 @@
-import { Vector4 } from "@galacean/engine-math";
 import { Engine } from "../../Engine";
 import { PipelineUtils } from "../../RenderPipeline/PipelineUtils";
 import { RenderContext } from "../../RenderPipeline/RenderContext";
@@ -54,32 +53,6 @@ export class TonemappingEffect extends PostProcessEffect {
   constructor(engine: Engine) {
     super(engine);
     this._material = new Material(engine, shader);
-    const shaderData = this._material.shaderData;
-
-    // Those could be tweakable for the neutral tonemapper, but in the case of the LookDev we don't need that
-    const k_BlackIn = 0.02;
-    const k_WhiteIn = 10.0;
-    const k_BlackOut = 0.0;
-    const k_WhiteOut = 10.0;
-    const k_WhiteLevel = 5.3;
-    const k_WhiteClip = 10.0;
-    const k_DialUnits = 20.0;
-    const k_HalfDialUnits = k_DialUnits * 0.5;
-
-    shaderData.setVector4(
-      "material_ToneMapCoeffs1",
-      new Vector4(
-        k_BlackIn * k_DialUnits + 1.0,
-        k_BlackOut * k_HalfDialUnits + 1.0,
-        k_WhiteIn / k_DialUnits,
-        1.0 - k_WhiteOut / k_DialUnits
-      )
-    );
-    shaderData.setVector4(
-      "material_ToneMapCoeffs2",
-      new Vector4(0.0, 0.0, k_WhiteLevel, k_WhiteClip / k_HalfDialUnits)
-    );
-
     this.mode = TonemappingMode.Neutral;
   }
 
@@ -94,15 +67,6 @@ const shader = Shader.create(
   `
   varying vec2 v_uv;
 	uniform sampler2D renderer_BlitTexture;
-  uniform vec4 material_ToneMapCoeffs1;
-  uniform vec4 material_ToneMapCoeffs2;
-
-  #define InBlack         material_ToneMapCoeffs1.x
-  #define OutBlack        material_ToneMapCoeffs1.y
-  #define InWhite         material_ToneMapCoeffs1.z
-  #define OutWhite        material_ToneMapCoeffs1.w
-  #define WhiteLevel      material_ToneMapCoeffs2.z
-  #define WhiteClip       material_ToneMapCoeffs2.w
 
   #define saturate( a ) clamp( a, 0.0, 1.0 )
 
@@ -114,59 +78,37 @@ const shader = Shader.create(
     return vec4( pow(linearIn.rgb, vec3(1.0 / 2.2)), linearIn.a);
   }
 
-  vec3 evalCurve(vec3 x, float A, float B, float C, float D, float E, float F){
-      return ((x*(A*x + C*B) + D*E) / (x*(A*x + B) + D*F)) - E / F;
+  
+  // Neutral tonemapping (Hable/Hejl/Frostbite)
+  // Input is linear RGB
+  // More accuracy to avoid NaN on extremely high values.
+  vec3 neutralCurve(vec3 x, float a, float b, float c, float d, float e, float f){
+      return vec3(((x * (a * x + c * b) + d * e) / (x * (a * x + b) + d * f)) - e / f);
   }
-
-  const mat3 AP1_2_AP0_MAT = mat3(
-    vec3(0.6954522414, 0.0447945634, -0.0055258826),
-    vec3(0.1406786965, 0.8596711185, 0.0040252103),
-    vec3(0.1638690622, 0.0955343182, 1.0015006723));
-
-  // ACES Color Space Conversion - ACEScg to ACES
-  vec3 ACEScg_to_ACES(vec3 x){
-      return AP1_2_AP0_MAT * x;
-  }
-
-
-  vec3 applyTonemapFilmicAD(vec3 linearColor){
-      float blackRatio = InBlack / OutBlack;
-      float whiteRatio = InWhite / OutWhite;
-
-      // blend tunable coefficients
-      float B = mix(0.57, 0.37, blackRatio);
-      float C = mix(0.01, 0.24, whiteRatio);
-      float D = mix(0.02, 0.20, blackRatio);
-
-      // constants
-      float A = 0.2;
-      float E = 0.02;
-      float F = 0.30;
-
-      // eval and correct for white point
-      vec3 whiteScale = 1.0 / evalCurve(vec3(WhiteLevel), A, B, C, D, E, F);
-      vec3 curr = evalCurve(linearColor * whiteScale, A, B, C, D, E, F);
-
-      return curr*whiteScale;
-  }
-
-  vec3 remapWhite(vec3 inPixel, float whitePt){
-      //  var breakout for readability
-      float inBlack = 0.0;
-      float outBlack = 0.0;
-      float inWhite = whitePt;
-      float outWhite = 1.0;
-
-      // remap input range to output range
-      vec3 outPixel = ((inPixel.rgb) - vec3(inBlack)) / (vec3(inWhite) - vec3(inBlack)) * (vec3(outWhite) - vec3(outBlack)) + vec3(outBlack);
-      return (outPixel.rgb);
-  }
+  
+  #define TONEMAPPING_CLAMP_MAX 435.18712 //(-b + sqrt(b * b - 4 * a * (HALF_MAX - d * f))) / (2 * a * whiteScale)
 
   vec3 neutralTonemap(vec3 x){
-      vec3 finalColor = applyTonemapFilmicAD(x); // curve (dynamic coeffs differ per level)
-      finalColor = remapWhite(finalColor, WhiteClip); // post-curve white point adjustment
-      finalColor = saturate(finalColor);
-      return finalColor;
+    // Tonemap
+    float a = 0.2;
+    float b = 0.29;
+    float c = 0.24;
+    float d = 0.272;
+    float e = 0.02;
+    float f = 0.3;
+    vec3 whiteLevel = vec3(5.3);
+    vec3 whiteClip = vec3(1.0);
+
+    x = min(x, TONEMAPPING_CLAMP_MAX);
+
+    vec3 whiteScale = 1.0 / neutralCurve(whiteLevel, a, b, c, d, e, f);
+    x = neutralCurve(x * whiteScale, a, b, c, d, e, f);
+    x *= whiteScale;
+
+    // Post-curve white point adjustment
+    x /= whiteClip;
+
+    return x;
   }
 
 
@@ -180,8 +122,6 @@ const shader = Shader.create(
     #if TONEMAPPING_MODE == 1
       color.rgb = neutralTonemap(color.rgb);
     #elif TONEMAPPING_MODE == 2
-      // Note: input is actually ACEScg (AP1 w/ linear encoding)
-      vec3 aces = ACEScg_to_ACES(color.rgb);
       // color.rgb = AcesTonemap(aces);
     #endif
 
