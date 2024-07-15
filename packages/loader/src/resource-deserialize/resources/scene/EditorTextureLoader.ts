a93b21d9043928358de8e7a23cad32000df41fae
gz65555 <gz65555@gmail.com>
2024-07-15 14:12:47 +0800

    fix: missing params in some loaders

--- packages/loader/src/resource-deserialize/resources/scene/EditorTextureLoader.ts
+++ packages/loader/src/resource-deserialize/resources/scene/EditorTextureLoader.ts
@@ -5,7 +5,7 @@ import { decode } from "../..";
 export class EditorTextureLoader extends Loader<Texture2D> {
   load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<Texture2D> {
     return new AssetPromise((resolve, reject) => {
-      this.request<ArrayBuffer>(item.url, { type: "arraybuffer" })
+      this.request<ArrayBuffer>(item.url, { ...item, type: "arraybuffer" })
         .then((data) => {
           decode<Texture2D>(data, resourceManager.engine).then((texture) => {
             resolve(texture);
