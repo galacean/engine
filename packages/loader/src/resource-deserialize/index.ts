dcb2d30a28109d35a96784824e73fde59838092e
cptbtptpbcptdtptp <njktsmshh@163.com>
2024-07-16 14:21:29 +0800

    feat: merge code

--- packages/loader/src/resource-deserialize/index.ts
+++ packages/loader/src/resource-deserialize/index.ts
@@ -3,11 +3,11 @@ import { BufferReader } from "./utils/BufferReader";
 import { decoderMap } from "./utils/Decorator";
 import { FileHeader } from "./utils/FileHeader";
 
+export { MeshDecoder } from "./resources/mesh/MeshDecoder";
+export { Texture2DDecoder } from "./resources/texture2D/TextureDecoder";
+export { ReflectionParser } from "./resources/parser/ReflectionParser";
 export * from "./resources/animationClip/AnimationClipDecoder";
 export type { IModelMesh } from "./resources/mesh/IModelMesh";
-export { MeshDecoder } from "./resources/mesh/MeshDecoder";
-export { ReflectionParser } from "./resources/parser/ReflectionParser";
-export { Texture2DDecoder } from "./resources/texture2D/TextureDecoder";
 
 /**
  * Decode engine binary resource.
@@ -24,11 +24,11 @@ export function decode<T>(arrayBuffer: ArrayBuffer, engine: Engine): Promise<T> 
   });
 }
 
-export * from "./resources/parser/ParserContext";
-export * from "./resources/scene/EditorTextureLoader";
-export * from "./resources/scene/MeshLoader";
-export * from "./resources/scene/SceneParser";
 export * from "./resources/schema";
+export * from "./resources/scene/SceneParser";
+export * from "./resources/scene/MeshLoader";
+export * from "./resources/scene/EditorTextureLoader";
+export * from "./resources/parser/ParserContext";
 
 export * from "./utils/BufferReader";
 export * from "./utils/Decorator";
