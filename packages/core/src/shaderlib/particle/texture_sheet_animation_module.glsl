c75523c07719a3a7977bafff304ee55fedc6c2ee
JujieX <jx2320@columbia.edu>
2024-08-19 11:34:32 +0800

    fix: opt code

--- packages/core/src/shaderlib/particle/texture_sheet_animation_module.glsl
+++ packages/core/src/shaderlib/particle/texture_sheet_animation_module.glsl
@@ -13,12 +13,12 @@ vec2 computeParticleUV(in vec2 uv, in float normalizedAge) {
         float scaledNormalizedAge = normalizedAge * renderer_TSACycles;
         float cycleNormalizedAge = scaledNormalizedAge - floor(scaledNormalizedAge);
         float normalizedFrame = evaluateParticleCurve(renderer_TSAFrameMaxCurve, cycleNormalizedAge);
-        
         #ifdef RENDERER_TSA_FRAME_RANDOM_CURVES
             normalizedFrame = mix(evaluateParticleCurve(renderer_TSAFrameMinCurve, cycleNormalizedAge), normalizedFrame, a_Random1.x);
         #endif
 
         float frame = floor(normalizedFrame * renderer_TSATillingParams.z);
+
         float totalULength = frame * renderer_TSATillingParams.x;
         float floorTotalULength = floor(totalULength);
         uv.x += totalULength - floorTotalULength;
