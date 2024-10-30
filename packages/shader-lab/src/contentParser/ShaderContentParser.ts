f1180a024dddefb961dec622c470aa0a7eee4f69
Sway007 <yansw007@gmail.com>
2024-10-30 16:27:02 +0800

    feat: code opt

--- packages/shader-lab/src/contentParser/ShaderContentParser.ts
+++ packages/shader-lab/src/contentParser/ShaderContentParser.ts
@@ -221,6 +221,10 @@ export class ShaderContentParser {
     this._symbolTable.newScope(symbolTable);
   }
 
+  private static _dropScope() {
+    this._symbolTable.dropScope();
+  }
+
   private static _parseRenderStatePropList(state: string, scanner: Scanner): IRenderStates {
     const ret: IRenderStates = { constantMap: {}, variableMap: {} };
     while (scanner.getCurChar() !== "}") {
@@ -409,7 +413,7 @@ export class ShaderContentParser {
             braceLevel -= 1;
             if (braceLevel === 0) {
               this._addGlobalStatement(ret, scanner, start, word.lexeme.length);
-              this._symbolTable.dropScope();
+              this._dropScope();
               return ret;
             }
           }
@@ -508,7 +512,7 @@ export class ShaderContentParser {
             braceLevel -= 1;
             if (braceLevel === 0) {
               this._addGlobalStatement(ret, scanner, start, word.lexeme.length);
-              this._symbolTable.dropScope();
+              this._dropScope();
               return ret;
             }
           }
