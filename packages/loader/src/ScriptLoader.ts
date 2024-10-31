2a1e40df56735f0e0b4a61f150e46a95e00ca10d
Kbscript <koubodehao@gmail.com>
2024-10-31 15:57:10 +0800

    fix: lint error

--- packages/loader/src/ScriptLoader.ts
+++ packages/loader/src/ScriptLoader.ts
@@ -5,10 +5,11 @@ class ScriptLoader extends Loader<Script> {
   load(item: LoadItem): AssetPromise<Script> {
     return new AssetPromise((resolve, reject) => {
       const { url } = item;
-      (import(url) as Promise<Script>).then(esModule => {
-        resolve(esModule);
-      }).catch(reject);
+      (import(url) as Promise<Script>)
+        .then((esModule) => {
+          resolve(esModule);
+        })
+        .catch(reject);
     });
   }
-  
 }
