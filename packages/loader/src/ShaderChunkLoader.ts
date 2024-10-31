d12640e6f1df482bd085a9c540b8353243f8b0c5
Sway007 <yansw007@gmail.com>
2024-10-31 17:19:52 +0800

    feat: code opt

--- packages/loader/src/ShaderChunkLoader.ts
+++ packages/loader/src/ShaderChunkLoader.ts
@@ -1,23 +1,23 @@
 import { AssetPromise, LoadItem, Loader, ResourceManager, ShaderFactory, resourceLoader } from "@galacean/engine-core";
 
 @resourceLoader("ShaderChunk", ["glsl"])
-class ShaderChunkLoader extends Loader<void> {
+class ShaderChunkLoader extends Loader<void[]> {
   private static _includeRegex = /^[ \t]*#include +"([^$\\"]+)"/gm;
 
-  load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<void> {
-    return this.request<string>(item.url, { ...item, type: "text" }).then(async (code: string) => {
+  load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<void[]> {
+    return this.request<string>(item.url, { ...item, type: "text" }).then((code: string) => {
       const { includeKey } = item.params;
       ShaderFactory.registerInclude(includeKey, code);
 
       const matches = code.matchAll(ShaderChunkLoader._includeRegex);
-      await Promise.all(
-        Array.from(matches).map((m) => {
+      return Promise.all(
+        matches.map((m) => {
           const path = m[1];
           if (path) {
             // @ts-ignore
             const resource = resourceManager._virtualPathMap[path];
             if (!resource) return;
-            return resourceManager.load({ type: "ShaderChunk", url: resource, params: { includeKey: path } });
+            return resourceManager.load<void>({ type: "ShaderChunk", url: resource, params: { includeKey: path } });
           }
         })
       );
