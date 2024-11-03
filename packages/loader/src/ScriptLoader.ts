89b957b3b0d7bc035fc307141022cb5e2e0fc778
ChenMo <gl3336563@163.com>
2024-11-03 18:57:35 +0800

    Update ScriptLoader.ts

--- packages/loader/src/ScriptLoader.ts
+++ packages/loader/src/ScriptLoader.ts
@@ -9,8 +9,7 @@ interface ESModuleStructure {
 export class ScriptLoader extends Loader<ESModuleStructure> {
   load(item: LoadItem): AssetPromise<ESModuleStructure> {
     return new AssetPromise((resolve, reject) => {
-      const { url } = item;
-      (import(/* @vite-ignore */ url) as Promise<ESModuleStructure>)
+      (import(/* @vite-ignore */ item.url) as Promise<ESModuleStructure>)
         .then((esModule) => {
           resolve(esModule);
         })
