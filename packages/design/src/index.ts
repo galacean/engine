4e666638eb65f7592629bdc03acca52de0a84494
cptbtptpbcptdtptp <njktsmshh@163.com>
2024-11-07 16:12:19 +0800

    feat: update code

--- packages/design/src/index.ts
+++ packages/design/src/index.ts
@@ -1,7 +1,7 @@
 export type { IClone } from "./IClone";
+export * from "./input/index";
 export * from "./physics/index";
 export * from "./renderingHardwareInterface/index";
 export * from "./shader-lab/index";
+export type { IUICanvas } from "./ui/IUICanvas";
 export * from "./xr/index";
-export * from "./input/index";
-export * from "./ui/index";
--- packages/design/src/ui/IUICanvas.ts
+++ packages/design/src/ui/IUICanvas.ts
@@ -1,8 +1,6 @@
 export interface IUICanvas {
   entity: any;
   sortOrder: number;
-  renderMode: number;
-  renderCamera: any;
   _canvasIndex: number;
   _renderElement: any;
   _prepareRender(renderContext: any): void;
