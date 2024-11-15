2749c7612edc397338e13d37e72abcfcdce02501
Sway007 <yansw007@gmail.com>
2024-11-15 17:46:19 +0800

    feat: add unitest

--- tests/src/shader-lab/ShaderLab.test.ts
+++ tests/src/shader-lab/ShaderLab.test.ts
@@ -136,6 +136,8 @@ describe("ShaderLab", () => {
     expect(pass1.renderStates).not.be.null;
 
     const { constantMap, variableMap } = pass1.renderStates;
+    expect(Object.values(variableMap).includes("customRenderQueue"));
+
     expect(constantMap).not.be.null;
 
     expect(toString(constantMap[RenderStateDataKey.BlendStateBlendColor] as Color)).eq("Color(1, 1, 1, 1)");
