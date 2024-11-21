97ec5831ec0db203d148046d3aecf9923c46c8a3
Sway007 <yansw007@gmail.com>
2024-11-21 15:19:06 +0800

    fix: unitest

--- tests/src/loader/Polyfill.test.ts
+++ tests/src/loader/Polyfill.test.ts
@@ -12,28 +12,28 @@ describe("Polyfill", () => {
     const originResultArray = Array.from(originMatchResult);
 
     String.prototype.matchAll = null;
-    await import("@galacean/engine-loader");
+    import("@galacean/engine-loader").then(() => {
+      const regexTest = /noGlobal/;
+      const contentTest = "test";
+      expect(
+        String.prototype.matchAll.bind(contentTest, regexTest),
+        "Should throw error caused by absence of g flag"
+      ).to.throw(TypeError);
 
-    const regexTest = /noGlobal/;
-    const contentTest = "test";
-    expect(
-      String.prototype.matchAll.bind(contentTest, regexTest),
-      "Should throw error caused by absence of g flag"
-    ).to.throw(TypeError);
+      const matchResult = content.matchAll(regex);
+      debugger;
+      expect(!!matchResult.next, "result should be iterable").to.be.true;
+      const resultArray = Array.from(matchResult);
+      expect(resultArray.length).to.equal(3).to.equal(originResultArray.length);
 
-    const matchResult = content.matchAll(regex);
-    debugger;
-    expect(!!matchResult.next, "result should be iterable").to.be.true;
-    const resultArray = Array.from(matchResult);
-    expect(resultArray.length).to.equal(3).to.equal(originResultArray.length);
+      expect(resultArray[0][0]).to.equal(originResultArray[0][0]).to.equal('#include "./f1.glsl"');
+      expect(resultArray[0][1]).to.equal(originResultArray[0][1]).to.equal("./f1.glsl");
 
-    expect(resultArray[0][0]).to.equal(originResultArray[0][0]).to.equal('#include "./f1.glsl"');
-    expect(resultArray[0][1]).to.equal(originResultArray[0][1]).to.equal("./f1.glsl");
+      expect(resultArray[1][0]).to.equal(originResultArray[1][0]).to.equal('#include "./f2.glsl"');
+      expect(resultArray[1][1]).to.equal(originResultArray[1][1]).to.equal("./f2.glsl");
 
-    expect(resultArray[1][0]).to.equal(originResultArray[1][0]).to.equal('#include "./f2.glsl"');
-    expect(resultArray[1][1]).to.equal(originResultArray[1][1]).to.equal("./f2.glsl");
-
-    expect(resultArray[2][0]).to.equal(resultArray[2][0]).to.equal('#include "/f3.glsl"');
-    expect(resultArray[2][1]).to.equal(resultArray[2][1]).to.equal("/f3.glsl");
+      expect(resultArray[2][0]).to.equal(resultArray[2][0]).to.equal('#include "/f3.glsl"');
+      expect(resultArray[2][1]).to.equal(resultArray[2][1]).to.equal("/f3.glsl");
+    });
   });
 });
