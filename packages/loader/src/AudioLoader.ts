45e0d0e5d5730ca2090b6337e5bb89bc1baf38eb
GuoLei1990 <gl3336563@163.com>
2024-12-05 15:07:17 +0800

    refactor: delete restorer

--- packages/loader/src/AudioLoader.ts
+++ packages/loader/src/AudioLoader.ts
@@ -9,7 +9,6 @@ import {
   ResourceManager
 } from "@galacean/engine-core";
 import { RequestConfig } from "@galacean/engine-core/types/asset/request";
-import { AudioContentRestorer } from "./AudioContentRestorer";
 @resourceLoader(AssetType.Audio, ["mp3", "ogg", "wav"])
 class AudioLoader extends Loader<AudioClip> {
   load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<AudioClip> {
@@ -34,7 +33,6 @@ class AudioLoader extends Loader<AudioClip> {
               audioClip.name = url.substring(index + 1);
             }
 
-            resourceManager.addContentRestorer(new AudioContentRestorer(audioClip, url, requestConfig));
             resolve(audioClip);
           })
           .catch((e) => {
