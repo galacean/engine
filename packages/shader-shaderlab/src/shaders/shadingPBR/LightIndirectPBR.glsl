9338ec26193e3d933ef3f0332bfbeafad845fe91
hhhhkrx <zengyan0605@163.com>
2024-12-26 20:48:40 +0800

    fix: diffuseColor mix  refraction

--- packages/shader-shaderlab/src/shaders/shadingPBR/LightIndirectPBR.glsl
+++ packages/shader-shaderlab/src/shaders/shadingPBR/LightIndirectPBR.glsl
@@ -108,15 +108,10 @@ void evaluateIBL(Varyings varyings, SurfaceData surfaceData, BRDFData brdfData, 
     
     #ifdef MATERIAL_ENABLE_TRANSMISSION 
         vec3 refractionTransmitted = evaluateRefraction(surfaceData, brdfData);
-        refractionTransmitted *= surfaceData.transmission;
-        diffuseColor *= (1.0 - surfaceData.transmission);
+        diffuseColor = mix(diffuseColor, refractionTransmitted, surfaceData.transmission);
     #endif
 
     color += diffuseColor + specularColor;
-
-    #ifdef MATERIAL_ENABLE_TRANSMISSION 
-        color.rgb += refractionTransmitted;
-    #endif
 
 }
 
