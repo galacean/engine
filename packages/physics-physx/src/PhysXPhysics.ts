2cbb084c3324c501e24946f3a78ba62e78932abd
luzhuang <364439895@qq.com>
2025-01-07 13:59:18 +0800

    feat: update downgrade version

--- packages/physics-physx/src/PhysXPhysics.ts
+++ packages/physics-physx/src/PhysXPhysics.ts
@@ -90,7 +90,7 @@ export class PhysXPhysics implements IPhysics {
       }
 
       if (runtimeMode == PhysXRuntimeMode.JavaScript) {
-        script.src = `https://mdn.alipayobjects.com/rms/afts/file/A*yYWPSZFhS_0AAAAAAAAAAAAAARQnAQ/physx.no-wasm.js`;
+        script.src = `https://mdn.alipayobjects.com/rms/afts/file/A*CfV8RrDQk5oAAAAAAAAAAAAAARQnAQ/physx.release.downgrade.js`;
       } else if (runtimeMode == PhysXRuntimeMode.WebAssembly) {
         script.src = `https://mdn.alipayobjects.com/rms/afts/file/A*LRfCQ4VcCFQAAAAAAAAAAAAAARQnAQ/physx.release.js`;
       }
