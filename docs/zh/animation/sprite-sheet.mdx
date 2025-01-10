a412d2a53c920a7d01c674e71b8cffec8cc76b21
木鳐 <zhubei.zb@antgroup.com>
2025-01-10 15:12:02 +0800

    feat: merge

---
+++ docs/zh/animation/sprite-sheet.mdx
@@ -0,0 +1,32 @@
+---
+order: 7
+title: 帧动画
+type: 动画
+label: Animation
+---
+
+Galacean 支持引用类型的动画曲线，你可以添加类型为资产的关键帧比如（精灵）下图为制作精灵动画的流程：
+
+1. 给节点添加 `SpriteRenderer` 组件
+
+![alt text](https://mdn.alipayobjects.com/huamei_3zduhr/afts/img/A*XiUaQ76M4Q0AAAAAAAAAAAAADsJ_AQ/original)
+
+2. 添加`精灵`，可以参考[精灵](/docs/graphics/2D/sprite)
+   
+![alt text](https://mdn.alipayobjects.com/huamei_3zduhr/afts/img/A*ababSZAMpJMAAAAAAAAAAAAADsJ_AQ/original)
+  
+3. 在 **[资产面板](/docs/assets/interface)** 中创建 [动画片段](/docs/animation/clip)
+   
+![alt text](https://mdn.alipayobjects.com/huamei_3zduhr/afts/img/A*CZQjSqZAHGsAAAAAAAAAAAAADsJ_AQ/original)
+
+
+4. 开启录制模式，编辑器中点到对应的帧数，在 `SpriteRenderer` 中添加 `Sprite` 即可自动添加关键帧
+
+![alt text](https://mdn.alipayobjects.com/huamei_3zduhr/afts/img/A*Eff6TbgYps8AAAAAAAAAAAAADsJ_AQ/original)
+
+
+### 脚本实现
+
+引擎在 1.1 版本支持引用类型的动画曲线（[AnimationRefCurve](/apis/core/#AnimationRefCurve)），关键帧的值可以是资产如（精灵，材质），你可以通过创建引用类型的动画曲线实现比如帧动画的能力：
+
+<playground src="animation-sprite.ts"></playground>
--- examples/obj-loader.ts
+++ examples/obj-loader.ts
@@ -3,50 +3,63 @@
  * @category Mesh
  * @thumbnail https://mdn.alipayobjects.com/merchant_appfe/afts/img/A*ZnBlT4UNh0IAAAAAAAAAAAAADiR2AQ/original
  */
+import { OrbitControl } from "@galacean/engine-toolkit-controls";
 import {
   AssetPromise,
   BlinnPhongMaterial,
   Camera,
   Color,
   DirectLight,
-  LoadItem,
+  Entity,
   Loader,
+  LoadItem,
   MeshRenderer,
   MeshTopology,
   ModelMesh,
+  resourceLoader,
   ResourceManager,
   Vector3,
   WebGLEngine,
-  request,
-  resourceLoader
 } from "@galacean/engine";
-import { OrbitControl } from "@galacean/engine-toolkit-controls";
 
 @resourceLoader("OBJ", ["obj"])
 class OBJLoader extends Loader<ModelMesh> {
-  load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<ModelMesh> {
-    return request<string>(item.url!, { ...item, type: "text" }).then((text: string) => {
-      const lines = text.split(/\n/);
-      const positions: Vector3[] = [];
-      const indices: number[] = [];
-      lines
-        .map((lineText) => lineText.split(" "))
-        .forEach((parseTexts) => {
-          if (parseTexts[0] === "v") {
-            positions.push(
-              new Vector3(parseFloat(parseTexts[1]), parseFloat(parseTexts[2]), parseFloat(parseTexts[3]))
-            );
-          } else if (parseTexts[0] === "f") {
-            indices.push(parseInt(parseTexts[1]) - 1, parseInt(parseTexts[2]) - 1, parseInt(parseTexts[3]) - 1);
-          }
-        });
-      const mesh = new ModelMesh(resourceManager.engine);
-      mesh.setPositions(positions);
-      mesh.setIndices(Uint16Array.from(indices));
-      mesh.addSubMesh(0, indices.length, MeshTopology.Triangles);
-      mesh.uploadData(false);
-      return mesh;
-    });
+  load(
+    item: LoadItem,
+    resourceManager: ResourceManager
+  ): AssetPromise<ModelMesh> {
+    return this.request<string>(item.url!, { ...item, type: "text" }).then(
+      (text: string) => {
+        const lines = text.split(/\n/);
+        const positions: Vector3[] = [];
+        const indices: number[] = [];
+        lines
+          .map((lineText) => lineText.split(" "))
+          .forEach((parseTexts) => {
+            if (parseTexts[0] === "v") {
+              positions.push(
+                new Vector3(
+                  parseFloat(parseTexts[1]),
+                  parseFloat(parseTexts[2]),
+                  parseFloat(parseTexts[3])
+                )
+              );
+            } else if (parseTexts[0] === "f") {
+              indices.push(
+                parseInt(parseTexts[1]) - 1,
+                parseInt(parseTexts[2]) - 1,
+                parseInt(parseTexts[3]) - 1
+              );
+            }
+          });
+        const mesh = new ModelMesh(resourceManager.engine);
+        mesh.setPositions(positions);
+        mesh.setIndices(Uint16Array.from(indices));
+        mesh.addSubMesh(0, indices.length, MeshTopology.Triangles);
+        mesh.uploadData(false);
+        return mesh;
+      }
+    );
   }
 }
 
@@ -66,7 +79,9 @@ WebGLEngine.create({ canvas: "canvas" }).then((engine) => {
   rootEntity.addComponent(DirectLight);
 
   engine.resourceManager
-    .load<ModelMesh>("https://gw.alipayobjects.com/os/bmw-prod/b885a803-5315-44f0-af54-6787ec47ed1b.obj")
+    .load<ModelMesh>(
+      "https://gw.alipayobjects.com/os/bmw-prod/b885a803-5315-44f0-af54-6787ec47ed1b.obj"
+    )
     .then((mesh) => {
       renderer.mesh = mesh;
     });
