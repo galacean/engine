02f961a6899d381448aa99be712e678ebc5e9f5c
luzhuang <364439895@qq.com>
2025-01-16 23:56:32 +0800

    feat: add ut

--- tests/src/core/physics/Joint.test.ts
+++ tests/src/core/physics/Joint.test.ts
@@ -95,6 +95,9 @@ describe("Joint", function () {
     joint.automaticConnectedAnchor = true;
     joint.connectedAnchor = new Vector3(1, 1, 1);
     expect(consoleWarnSpy).toBeCalledTimes(1);
+    joint.connectedAnchor.y = 3;
+    expect(consoleWarnSpy).toBeCalledTimes(2);
+    expect(joint.connectedAnchor).deep.include({ x: 4, y: 4, z: 4 });
 
     // @ts-ignore
     engine.sceneManager.activeScene.physics._update(1 / 60);
