917c2ca0a32c1e8281337946c08a41f155b750d7
zhuxudong <callzhuxudong@163.com>
2025-04-23 11:16:09 +0800

    fix: bloom ignore alpha

--- packages/core/src/postProcess/shaders/Bloom/BloomPrefilter.glsl
+++ packages/core/src/postProcess/shaders/Bloom/BloomPrefilter.glsl
@@ -51,7 +51,8 @@ void main(){
     // Clamp colors to positive once in prefilter. Encode can have a sqrt, and sqrt(-x) == NaN. Up/Downsample passes would then spread the NaN.
     color = max(color, 0.0);
 
-    gl_FragColor = vec4(color, samplerColor.a);
+    // Ignore alpha to blend with the transparent background
+    gl_FragColor = vec4(color, 0.0);
     
     #ifndef ENGINE_IS_COLORSPACE_GAMMA
       gl_FragColor = linearToGamma(gl_FragColor);
