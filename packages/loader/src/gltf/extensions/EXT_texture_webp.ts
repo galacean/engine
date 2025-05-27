9ce9158a51c070a40fa3c426684806dc85bfda0c
init <ning.su@vivo.com>
2025-05-27 17:56:15 +0800

    检查方法合并至SystemInfo

--- packages/loader/src/gltf/extensions/EXT_texture_webp.ts
+++ packages/loader/src/gltf/extensions/EXT_texture_webp.ts
@@ -9,31 +9,6 @@ interface EXTWebPSchema {
   source: number;
 }
 
-let webpSupportPromise: AssetPromise<boolean> | null = null;
-function checkWebpSupport(): AssetPromise<boolean> {
-  if (webpSupportPromise) {
-    return webpSupportPromise;
-  }
-  webpSupportPromise = new AssetPromise((resolve) => {
-    // @ts-ignore
-    if (SystemInfo._isBrowser) {
-      const img = new Image();
-      img.onload = function () {
-        const result = img.width > 0 && img.height > 0;
-        resolve(result);
-      };
-      img.onerror = function () {
-        resolve(false);
-      };
-      img.src =
-        "data:image/webp;base64,UklGRhACAABXRUJQVlA4WAoAAAAwAAAAAAAAAAAASUNDUMgBAAAAAAHIAAAAAAQwAABtbnRyUkdCIFhZWiAH4AABAAEAAAAAAABhY3NwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAA9tYAAQAAAADTLQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAlkZXNjAAAA8AAAACRyWFlaAAABFAAAABRnWFlaAAABKAAAABRiWFlaAAABPAAAABR3dHB0AAABUAAAABRyVFJDAAABZAAAAChnVFJDAAABZAAAAChiVFJDAAABZAAAAChjcHJ0AAABjAAAADxtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAAgAAAAcAHMAUgBHAEJYWVogAAAAAAAAb6IAADj1AAADkFhZWiAAAAAAAABimQAAt4UAABjaWFlaIAAAAAAAACSgAAAPhAAAts9YWVogAAAAAAAA9tYAAQAAAADTLXBhcmEAAAAAAAQAAAACZmYAAPKnAAANWQAAE9AAAApbAAAAAAAAAABtbHVjAAAAAAAAAAEAAAAMZW5VUwAAACAAAAAcAEcAbwBvAGcAbABlACAASQBuAGMALgAgADIAMAAxADZBTFBIAgAAAAAAVlA4IBgAAAAwAQCdASoBAAEAAUAmJaQAA3AA/v02aAA=";
-    } else {
-      resolve(false);
-    }
-  });
-  return webpSupportPromise;
-}
-
 @registerGLTFExtension("EXT_texture_webp", GLTFExtensionMode.CreateAndParse)
 class EXT_texture_webp extends GLTFExtensionParser {
   override createAndParse(
@@ -45,7 +20,7 @@ class EXT_texture_webp extends GLTFExtensionParser {
   ): AssetPromise<Texture2D> {
     const webPIndex = schema.source;
     const { sampler, source: fallbackIndex = 0, name: textureName } = textureInfo;
-    return checkWebpSupport().then((supportWebP) => {
+    return SystemInfo._checkWebpSupported().then((supportWebP) => {
       return GLTFTextureParser._parseTexture(
         context,
         supportWebP ? webPIndex : fallbackIndex,
