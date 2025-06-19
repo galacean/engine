0096498c71be76826a71ec039114bfd99886acfb
zhuxudong <callzhuxudong@163.com>
2025-06-19 14:25:16 +0800

    chore: revert fn code

--- packages/shader-lab/src/parser/symbolTable/FnSymbol.ts
+++ packages/shader-lab/src/parser/symbolTable/FnSymbol.ts
@@ -1,4 +1,3 @@
-import { GalaceanDataType } from "../../common";
 import { ASTNode } from "../AST";
 import { SymbolDataType } from "./SymbolDataType";
 import { ESymbolType, SymbolInfo } from "./SymbolInfo";
@@ -6,11 +5,11 @@ import { ESymbolType, SymbolInfo } from "./SymbolInfo";
 export class FnSymbol extends SymbolInfo {
   declare astNode: ASTNode.FunctionDefinition;
 
-  constructor(lexeme: string, astNode: ASTNode.FunctionDefinition, paramSignature?: GalaceanDataType[]) {
+  constructor(lexeme: string, astNode: ASTNode.FunctionDefinition) {
     const type = new SymbolDataType(
       astNode.protoType.returnType.type,
       astNode.protoType.returnType.typeSpecifier.lexeme
     );
-    super(lexeme, ESymbolType.FN, astNode, type, paramSignature);
+    super(lexeme, ESymbolType.FN, astNode, type);
   }
 }
