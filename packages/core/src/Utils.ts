df48d3269ce083c674a5181697aa32d2ee4ef13a
changhuiping.chp <changhuiping.chp@digital-engine.com>
2025-11-24 10:33:21 +0800

    fix: 还原非必要更改

--- packages/core/src/Utils.ts
+++ packages/core/src/Utils.ts
@@ -122,15 +122,15 @@ export class Utils {
 
     // prettier-ignore
     const l11 = le[0], l12 = le[1], l13 = le[2], l14 = le[3],
-      l21 = le[4], l22 = le[5], l23 = le[6], l24 = le[7],
-      l31 = le[8], l32 = le[9], l33 = le[10], l34 = le[11],
-      l41 = le[12], l42 = le[13], l43 = le[14], l44 = le[15];
+    l21 = le[4], l22 = le[5], l23 = le[6], l24 = le[7],
+    l31 = le[8], l32 = le[9], l33 = le[10], l34 = le[11],
+    l41 = le[12], l42 = le[13], l43 = le[14], l44 = le[15];
 
     // prettier-ignore
     const r11 = re[rOffset], r12 = re[rOffset + 1], r13 = re[rOffset + 2], r14 = re[rOffset + 3],
-      r21 = re[rOffset + 4], r22 = re[rOffset + 5], r23 = re[rOffset + 6], r24 = re[rOffset + 7],
-      r31 = re[rOffset + 8], r32 = re[rOffset + 9], r33 = re[rOffset + 10], r34 = re[rOffset + 11],
-      r41 = re[rOffset + 12], r42 = re[rOffset + 13], r43 = re[rOffset + 14], r44 = re[rOffset + 15];
+    r21 = re[rOffset + 4], r22 = re[rOffset + 5], r23 = re[rOffset + 6], r24 = re[rOffset + 7],
+    r31 = re[rOffset + 8], r32 = re[rOffset + 9], r33 = re[rOffset + 10], r34 = re[rOffset + 11],
+    r41 = re[rOffset + 12], r42 = re[rOffset + 13], r43 = re[rOffset + 14], r44 = re[rOffset + 15];
 
     oe[offset] = l11 * r11 + l21 * r12 + l31 * r13 + l41 * r14;
     oe[offset + 1] = l12 * r11 + l22 * r12 + l32 * r13 + l42 * r14;
@@ -300,17 +300,17 @@ const reEscapeChar = /\\(\\)?/g;
 const rePropName = RegExp(
   // Match anything that isn't a dot or bracket.
   "[^.[\\]]+" +
-  "|" +
-  // Or match property names within brackets.
-  "\\[(?:" +
-  // Match a non-string expression.
-  "([^\"'][^[]*)" +
-  "|" +
-  // Or match strings (supports escaping characters).
-  "([\"'])((?:(?!\\2)[^\\\\]|\\\\.)*?)\\2" +
-  ")\\]" +
-  "|" +
-  // Or match "" as the space between consecutive dots or empty brackets.
-  "(?=(?:\\.|\\[\\])(?:\\.|\\[\\]|$))",
+    "|" +
+    // Or match property names within brackets.
+    "\\[(?:" +
+    // Match a non-string expression.
+    "([^\"'][^[]*)" +
+    "|" +
+    // Or match strings (supports escaping characters).
+    "([\"'])((?:(?!\\2)[^\\\\]|\\\\.)*?)\\2" +
+    ")\\]" +
+    "|" +
+    // Or match "" as the space between consecutive dots or empty brackets.
+    "(?=(?:\\.|\\[\\])(?:\\.|\\[\\]|$))",
   "g"
 );
