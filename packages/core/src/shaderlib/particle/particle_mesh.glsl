bf3890d140a63bec90d577a89ef62268ebe9b5de
GuoLei1990 <gl3336563@163.com>
2025-12-10 22:28:06 +0800

    refactor: opt code

--- packages/core/src/shaderlib/particle/particle_mesh.glsl
+++ packages/core/src/shaderlib/particle/particle_mesh.glsl
@@ -9,40 +9,40 @@
             renderer_SizeScale * rotationByEuler(POSITION * size, rotation),
             worldRotation);
         } else {
-        #ifdef RENDERER_ROL_IS_SEPARATE
-            float angle = computeParticleRotationFloat(a_StartRotation0.x, age, normalizedAge);
-            if (a_ShapePositionStartLifeTime.x != 0.0 || a_ShapePositionStartLifeTime.y != 0.0) {
-            center += (rotationByQuaternions(
-                rotationByAxis(
-                renderer_SizeScale * POSITION * size,
-                normalize(cross(vec3(0.0, 0.0, 1.0),
-                    vec3(a_ShapePositionStartLifeTime.xy, 0.0))),
-                angle),
-                worldRotation)); //已验证
-            } else {
-            #ifdef SHAPE
-                center += renderer_SizeScale.xzy * (rotationByQuaternions(rotationByAxis(POSITION * size, vec3(0.0, -1.0, 0.0), angle), worldRotation));
-            #else
-                if (renderer_SimulationSpace == 1)
-                    center += rotationByAxis(renderer_SizeScale * POSITION * size,
-                    vec3(0.0, 0.0, -1.0),
-                    angle); //已验证
-                else if (renderer_SimulationSpace == 0)
-                    center += rotationByQuaternions(
-                    renderer_SizeScale * rotationByAxis(POSITION * size, vec3(0.0, 0.0, -1.0), angle),
-                    worldRotation); //已验证
+            #ifdef RENDERER_ROL_IS_SEPARATE
+                float angle = computeParticleRotationFloat(a_StartRotation0.x, age, normalizedAge);
+                if (a_ShapePositionStartLifeTime.x != 0.0 || a_ShapePositionStartLifeTime.y != 0.0) {
+                center += (rotationByQuaternions(
+                    rotationByAxis(
+                    renderer_SizeScale * POSITION * size,
+                    normalize(cross(vec3(0.0, 0.0, 1.0),
+                        vec3(a_ShapePositionStartLifeTime.xy, 0.0))),
+                    angle),
+                    worldRotation)); //已验证
+                } else {
+                #ifdef SHAPE
+                    center += renderer_SizeScale.xzy * (rotationByQuaternions(rotationByAxis(POSITION * size, vec3(0.0, -1.0, 0.0), angle), worldRotation));
+                #else
+                    if (renderer_SimulationSpace == 1)
+                        center += rotationByAxis(renderer_SizeScale * POSITION * size,
+                        vec3(0.0, 0.0, -1.0),
+                        angle); //已验证
+                    else if (renderer_SimulationSpace == 0)
+                        center += rotationByQuaternions(
+                        renderer_SizeScale * rotationByAxis(POSITION * size, vec3(0.0, 0.0, -1.0), angle),
+                        worldRotation); //已验证
+                #endif
+                }
             #endif
-            }
-        #endif
-        #ifdef ROTATION_OVER_LIFETIME_SEPARATE
-            // TODO:是否应合并if(renderer_ThreeDStartRotation)分支代码,待测试
-            vec3 angle = computeParticleRotationVec3(
-            vec3(0.0, 0.0, -a_StartRotation0.x), age, normalizedAge);
-            center += (rotationByQuaternions(
-            rotationByEuler(renderer_SizeScale * POSITION * size,
-                vec3(angle.x, angle.y, angle.z)),
-            worldRotation)); //已验证
-        #endif
+            #ifdef ROTATION_OVER_LIFETIME_SEPARATE
+                // TODO:是否应合并if(renderer_ThreeDStartRotation)分支代码,待测试
+                vec3 angle = computeParticleRotationVec3(
+                vec3(0.0, 0.0, -a_StartRotation0.x), age, normalizedAge);
+                center += (rotationByQuaternions(
+                rotationByEuler(renderer_SizeScale * POSITION * size,
+                    vec3(angle.x, angle.y, angle.z)),
+                worldRotation)); //已验证
+            #endif
         }
     #else
         if (renderer_ThreeDStartRotation) {
@@ -51,36 +51,36 @@
             worldRotation); //已验证
         } else {
             if (a_ShapePositionStartLifeTime.x != 0.0 || a_ShapePositionStartLifeTime.y != 0.0) {
-            if (renderer_SimulationSpace == 1)
-                center += rotationByAxis(
-                renderer_SizeScale * POSITION * size,
-                normalize(cross(vec3(0.0, 0.0, 1.0),
+                if (renderer_SimulationSpace == 1)
+                    center += rotationByAxis(
+                    renderer_SizeScale * POSITION * size,
+                    normalize(cross(vec3(0.0, 0.0, 1.0),
                     vec3(a_ShapePositionStartLifeTime.xy, 0.0))),
-                a_StartRotation0.x);
-            else if (renderer_SimulationSpace == 0)
-                center += (rotationByQuaternions(
-                renderer_SizeScale * rotationByAxis(POSITION * size, normalize(cross(vec3(0.0, 0.0, 1.0),
-                                             vec3(a_ShapePositionStartLifeTime.xy, 0.0))), a_StartRotation0.x),
-                worldRotation)); //已验证
+                    a_StartRotation0.x);
+                else if (renderer_SimulationSpace == 0)
+                    center += (rotationByQuaternions(
+                    renderer_SizeScale * rotationByAxis(POSITION * size, normalize(cross(vec3(0.0, 0.0, 1.0),
+                                                vec3(a_ShapePositionStartLifeTime.xy, 0.0))), a_StartRotation0.x),
+                    worldRotation)); //已验证
             } else {
-        #ifdef SHAPE
-            if (renderer_SimulationSpace == 1)
-                center += renderer_SizeScale * rotationByAxis(POSITION * size, vec3(0.0, -1.0, 0.0), a_StartRotation0.x);
-            else if (renderer_SimulationSpace == 0)
-                center += rotationByQuaternions(
-                renderer_SizeScale * rotationByAxis(POSITION * size, vec3(0.0, -1.0, 0.0), a_StartRotation0.x),
-                worldRotation);
-        #else
-            if (renderer_SimulationSpace == 1)
-                center += rotationByAxis(renderer_SizeScale * POSITION * size,
-                vec3(0.0, 0.0, -1.0),
-                a_StartRotation0.x);
-            else if (renderer_SimulationSpace == 0)
-                center += rotationByQuaternions(
-                renderer_SizeScale * rotationByAxis(POSITION * size, vec3(0.0, 0.0, -1.0), a_StartRotation0.x),
-                worldRotation); //已验证
-        #endif
-            }
+                #ifdef SHAPE
+                    if (renderer_SimulationSpace == 1)
+                        center += renderer_SizeScale * rotationByAxis(POSITION * size, vec3(0.0, -1.0, 0.0), a_StartRotation0.x);
+                    else if (renderer_SimulationSpace == 0)
+                        center += rotationByQuaternions(
+                        renderer_SizeScale * rotationByAxis(POSITION * size, vec3(0.0, -1.0, 0.0), a_StartRotation0.x),
+                        worldRotation);
+                #else
+                    if (renderer_SimulationSpace == 1)
+                        center += rotationByAxis(renderer_SizeScale * POSITION * size,
+                        vec3(0.0, 0.0, -1.0),
+                        a_StartRotation0.x);
+                    else if (renderer_SimulationSpace == 0)
+                        center += rotationByQuaternions(
+                        renderer_SizeScale * rotationByAxis(POSITION * size, vec3(0.0, 0.0, -1.0), a_StartRotation0.x),
+                        worldRotation); //已验证
+                #endif
+                }
         }
     #endif
     #ifdef RENDERER_ENABLE_VERTEXCOLOR
