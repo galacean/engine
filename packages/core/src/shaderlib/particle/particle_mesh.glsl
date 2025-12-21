37ff9e9ba9e681f9cec7ee3d1853d2948d45de58
GuoLei1990 <gl3336563@163.com>
2025-12-21 21:28:44 +0800

    fix: 优化粒子旋转逻辑以支持3D旋转和轴向旋转

--- packages/core/src/shaderlib/particle/particle_mesh.glsl
+++ packages/core/src/shaderlib/particle/particle_mesh.glsl
@@ -2,62 +2,48 @@
 
 // Only support local alignment mode
 #ifdef RENDERER_MODE_MESH
-	vec3 size = computeParticleSizeMesh(a_StartSize, normalizedAge);
     #if defined(RENDERER_ROL_CONSTANT_MODE) || defined(RENDERER_ROL_CURVE_MODE)
-        bool is3DRotation = renderer_ThreeDStartRotation;
-        #ifdef RENDERER_ROL_IS_SEPARATE
-            is3DRotation = true;
-        #endif
-
-        if(is3DRotation){
-            vec3 rotation = vec3(0.0);
-            if (renderer_ThreeDStartRotation) {
-                rotation = computeParticleRotationVec3(a_StartRotation0, age, normalizedAge);
-            }
-            else{
-                rotation = computeParticleRotationVec3(vec3(0.0, 0.0, a_StartRotation0.x), age, normalizedAge);
-            }
-            center += rotationByQuaternions(renderer_SizeScale * rotationByEuler(POSITION * size, rotation),worldRotation);
-        }
-        else{
-            float angle = computeParticleRotationFloat(a_StartRotation0.x, age, normalizedAge);
-            #ifdef RENDERER_EMISSION_SHAPE
-                vec3 axis = vec3(a_ShapePositionStartLifeTime.xy, 0.0);
-                if (renderer_SimulationSpace == 1){
-                    axis = rotationByQuaternions(axis, worldRotation);
-                }
-                vec3 crossResult = cross(axis, vec3(0.0, 0.0, -1.0));
-                float crossLen = length(crossResult);
-                vec3 rotateAxis = crossLen > 0.0001 ? crossResult / crossLen : vec3(0.0, -1.0, 0.0);
-                center += rotationByQuaternions(renderer_SizeScale * rotationByAxis(POSITION * size, rotateAxis, angle), worldRotation);
-            #else
-                // Axis is negative z
-                vec3 axis = vec3(0.0, 0.0, -1.0);
-                center += rotationByQuaternions(renderer_SizeScale *rotationByAxis(POSITION * size, axis, angle), worldRotation);
-            #endif
-        }
-    #else
-        if (renderer_ThreeDStartRotation) {
-            // 3D Start Rotation is same in local and world simulation space
-            center += rotationByQuaternions(renderer_SizeScale * rotationByEuler(POSITION * size, a_StartRotation0), worldRotation);
-        } else {
-            #ifdef RENDERER_EMISSION_SHAPE
-                // Axis is side vector of emit position look at zero
-                vec3 axis = vec3(a_ShapePositionStartLifeTime.xy, 0.0);
-                if (renderer_SimulationSpace == 1){
-                    axis = rotationByQuaternions(axis, worldRotation);
-                }
-                vec3 crossResult = cross(axis, vec3(0.0, 0.0, -1.0));
-                float crossLen = length(crossResult);
-                vec3 rotateAxis = crossLen > 0.0001 ? crossResult / crossLen : vec3(0.0, -1.0, 0.0);
-                center += rotationByQuaternions(renderer_SizeScale * rotationByAxis(POSITION * size, rotateAxis, a_StartRotation0.x), worldRotation);
-            #else
-                // Axis is negative z
-                vec3 axis = vec3(0.0, 0.0, -1.0);
-                center += rotationByQuaternions(renderer_SizeScale *rotationByAxis(POSITION * size, axis, a_StartRotation0.x), worldRotation);
-            #endif
-        }
+        #define RENDERER_ROL_ENABLED
     #endif
+
+	vec3 size = computeParticleSizeMesh(a_StartSize, normalizedAge);
+
+    bool is3DRotation = renderer_ThreeDStartRotation;
+    #if defined(RENDERER_ROL_ENABLED) && defined(RENDERER_ROL_IS_SEPARATE)
+        is3DRotation = true;
+    #endif
+
+    if (is3DRotation) {
+        #ifdef RENDERER_ROL_ENABLED
+            vec3 startRotation = renderer_ThreeDStartRotation ? a_StartRotation0 : vec3(0.0, 0.0, a_StartRotation0.x);
+            vec3 rotation = computeParticleRotationVec3(startRotation, age, normalizedAge);
+        #else
+            vec3 rotation = a_StartRotation0;
+        #endif
+        // 3D Start Rotation is same in local and world simulation space
+        center += rotationByQuaternions(renderer_SizeScale * rotationByEuler(POSITION * size, rotation), worldRotation);
+    } else {
+        #ifdef RENDERER_ROL_ENABLED
+            float angle = computeParticleRotationFloat(a_StartRotation0.x, age, normalizedAge);
+        #else
+            float angle = a_StartRotation0.x;
+        #endif
+        #ifdef RENDERER_EMISSION_SHAPE
+            // Axis is side vector of emit position look at zero
+            vec3 axis = vec3(a_ShapePositionStartLifeTime.xy, 0.0);
+            if (renderer_SimulationSpace == 1){
+                axis = rotationByQuaternions(axis, worldRotation);
+            }
+            vec3 crossResult = cross(axis, vec3(0.0, 0.0, -1.0));
+            float crossLen = length(crossResult);
+            vec3 rotateAxis = crossLen > 0.0001 ? crossResult / crossLen : vec3(0.0, -1.0, 0.0);
+            center += rotationByQuaternions(renderer_SizeScale * rotationByAxis(POSITION * size, rotateAxis, angle), worldRotation);
+        #else
+            // Axis is negative z
+            vec3 axis = vec3(0.0, 0.0, -1.0);
+            center += rotationByQuaternions(renderer_SizeScale *rotationByAxis(POSITION * size, axis, angle), worldRotation);
+        #endif
+    }
     #ifdef RENDERER_ENABLE_VERTEXCOLOR
 		v_MeshColor = COLOR_0;
 	#endif
