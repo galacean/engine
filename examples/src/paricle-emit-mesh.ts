1a592de9cfad0c9ad9bbcf7c4d662b2f6699e96c
GuoLei1990 <gl3336563@163.com>
2025-12-21 21:57:03 +0800

    fix: 重构粒子发射器以支持网格渲染和改进相机设置

--- examples/src/paricle-emit-mesh.ts
+++ examples/src/paricle-emit-mesh.ts
@@ -5,117 +5,99 @@
  */
 import {
   AssetType,
-  BlendMode,
   Camera,
   Color,
   ConeShape,
-  CurveKey,
   Engine,
   Entity,
   GLTFResource,
   Logger,
-  ParticleCurve,
+  MeshRenderer,
   ParticleCurveMode,
-  ParticleGradientMode,
   ParticleMaterial,
   ParticleRenderer,
   ParticleRenderMode,
   ParticleSimulationSpace,
   PrimitiveMesh,
   Texture2D,
-  Vector2,
+  UnlitMaterial,
   Vector3,
   WebGLEngine
 } from "@galacean/engine";
 
+import { OrbitControl } from "@galacean/engine-toolkit";
+
 Logger.enable();
 main();
 
-function createFireParticle(engine: Engine, texture: Texture2D, glTFModel: GLTFResource): Entity {
-  const particleEntity = new Entity(engine, "Fire");
-  particleEntity.transform.scale.set(1.268892, 1.268892, 1.268892);
-  particleEntity.transform.rotate(90, 0, 0);
-  // particleEntity.transform.position.set(-10, 0, 0);
+function createFireParticle(rootEntity: Entity, engine: Engine, texture: Texture2D, glTFModel: GLTFResource): Entity {
+  const meshRenderer = rootEntity.createChild("Mesh").addComponent(MeshRenderer);
+  meshRenderer.mesh = PrimitiveMesh.createCylinder(engine);
+  const unlitMaterial = new UnlitMaterial(engine);
+  unlitMaterial.baseTexture = glTFModel.textures[0];
+  meshRenderer.setMaterial(unlitMaterial);
+
+  const particleEntity = new Entity(engine, "EmitMeshParticle");
+  particleEntity.transform.rotation.set(90, 0, 0);
+  particleEntity.transform.scale.set(6, 4, 12);
 
   const particleRenderer = particleEntity.addComponent(ParticleRenderer);
 
   const material = new ParticleMaterial(engine);
   material.baseColor = new Color(1.0, 1.0, 1.0, 1.0);
-  material.blendMode = BlendMode.Additive;
-  // material.baseTexture = texture;
-   material.baseTexture = glTFModel.textures[0];
+  // material.blendMode = BlendMode.Additive;
+  material.baseTexture = glTFModel.textures[0];
+  material.baseTexture.anisoLevel = 12;
   particleRenderer.setMaterial(material);
-  particleRenderer.priority = 2;
 
-  // debugger;
   particleRenderer.renderMode = ParticleRenderMode.Mesh;
-  particleRenderer.mesh = glTFModel.meshes[0][0];
-  // particleRenderer.mesh = PrimitiveMesh.createCuboid(engine, 0.5);
-  // debugger;
+  // particleRenderer.mesh = glTFModel.meshes[0][0];
+  particleRenderer.mesh = PrimitiveMesh.createCylinder(engine);
 
   const generator = particleRenderer.generator;
-  const { main, emission, textureSheetAnimation, sizeOverLifetime, colorOverLifetime } = generator;
-
   generator.useAutoRandomSeed = false;
+  const { main, emission, rotationOverLifetime, textureSheetAnimation, sizeOverLifetime, colorOverLifetime } = generator;
 
   // Main module
   const { startLifetime, startSpeed, startSize, startRotationZ } = main;
-  startLifetime.constantMin = 0.2;
-  startLifetime.constantMax = 0.8;
-  startLifetime.mode = ParticleCurveMode.TwoConstants;
+  startSpeed.constant = 1.0;
+  startSize.constant = 0.2;
 
-  startSpeed.constantMin = 0.4;
-  startSpeed.constantMax = 1.6;
-  startSpeed.mode = ParticleCurveMode.TwoConstants;
+  main.simulationSpace = ParticleSimulationSpace.Local;
+  main.startRotation3D = true;
+   main.startRotationX.constant = -40;
+  main.startRotationZ.constant = 0;
+ 
+  // main.startRotationY.constant = 40;
+  // main.startRotationZ.constant = -70;
 
-  startSize.constantMin = 0.0005;
-  startSize.constantMax = 0.0008;
-  startSize.mode = ParticleCurveMode.TwoConstants;
 
-  startRotationZ.constantMin = 0;
-  startRotationZ.constantMax = 360;
-  startRotationZ.mode = ParticleCurveMode.TwoConstants;
+  rotationOverLifetime.enabled = true;
+  rotationOverLifetime.separateAxes = true;
+  rotationOverLifetime.rotationX.constant = 100;
+  rotationOverLifetime.rotationY.constant = 0;
+  rotationOverLifetime.rotationZ.constant = 0;
 
-  main.simulationSpace = ParticleSimulationSpace.World;
+  // startLifetime.constantMin = 0.2;
+  // startLifetime.constantMax = 0.8;
+  // startLifetime.mode = ParticleCurveMode.TwoConstants;
 
-  // Emission module
-  emission.rateOverTime.constant = 35;
+  // startSpeed.constantMin = 0.4;
+  // startSpeed.constantMax = 1.6;
+  // startSpeed.mode = ParticleCurveMode.TwoConstants;
+
+  // startSize.constant = 0.01;
+  // startSize.constantMax = 0.008;
+  // startSize.mode = ParticleCurveMode.TwoConstants;
+
+  // const coneShape = new ConeShape();
+  // coneShape.angle = 0.01;
+  // emission.shape = coneShape;
+
 
   const coneShape = new ConeShape();
-  coneShape.angle = .96;
-  coneShape.radius = 0.1;
+  coneShape.angle = 0.01;
   emission.shape = coneShape;
-
-  // Color over lifetime module
-  colorOverLifetime.enabled = true;
-  colorOverLifetime.color.mode = ParticleGradientMode.Gradient;
-
-  const gradient = colorOverLifetime.color.gradient;
-  const colorKeys = gradient.colorKeys;
-  colorKeys[0].color.set(1.0, 0.21223075741405523, 0.00121410793419535, 1.0);
-  colorKeys[1].time = 0.998;
-  colorKeys[1].color.set(1.0, 0.19806931955994886, 0.0, 1.0);
-  gradient.addColorKey(0.157, new Color(1, 1, 1, 1));
-  gradient.addColorKey(0.573, new Color(1.0, 1.0, 0.25015828472995344, 1));
-  gradient.alphaKeys[1].time = 0.089;
-
-  // Size over lifetime module
-  sizeOverLifetime.enabled = true;
-  sizeOverLifetime.size.mode = ParticleCurveMode.Curve;
-
-  const curve = sizeOverLifetime.size.curve;
-  const keys = curve.keys;
-  keys[0].value = 0.153;
-  keys[1].value = 0.529;
-  curve.addKey(0.074, 0.428 + 0.2);
-  curve.addKey(0.718, 0.957 + 0.03);
-
-  // Texture sheet animation module
-  // textureSheetAnimation.enabled = true;
-  // textureSheetAnimation.tiling = new Vector2(6, 6);
-  // const frameOverTime = textureSheetAnimation.frameOverTime;
-  // frameOverTime.mode = ParticleCurveMode.TwoCurves;
-  // frameOverTime.curveMin = new ParticleCurve(new CurveKey(0, 0.47), new CurveKey(1, 1));
 
   return particleEntity;
 }
@@ -129,8 +111,12 @@ async function main() {
 
   // Create camera
   const cameraEntity = rootEntity.createChild("Camera");
-  cameraEntity.transform.position = new Vector3(0, 1, 3); // -10 can test bounds transform
-  cameraEntity.addComponent(Camera);
+  cameraEntity.transform.position = new Vector3(0, 1, 10);
+  const camera = cameraEntity.addComponent(Camera);
+  camera.fieldOfView = 60;
+  camera.nearClipPlane = 0.3;
+  camera.farClipPlane = 1000;
+  // cameraEntity.addComponent(OrbitControl);
 
   engine.resourceManager
     .load([
@@ -144,8 +130,13 @@ async function main() {
       }
     ])
     .then((resoueces) => {
-      const fireEntity = createFireParticle(engine, <Texture2D>resoueces[0], <GLTFResource>resoueces[1]);
-      rootEntity.addChild(fireEntity);
+      const particleEntity = createFireParticle(
+        rootEntity,
+        engine,
+        <Texture2D>resoueces[0],
+        <GLTFResource>resoueces[1]
+      );
+      rootEntity.addChild(particleEntity);
     });
 
   // Run engine
