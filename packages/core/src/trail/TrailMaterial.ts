9be80c4127ec374de51d64c0b172496dbf09649d
GuoLei1990 <gl3336563@163.com>
2025-12-28 01:20:10 +0800

    feat: refactor ParticleMaterial and TrailMaterial to extend EffectMaterial

--- packages/core/src/trail/TrailMaterial.ts
+++ packages/core/src/trail/TrailMaterial.ts
@@ -1,85 +1,17 @@
-import { Color } from "@galacean/engine-math";
 import { Engine } from "../Engine";
-import { BaseMaterial } from "../material/BaseMaterial";
+import { EffectMaterial } from "../material/EffectMaterial";
 import { Shader } from "../shader";
-import { Texture2D } from "../texture";
 
 /**
  * Trail material.
  */
-export class TrailMaterial extends BaseMaterial {
-  /**
-   * Base color.
-   */
-  get baseColor(): Color {
-    return this.shaderData.getColor(BaseMaterial._baseColorProp);
-  }
-
-  set baseColor(value: Color) {
-    const baseColor = this.shaderData.getColor(BaseMaterial._baseColorProp);
-    if (value !== baseColor) {
-      baseColor.copyFrom(value);
-    }
-  }
-
-  /**
-   * Base texture.
-   */
-  get baseTexture(): Texture2D {
-    return <Texture2D>this.shaderData.getTexture(BaseMaterial._baseTextureProp);
-  }
-
-  set baseTexture(value: Texture2D) {
-    this.shaderData.setTexture(BaseMaterial._baseTextureProp, value);
-    if (value) {
-      this.shaderData.enableMacro(BaseMaterial._baseTextureMacro);
-    } else {
-      this.shaderData.disableMacro(BaseMaterial._baseTextureMacro);
-    }
-  }
-
-  /**
-   * Emissive color.
-   */
-  get emissiveColor(): Color {
-    return this.shaderData.getColor(BaseMaterial._emissiveColorProp);
-  }
-
-  set emissiveColor(value: Color) {
-    const emissiveColor = this.shaderData.getColor(BaseMaterial._emissiveColorProp);
-    if (value !== emissiveColor) {
-      emissiveColor.copyFrom(value);
-    }
-  }
-
-  /**
-   * Emissive texture.
-   */
-  get emissiveTexture(): Texture2D {
-    return <Texture2D>this.shaderData.getTexture(BaseMaterial._emissiveTextureProp);
-  }
-
-  set emissiveTexture(value: Texture2D) {
-    this.shaderData.setTexture(BaseMaterial._emissiveTextureProp, value);
-    if (value) {
-      this.shaderData.enableMacro(BaseMaterial._emissiveTextureMacro);
-    } else {
-      this.shaderData.disableMacro(BaseMaterial._emissiveTextureMacro);
-    }
-  }
-
+export class TrailMaterial extends EffectMaterial {
   /**
    * Create a trail material instance.
    * @param engine - Engine to which the material belongs
    */
   constructor(engine: Engine) {
     super(engine, Shader.find("trail"));
-
-    const shaderData = this.shaderData;
-    shaderData.setColor(BaseMaterial._baseColorProp, new Color(1, 1, 1, 1));
-    shaderData.setColor(BaseMaterial._emissiveColorProp, new Color(0, 0, 0, 1));
-
-    this.isTransparent = true;
   }
 
   /**
