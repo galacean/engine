cdaec88bab64e8051bfbc54d29bb8e09c855b82b
GuoLei1990 <gl3336563@163.com>
2025-12-28 01:02:48 +0800

    feat: add trail shader support in ShaderPool

--- packages/core/src/trail/TrailMaterial.ts
+++ packages/core/src/trail/TrailMaterial.ts
@@ -1,41 +1,22 @@
 import { Color } from "@galacean/engine-math";
 import { Engine } from "../Engine";
-import { Material } from "../material/Material";
-import { BlendFactor, CullMode, Shader, ShaderPass, SubShader } from "../shader";
+import { BaseMaterial } from "../material/BaseMaterial";
+import { BlendFactor, CullMode, Shader } from "../shader";
 import { Texture2D } from "../texture";
-import { ShaderMacro } from "../shader/ShaderMacro";
-import { ShaderProperty } from "../shader/ShaderProperty";
-import FRAG_SHADER from "./trail.fs.glsl";
-import VERT_SHADER from "./trail.vs.glsl";
 
 /**
  * Trail material.
  */
-export class TrailMaterial extends Material {
-  private static _baseTextureMacro: ShaderMacro = ShaderMacro.getByName("MATERIAL_HAS_BASETEXTURE");
-  private static _baseColorProp: ShaderProperty = ShaderProperty.getByName("material_BaseColor");
-  private static _baseTextureProp: ShaderProperty = ShaderProperty.getByName("material_BaseTexture");
-
-  private static _isShaderCreated = false;
-
-  private static _createShader(): void {
-    if (TrailMaterial._isShaderCreated) return;
-
-    const shaderPass = new ShaderPass(VERT_SHADER, FRAG_SHADER);
-    const subShader = new SubShader("default", [shaderPass]);
-    Shader.create("trail", [subShader]);
-    TrailMaterial._isShaderCreated = true;
-  }
-
+export class TrailMaterial extends BaseMaterial {
   /**
    * Base color.
    */
   get baseColor(): Color {
-    return this.shaderData.getColor(TrailMaterial._baseColorProp);
+    return this.shaderData.getColor(BaseMaterial._baseColorProp);
   }
 
   set baseColor(value: Color) {
-    const baseColor = this.shaderData.getColor(TrailMaterial._baseColorProp);
+    const baseColor = this.shaderData.getColor(BaseMaterial._baseColorProp);
     if (value !== baseColor) {
       baseColor.copyFrom(value);
     }
@@ -45,15 +26,15 @@ export class TrailMaterial extends Material {
    * Base texture.
    */
   get baseTexture(): Texture2D {
-    return <Texture2D>this.shaderData.getTexture(TrailMaterial._baseTextureProp);
+    return <Texture2D>this.shaderData.getTexture(BaseMaterial._baseTextureProp);
   }
 
   set baseTexture(value: Texture2D) {
-    this.shaderData.setTexture(TrailMaterial._baseTextureProp, value);
+    this.shaderData.setTexture(BaseMaterial._baseTextureProp, value);
     if (value) {
-      this.shaderData.enableMacro(TrailMaterial._baseTextureMacro);
+      this.shaderData.enableMacro(BaseMaterial._baseTextureMacro);
     } else {
-      this.shaderData.disableMacro(TrailMaterial._baseTextureMacro);
+      this.shaderData.disableMacro(BaseMaterial._baseTextureMacro);
     }
   }
 
@@ -62,11 +43,10 @@ export class TrailMaterial extends Material {
    * @param engine - Engine to which the material belongs
    */
   constructor(engine: Engine) {
-    TrailMaterial._createShader();
     super(engine, Shader.find("trail"));
 
     const shaderData = this.shaderData;
-    shaderData.setColor(TrailMaterial._baseColorProp, new Color(1, 1, 1, 1));
+    shaderData.setColor(BaseMaterial._baseColorProp, new Color(1, 1, 1, 1));
 
     // Default blend state for additive blending
     const target = this.renderState.blendState.targetBlendState;
@@ -81,5 +61,14 @@ export class TrailMaterial extends Material {
 
     // Disable culling for double-sided rendering
     this.renderState.rasterState.cullMode = CullMode.Off;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  override clone(): TrailMaterial {
+    const dest = new TrailMaterial(this._engine);
+    this._cloneToAndModifyName(dest);
+    return dest;
   }
 }
