ee8a2e9662aeae5728cbee875342278a698c2590
GuoLei1990 <gl3336563@163.com>
2026-02-21 21:07:19 +0800

    fix: simplify mesh setter logic in MeshColliderShape

--- packages/core/src/physics/shape/MeshColliderShape.ts
+++ packages/core/src/physics/shape/MeshColliderShape.ts
@@ -64,8 +64,8 @@ export class MeshColliderShape extends ColliderShape {
 
   set mesh(value: ModelMesh) {
     if (this._mesh === value) return;
+    this._mesh = value;
     if (this._extractMeshData(value)) {
-      this._mesh = value;
       this._updateNativeMesh();
     }
   }
