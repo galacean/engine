1341e9c1250f936d335b20c8e439ea4bb3fa825d
GuoLei1990 <gl3336563@163.com>
2026-02-26 17:07:14 +0800

    refactor: rename alpha to coverage for clarity in fragment shader

--- packages/core/src/shaderlib/extra/text.fs.glsl
+++ packages/core/src/shaderlib/extra/text.fs.glsl
@@ -7,9 +7,9 @@ void main()
 {
   vec4 texColor = texture2D(renderElement_TextTexture, v_uv);
   #ifdef GRAPHICS_API_WEBGL2
-    float alpha = texColor.r;
+    float coverage = texColor.r;
   #else
-    float alpha = texColor.a;
+    float coverage = texColor.a;
   #endif
-  gl_FragColor = v_color * alpha;
+  gl_FragColor = vec4(v_color.rgb, v_color.a * coverage);
 }
