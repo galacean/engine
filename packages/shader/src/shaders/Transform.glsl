128b16e04f5a9ac7896801572b2ed8ccff6e9c36
chenmo.gl <chenmo.gl@antgroup.com>
2026-04-07 23:15:24 +0800

    fix: restore non-Layer shader improvements that were over-reverted
    Bring back normalMat extraction, transform_declare reordering,
    trailing whitespace fixes, and VertexPBR indent fix. Only the
    renderer_Layer relocation stays reverted.

--- packages/shader/src/shaders/Transform.glsl
+++ packages/shader/src/shaders/Transform.glsl
@@ -1,16 +1,17 @@
 #ifndef TRANSFORM_INCLUDED
 #define TRANSFORM_INCLUDED
 
-mat4 renderer_LocalMat;
-mat4 renderer_ModelMat;
 mat4 camera_ViewMat;
 mat4 camera_ProjMat;
+
+vec3 camera_Position;
+vec3 camera_Forward;
+vec4 camera_ProjectionParams;
+
+mat4 renderer_LocalMat;
+mat4 renderer_ModelMat;
 mat4 renderer_MVMat;
 mat4 renderer_MVPMat;
 mat4 renderer_NormalMat;
-
-vec3 camera_Position;
-vec3 camera_Forward; 
-vec4 camera_ProjectionParams;
 
 #endif
