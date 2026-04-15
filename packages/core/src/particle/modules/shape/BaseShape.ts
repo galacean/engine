7744e5727877b08a4a154ca44cda14da8634e098
chenmo.gl <chenmo.gl@antgroup.com>
2026-04-15 20:57:39 +0800

    refactor(particle): use Matrix 4x4 with standard math methods
    Replace Matrix3x3 with Matrix, use affineTransformation to build TRS
    matrix, transformToVec3 for position and transformNormal for direction.

--- packages/core/src/particle/modules/shape/BaseShape.ts
+++ packages/core/src/particle/modules/shape/BaseShape.ts
@@ -1,4 +1,4 @@
-import { MathUtil, Matrix3x3, Quaternion, Rand, Vector3 } from "@galacean/engine-math";
+import { MathUtil, Matrix, Quaternion, Rand, Vector3 } from "@galacean/engine-math";
 import { ParticleShapeType } from "./enums/ParticleShapeType";
 import { UpdateFlagManager } from "../../../UpdateFlagManager";
 import { ignoreClone } from "../../../clone/CloneManager";
@@ -25,7 +25,7 @@ export abstract class BaseShape {
   @ignoreClone
   private _rotationQuaternion = new Quaternion();
   @ignoreClone
-  private _matrix = new Matrix3x3();
+  private _matrix = new Matrix();
   @ignoreClone
   private _transformDirty = false;
 
@@ -147,18 +147,10 @@ export abstract class BaseShape {
   _generatePositionAndDirection(rand: Rand, emitTime: number, position: Vector3, direction: Vector3): void {
     this._generateLocalPositionAndDirection(rand, emitTime, position, direction);
     if (this._hasShapeTransform()) {
-      const e = this._getRSMatrix().elements;
-      const { x: px, y: py, z: pz } = position;
-      position.set(
-        e[0] * px + e[3] * py + e[6] * pz,
-        e[1] * px + e[4] * py + e[7] * pz,
-        e[2] * px + e[5] * py + e[8] * pz
-      );
-      const { x: dx, y: dy, z: dz } = direction;
-      direction
-        .set(e[0] * dx + e[3] * dy + e[6] * dz, e[1] * dx + e[4] * dy + e[7] * dz, e[2] * dx + e[5] * dy + e[8] * dz)
-        .normalize();
-      position.add(this._position);
+      const matrix = this._getMatrix();
+      Vector3.transformToVec3(position, matrix, position);
+      Vector3.transformNormal(direction, matrix, direction);
+      direction.normalize();
     }
   }
 
@@ -168,9 +160,7 @@ export abstract class BaseShape {
   _getPositionRange(outMin: Vector3, outMax: Vector3): void {
     this._getLocalPositionRange(outMin, outMax);
     if (this._hasShapeTransform()) {
-      this._transformBoundingBox(outMin, outMax);
-      outMin.add(this._position);
-      outMax.add(this._position);
+      this._transformBoundingBox(outMin, outMax, true);
     }
   }
 
@@ -180,7 +170,7 @@ export abstract class BaseShape {
   _getDirectionRange(outMin: Vector3, outMax: Vector3): void {
     this._getLocalDirectionRange(outMin, outMax);
     if (this._hasShapeTransform()) {
-      this._transformBoundingBox(outMin, outMax);
+      this._transformBoundingBox(outMin, outMax, false);
     }
   }
 
@@ -201,27 +191,16 @@ export abstract class BaseShape {
     this._updateManager.dispatch();
   };
 
-  private _getRSMatrix(): Matrix3x3 {
+  private _getMatrix(): Matrix {
     if (this._transformDirty) {
-      const { _rotation: r, _scale: s, _rotationQuaternion: q, _matrix: rs } = this;
+      const { _rotation: r, _rotationQuaternion: q } = this;
       Quaternion.rotationEuler(
         MathUtil.degreeToRadian(r.x),
         MathUtil.degreeToRadian(r.y),
         MathUtil.degreeToRadian(r.z),
         q
       );
-      Matrix3x3.rotationQuaternion(q, rs);
-      // Multiply each column by scale: col0 *= sx, col1 *= sy, col2 *= sz
-      const e = rs.elements;
-      e[0] *= s.x;
-      e[1] *= s.x;
-      e[2] *= s.x;
-      e[3] *= s.y;
-      e[4] *= s.y;
-      e[5] *= s.y;
-      e[6] *= s.z;
-      e[7] *= s.z;
-      e[8] *= s.z;
+      Matrix.affineTransformation(this._scale, q, this._position, this._matrix);
       this._transformDirty = false;
     }
     return this._matrix;
@@ -235,10 +214,10 @@ export abstract class BaseShape {
   }
 
   /**
-   * Arvo method: transform AABB by RS matrix
+   * Arvo method: transform AABB by matrix
    */
-  private _transformBoundingBox(outMin: Vector3, outMax: Vector3): void {
-    const e = this._getRSMatrix().elements;
+  private _transformBoundingBox(outMin: Vector3, outMax: Vector3, includeTranslation: boolean): void {
+    const e = this._getMatrix().elements;
 
     const minX = outMin.x,
       minY = outMin.y,
@@ -250,23 +229,32 @@ export abstract class BaseShape {
     const e0 = e[0],
       e1 = e[1],
       e2 = e[2];
-    const e3 = e[3],
-      e4 = e[4],
-      e5 = e[5];
-    const e6 = e[6],
-      e7 = e[7],
-      e8 = e[8];
+    const e4 = e[4],
+      e5 = e[5],
+      e6 = e[6];
+    const e8 = e[8],
+      e9 = e[9],
+      e10 = e[10];
 
     outMin.set(
-      (e0 > 0 ? e0 * minX : e0 * maxX) + (e3 > 0 ? e3 * minY : e3 * maxY) + (e6 > 0 ? e6 * minZ : e6 * maxZ),
-      (e1 > 0 ? e1 * minX : e1 * maxX) + (e4 > 0 ? e4 * minY : e4 * maxY) + (e7 > 0 ? e7 * minZ : e7 * maxZ),
-      (e2 > 0 ? e2 * minX : e2 * maxX) + (e5 > 0 ? e5 * minY : e5 * maxY) + (e8 > 0 ? e8 * minZ : e8 * maxZ)
+      (e0 > 0 ? e0 * minX : e0 * maxX) + (e4 > 0 ? e4 * minY : e4 * maxY) + (e8 > 0 ? e8 * minZ : e8 * maxZ),
+      (e1 > 0 ? e1 * minX : e1 * maxX) + (e5 > 0 ? e5 * minY : e5 * maxY) + (e9 > 0 ? e9 * minZ : e9 * maxZ),
+      (e2 > 0 ? e2 * minX : e2 * maxX) + (e6 > 0 ? e6 * minY : e6 * maxY) + (e10 > 0 ? e10 * minZ : e10 * maxZ)
     );
 
     outMax.set(
-      (e0 > 0 ? e0 * maxX : e0 * minX) + (e3 > 0 ? e3 * maxY : e3 * minY) + (e6 > 0 ? e6 * maxZ : e6 * minZ),
-      (e1 > 0 ? e1 * maxX : e1 * minX) + (e4 > 0 ? e4 * maxY : e4 * minY) + (e7 > 0 ? e7 * maxZ : e7 * minZ),
-      (e2 > 0 ? e2 * maxX : e2 * minX) + (e5 > 0 ? e5 * maxY : e5 * minY) + (e8 > 0 ? e8 * maxZ : e8 * minZ)
+      (e0 > 0 ? e0 * maxX : e0 * minX) + (e4 > 0 ? e4 * maxY : e4 * minY) + (e8 > 0 ? e8 * maxZ : e8 * minZ),
+      (e1 > 0 ? e1 * maxX : e1 * minX) + (e5 > 0 ? e5 * maxY : e5 * minY) + (e9 > 0 ? e9 * maxZ : e9 * minZ),
+      (e2 > 0 ? e2 * maxX : e2 * minX) + (e6 > 0 ? e6 * maxY : e6 * minY) + (e10 > 0 ? e10 * maxZ : e10 * minZ)
     );
+
+    if (includeTranslation) {
+      outMin.x += e[12];
+      outMin.y += e[13];
+      outMin.z += e[14];
+      outMax.x += e[12];
+      outMax.y += e[13];
+      outMax.z += e[14];
+    }
   }
 }
