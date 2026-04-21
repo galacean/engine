6dfbdacfc304363c3c94e8f524527662f5c4ffbf
shensi.zxd <shensi.zxd@antgroup.com>
2026-04-21 11:14:11 +0800

    fix(e2e): move Shader.create after engine init in opaque-texture

--- e2e/case/camera-opaque-texture.ts
+++ e2e/case/camera-opaque-texture.ts
@@ -22,72 +22,71 @@ import {
 import { ShaderLab } from "@galacean/engine-shaderlab";
 import { initScreenshot, updateForE2E } from "./.mockForE2E";
 
-Shader.create(`
-  Shader "RenderOpaqueTexture" {
-    SubShader "Default" {
-      Pass "Forward" {
-        Tags { pipelineStage = "Forward" }
-
-        #include "Common/Common.glsl"
-        #include "Common/Transform.glsl"
-        #include "Common/Fog.glsl"
-        #include "Common/Attributes.glsl"
-        #include "Skin/Skin.glsl"
-        #include "Skin/BlendShape.glsl"
-
-        sampler2D camera_OpaqueTexture;
-
-        struct Varyings {
-          vec2 v_uv;
-          #if SCENE_FOG_MODE != 0
-            vec3 v_positionVS;
-          #endif
-        };
-
-        Varyings vert(Attributes attr) {
-          Varyings v;
-
-          vec4 position = vec4(attr.POSITION, 1.0);
-
-          #ifdef RENDERER_HAS_BLENDSHAPE
-            calculateBlendShape(attr, position);
-          #endif
-
-          #ifdef RENDERER_HAS_SKIN
-            mat4 skinMatrix = getSkinMatrix(attr);
-            position = skinMatrix * position;
-          #endif
-
-          gl_Position = renderer_MVPMat * position;
-          #ifdef RENDERER_HAS_UV
-            v.v_uv = attr.TEXCOORD_0;
-          #endif
-
-          #if SCENE_FOG_MODE != 0
-            v.v_positionVS = (renderer_MVMat * position).xyz;
-          #endif
-
-          return v;
-        }
-
-        void frag(Varyings v) {
-          vec4 baseColor = texture2D(camera_OpaqueTexture, v.v_uv);
-          gl_FragColor = baseColor;
-
-          #ifndef MATERIAL_IS_TRANSPARENT
-            gl_FragColor.a = 1.0;
-          #endif
-        }
-
-        VertexShader = vert;
-        FragmentShader = frag;
-      }
-    }
-  }
-`);
-
 Logger.enable();
 WebGLEngine.create({ canvas: "canvas", shaderLab: new ShaderLab() }).then((engine) => {
+  Shader.create(`
+    Shader "RenderOpaqueTexture" {
+      SubShader "Default" {
+        Pass "Forward" {
+          Tags { pipelineStage = "Forward" }
+
+          #include "Common/Common.glsl"
+          #include "Common/Transform.glsl"
+          #include "Common/Fog.glsl"
+          #include "Common/Attributes.glsl"
+          #include "Skin/Skin.glsl"
+          #include "Skin/BlendShape.glsl"
+
+          sampler2D camera_OpaqueTexture;
+
+          struct Varyings {
+            vec2 v_uv;
+            #if SCENE_FOG_MODE != 0
+              vec3 v_positionVS;
+            #endif
+          };
+
+          Varyings vert(Attributes attr) {
+            Varyings v;
+
+            vec4 position = vec4(attr.POSITION, 1.0);
+
+            #ifdef RENDERER_HAS_BLENDSHAPE
+              calculateBlendShape(attr, position);
+            #endif
+
+            #ifdef RENDERER_HAS_SKIN
+              mat4 skinMatrix = getSkinMatrix(attr);
+              position = skinMatrix * position;
+            #endif
+
+            gl_Position = renderer_MVPMat * position;
+            #ifdef RENDERER_HAS_UV
+              v.v_uv = attr.TEXCOORD_0;
+            #endif
+
+            #if SCENE_FOG_MODE != 0
+              v.v_positionVS = (renderer_MVMat * position).xyz;
+            #endif
+
+            return v;
+          }
+
+          void frag(Varyings v) {
+            vec4 baseColor = texture2D(camera_OpaqueTexture, v.v_uv);
+            gl_FragColor = baseColor;
+
+            #ifndef MATERIAL_IS_TRANSPARENT
+              gl_FragColor.a = 1.0;
+            #endif
+          }
+
+          VertexShader = vert;
+          FragmentShader = frag;
+        }
+      }
+    }
+  `);
   engine.canvas.resizeByClientSize(2);
   const scene = engine.sceneManager.activeScene;
   const rootEntity = scene.createRootEntity();
