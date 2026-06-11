09081f1b7b4aaee55efc5a609c7669515423a97d
luzhuang <wz84888@antgroup.com>
2026-06-11 15:37:56 +0800

    chore(animation): remove stale animator state instance

---
+++ tests/src/core/AnimatorHang.test.ts
@@ -0,0 +1,20 @@
+import { Animator, Camera } from "@galacean/engine-core";
+import "@galacean/engine-loader";
+import type { GLTFResource } from "@galacean/engine-loader";
+import { WebGLEngine } from "@galacean/engine-rhi-webgl";
+import { describe, expect, it } from "vitest";
+import { glbResource } from "./model/fox";
+const canvasDOM = document.createElement("canvas");
+canvasDOM.width = 1024;
+canvasDOM.height = 1024;
+describe("Canvas 1024 test", async function () {
+  const engine = await WebGLEngine.create({ canvas: canvasDOM });
+  const scene = engine.sceneManager.activeScene;
+  const rootEntity = scene.createRootEntity();
+  rootEntity.addComponent(Camera);
+  const resource = await engine.resourceManager.load<GLTFResource>(glbResource);
+  const defaultSceneRoot = resource.defaultSceneRoot;
+  rootEntity.addChild(defaultSceneRoot);
+  const animator = defaultSceneRoot.getComponent(Animator);
+  it("loaded", () => { expect(animator).not.eq(null); });
+});
