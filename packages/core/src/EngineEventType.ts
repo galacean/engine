d9e518d64b7adf3e8dca25f740c52c9744db8046
chenmo.gl <chenmo.gl@antgroup.com>
2026-06-15 16:59:50 +0800

    docs(core): add EngineEventType API comments

--- packages/core/src/EngineEventType.ts
+++ packages/core/src/EngineEventType.ts
@@ -1,6 +1,13 @@
+/**
+ * Engine event type.
+ */
 export enum EngineEventType {
+  /** Dispatched when the engine starts running. */
   Run = "run",
+  /** Dispatched when the engine shuts down. */
   Shutdown = "shutdown",
+  /** Dispatched when the graphic device is lost. */
   DeviceLost = "devicelost",
+  /** Dispatched when the graphic device is restored. */
   DeviceRestored = "devicerestored"
 }
