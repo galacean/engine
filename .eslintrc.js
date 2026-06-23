01cb0453a7120e4409005578340f373907e12366
luo2430 <luo2430@qq.com>
2026-06-23 20:22:45 +0800

    chore: enhance eslint config and migrate docs to pnpm

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,75 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Eslint TypeScript Parser
-  extends: ["plugin:prettier/recommended"],
-  plugins: ["@typescript-eslint"],
-  parserOptions: {
-    ecmaVersion: 2019,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true
+  root: true,
+  ignorePatterns: ["pnpm-lock.yaml"],
+  overrides: [
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      extends: ["plugin:prettier/recommended"],
+      plugins: ["@typescript-eslint"],
+      parserOptions: {
+        ecmaVersion: 2019,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      env: {
+        browser: true,
+        node: true
+      }
+    },
+    {
+      files: ["*.md", "*.mdx"],
+      extends: ["plugin:prettier/recommended"],
+      parser: "eslint-parser-plain",
+      rules: {
+        "prettier/prettier": [
+          "error",
+          {
+            parser: "markdown"
+          }
+        ]
+      }
+    },
+    {
+      files: ["*.html"],
+      extends: ["plugin:prettier/recommended"],
+      parser: "eslint-parser-plain",
+      rules: {
+        "prettier/prettier": [
+          "error",
+          {
+            parser: "html"
+          }
+        ]
+      }
+    },
+    {
+      files: ["*.json"],
+      extends: ["plugin:prettier/recommended"],
+      parser: "eslint-parser-plain",
+      rules: {
+        "prettier/prettier": [
+          "error",
+          {
+            parser: "json"
+          }
+        ]
+      }
+    },
+    {
+      files: ["*.yaml", "*.yml"],
+      extends: ["plugin:prettier/recommended"],
+      parser: "eslint-parser-plain",
+      rules: {
+        "prettier/prettier": [
+          "error",
+          {
+            parser: "yaml"
+          }
+        ]
+      }
     }
-  },
-  env: {
-    browser: true,
-    node: true
-  }
+  ]
 };
